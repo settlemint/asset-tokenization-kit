e76498c692f96fa2542a167971fb7f3e4900278d
Patrick Mualaba <patrick.mualaba@live.be>
2024-09-16 15:52:39 +0200

    file uploads

--- apps/dapp/actions/upload.action.ts
+++ apps/dapp/actions/upload.action.ts
@@ -1,10 +1,18 @@
 "use server";
 
 import fs from "node:fs/promises";
+import path from "node:path";
 
-export async function uploadFile(state: unknown, formData: FormData) {
-  const file = formData.get("file") as File;
-  const buffer = await file.arrayBuffer();
-  const bytes = new Uint8Array(buffer);
-  await fs.writeFile(`./public/uploads/${file.name}`, bytes);
+export async function uploadFile(meta: { name: string; uploadDir: string }, formData: FormData) {
+  const files = formData.getAll(meta.name) as File[];
+  const uploadsDir = path.join(process.cwd(), meta.uploadDir);
+  await fs.mkdir(uploadsDir, { recursive: true });
+
+  for (const file of files) {
+    const buffer = await file.arrayBuffer();
+    const bytes = new Uint8Array(buffer);
+    await fs.writeFile(`${uploadsDir}/${file.name}`, bytes);
+  }
+
+  return { message: `${files.length} file(s) uploaded successfully` };
 }
