f67e1236c49ef749a489ce419abefdbe93ee8af2
Patrick Mualaba <patrick.mualaba@live.be>
2024-10-14 11:23:38 +0200

    restore table

--- app/wallet/tokens/page.tsx
+++ app/wallet/tokens/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { SidePanel } from "@/components/ui/sidepanel-sheet";
 import { type SearchParams, createSearchParamsCache, parseAsInteger, parseAsJson, parseAsString } from "nuqs/server";
 import { CreateTokenForm } from "./forms/create-token-form";
+import { TokenTable } from "./tables/tokens-table";
 
 const searchParamsCache = createSearchParamsCache({
   currentStep: parseAsInteger.withDefault(1),
@@ -24,13 +25,16 @@ export default function WalletTokenPage({ searchParams }: WalletTokenPageProps) 
         title="Create a new token"
         description="Easily convert your assets into digital tokens using this step-by-step wizard."
         trigger={
-          <Button className="absolute right-10" variant="outline">
-            Start Wizard
+          <Button className="absolute right-8" variant="outline">
+            Create new token
           </Button>
         }
       >
         <CreateTokenForm defaultValues={parsedParams.state} />
       </SidePanel>
+      <div className="mt-10">
+        <TokenTable />
+      </div>
     </div>
   );
 }
