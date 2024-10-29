7a7dc7016cc20ad22596ae4f49ca695d9fa10a00
Patrick Mualaba <patrick.mualaba@live.be>
2024-10-29 15:27:19 +0100

    fix: submit button

--- src/app/(sections)/issuer/tokens/_forms/mint-token-form-schema.ts
+++ src/app/(sections)/issuer/tokens/_forms/mint-token-form-schema.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const MintTokenSchema = z.object({
   to: z.string(),
-  amount: z.string(),
+  amount: z.number(),
 });
 
 export type MintTokenSchemaType = z.infer<typeof MintTokenSchema>;
 
 export const mintTokenDefaultValues: MintTokenSchemaType = {
   to: "",
-  amount: "",
+  amount: 0,
 } as const;
 
 export type MintTokenFormPageFields = keyof typeof mintTokenDefaultValues;
