86ecdca197759dddd7ef8206d4e7b819a726bcc9
Roderik van der Veer <roderik@settlemint.com>
2024-10-31 11:46:47 +0100

    feat: add a transfer form (#204)
    ## Summary by Sourcery
    
    Add a new feature for transferring tokens with a form and display token
    holder balances in a table format.
    
    New Features:
    - Introduce a transfer token form that allows users to transfer tokens
    to another wallet, including input fields for the amount and recipient
    address.
    - Add a balances table component to display token holder information,
    including holder address, last transaction, balance, percentage of total
    supply, and transaction counts.

---
+++ src/app/(sections)/user/portfolio/[address]/details/_forms/transfer-token-form-schema.ts
@@ -0,0 +1,24 @@
+import { isAddress } from "viem";
+import { z } from "zod";
+
+export const TransferTokenSchema = z.object({
+  to: z.string(),
+  amount: z.coerce.number(),
+  tokenAddress: z
+    .string()
+    .refine((address) => isAddress(address), { message: "Please enter a valid Ethereum address" }),
+});
+
+export type TransferTokenSchemaType = z.infer<typeof TransferTokenSchema>;
+
+export const transferTokenDefaultValues: TransferTokenSchemaType = {
+  to: "",
+  amount: 0,
+  tokenAddress: "0x",
+} as const;
+
+export type TransferTokenFormPageFields = keyof typeof transferTokenDefaultValues;
+
+export const transferTokenFormPageFields: TransferTokenFormPageFields[] = Object.keys(
+  transferTokenDefaultValues,
+) as TransferTokenFormPageFields[];
---
+++ src/app/(sections)/user/portfolio/[address]/details/_forms/transfer-token-form.tsx
@@ -0,0 +1,162 @@
+"use client";
+
+import { NumericInput } from "@/components/blocks/form/form-input-numeric";
+import { FormMultiStepProvider } from "@/components/blocks/form/form-multistep";
+import { FormPage } from "@/components/blocks/form/form-page";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { portalClient, portalGraphql } from "@/lib/settlemint/portal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useHookFormAction } from "@next-safe-action/adapter-react-hook-form/hooks";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useLocalStorage } from "usehooks-ts";
+import { transferTokenAction } from "./transfer-token-action";
+import {
+  TransferTokenSchema,
+  type TransferTokenSchemaType,
+  transferTokenFormPageFields,
+} from "./transfer-token-form-schema";
+
+interface TransferTokenFormProps {
+  defaultValues: Partial<TransferTokenSchemaType>;
+  formId: string;
+  className?: string;
+}
+
+const TransferTokenReceiptQuery = portalGraphql(`
+query TransferTokenReceiptQuery($transactionHash: String!) {
+  getTransaction(transactionHash: $transactionHash) {
+    receipt {
+      contractAddress
+      status
+      blockNumber
+    }
+  }
+}`);
+
+export function TransferTokenForm({ defaultValues }: TransferTokenFormProps) {
+  const [localStorageState] = useLocalStorage<Partial<TransferTokenSchemaType>>("state", defaultValues);
+  const queryClient = useQueryClient();
+
+  const { form, resetFormAndAction } = useHookFormAction(transferTokenAction, zodResolver(TransferTokenSchema), {
+    actionProps: {
+      onSuccess: () => {
+        resetFormAndAction();
+      },
+    },
+    formProps: {
+      mode: "all",
+      defaultValues: {
+        ...transferTokenFormPageFields,
+        ...defaultValues,
+        ...localStorageState,
+      },
+    },
+    errorMapProps: {},
+  });
+
+  function onSubmit(values: TransferTokenSchemaType) {
+    toast.promise(
+      async () => {
+        const transactionHash = await transferTokenAction(values);
+
+        const startTime = Date.now();
+        const timeout = 120000; // 2 minutes
+
+        while (Date.now() - startTime < timeout) {
+          const txresult = await portalClient.request(TransferTokenReceiptQuery, {
+            transactionHash: transactionHash?.data ?? "",
+          });
+
+          const receipt = txresult.getTransaction?.receipt;
+          if (receipt) {
+            if (receipt.status === "Success") {
+              return receipt;
+            }
+            throw new Error("Transaction failed");
+          }
+
+          // Wait for 500 milliseconds before the next attempt
+          await new Promise((resolve) => setTimeout(resolve, 500));
+        }
+        throw new Error(`Transaction not processed within ${timeout / 1000} seconds`);
+      },
+      {
+        loading: "Transferring token...",
+        success: (data) => {
+          queryClient.invalidateQueries({ queryKey: ["token-volumes"] });
+          return `${values.amount} tokens transferred to (${values.to}) in block ${data.blockNumber}`;
+        },
+        error: (error) => {
+          queryClient.invalidateQueries({ queryKey: ["token-volumes"] });
+          console.error(error);
+          return `Error: ${error instanceof Error ? error.message : "An unexpected error occurred"}`;
+        },
+      },
+    );
+  }
+
+  return (
+    <div className="MinTokenForm container mt-8">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Transfer tokens</CardTitle>
+          <CardDescription>Transfer your tokens to another wallet</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <FormMultiStepProvider
+            config={{ useLocalStorageState: false, useQueryState: false }}
+            formId="transfer-token-form"
+          >
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormPage
+                  form={form}
+                  title="Transfer tokens"
+                  fields={["amount", "to"]}
+                  withSheetClose
+                  controls={{
+                    submit: { buttonText: "Submit" },
+                  }}
+                >
+                  {/* Transfer amount */}
+                  <FormField
+                    control={form.control}
+                    name="amount"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Amount</FormLabel>
+                        <FormControl>
+                          <NumericInput placeholder="Amount" {...field} />
+                        </FormControl>
+                        <FormDescription>This is the amount of tokens you want to transfer</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Transfer to wallet */}
+                  <FormField
+                    control={form.control}
+                    name="to"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Wallet address</FormLabel>
+                        <FormControl>
+                          <Input placeholder="To address" {...field} />
+                        </FormControl>
+                        <FormDescription>This is the wallet address of the token holder</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </FormPage>
+              </form>
+            </Form>
+          </FormMultiStepProvider>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
