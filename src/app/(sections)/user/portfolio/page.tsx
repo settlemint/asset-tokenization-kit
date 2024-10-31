56498deaa90e9294ff1509242e72644d7e4a7c38
Roderik van der Veer <roderik@settlemint.com>
2024-10-31 10:15:36 +0100

    feat: add a portfolio table with prices

--- src/app/(sections)/user/portfolio/page.tsx
+++ src/app/(sections)/user/portfolio/page.tsx
@@ -1,9 +1,17 @@
-export default function UserPortfolio() {
+import { auth } from "@/lib/auth/auth";
+import type { Address } from "viem";
+import { PortfolioTable } from "./_components/portfolio-table";
+
+export default async function UserPortfolio() {
+  const session = await auth();
+  const address = session?.user.wallet;
+
   return (
     <>
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Portfolio</h2>
       </div>
+      <PortfolioTable address={address as Address} />
     </>
   );
 }
