0f5c38b110fe0ed846b10eaa88c27fb6a2de2bfa
Roderik van der Veer <roderik@settlemint.com>
2024-10-31 11:44:26 +0100

    feat: add a transfer form

---
+++ src/app/(sections)/user/portfolio/[address]/details/_forms/transfer-token-action.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { auth } from "@/lib/auth/auth";
+import { actionClient } from "@/lib/safe-action";
+import { portalClient, portalGraphql } from "@/lib/settlemint/portal";
+import { parseEther } from "viem";
+import { TransferTokenSchema } from "./transfer-token-form-schema";
+
+const TransferTokenMutation = portalGraphql(`
+mutation TransferTokenMutation($address: String!, $from: String!, $to: String!, $amount: String!) {
+  StarterKitERC20Transfer(
+    address: $address
+    from: $from
+    input: {to: $to, value: $amount}
+  ) {
+    transactionHash
+  }
+}
+`);
+
+export const transferTokenAction = actionClient.schema(TransferTokenSchema).action(async ({ parsedInput }) => {
+  const { to, amount, tokenAddress } = parsedInput;
+  const session = await auth();
+
+  if (!session?.user) {
+    throw new Error("User not authenticated");
+  }
+
+  const result = await portalClient.request(TransferTokenMutation, {
+    address: tokenAddress,
+    from: session.user.wallet,
+    to: to,
+    amount: parseEther(amount.toString()).toString(),
+  });
+
+  const transactionHash = result.StarterKitERC20Transfer?.transactionHash;
+
+  if (!transactionHash) {
+    throw new Error("Transaction hash not found");
+  }
+
+  return transactionHash;
+});
