ee11b1e641c4c264a0b6762bbb56741910492d1e
Roderik van der Veer <roderik@settlemint.com>
2024-12-04 12:16:46 +0100

    fix: publish and custom deployment

--- .github/scripts/update-deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Updating the Custom Deployment with the new version $VERSION"
-
-if [[ $TAG == "latest" ]]; then
-  bun settlemint platform update custom-deployment $VERSION --prod --wait
-elif [[ $TAG == "main" ]]; then
-  bun settlemint platform update custom-deployment $VERSION --wait
-fi
