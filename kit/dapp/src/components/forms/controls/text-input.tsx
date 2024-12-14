805c7081a067c1d083d08dd1748166e725943252
pmualaba <patrick.mualaba@live.be>
2024-12-14 12:08:46 +0100

    feat: text-input cva (#349)
    ## Summary by Sourcery
    
    Implement class variance authority for text input components to support
    variant-based styling and icon integration. Refactor existing text input
    components to utilize this new styling approach. Add Hasura
    configuration and schema files to establish database schema and
    metadata.
    
    New Features:
    - Introduce class variance authority (CVA) for text input components,
    allowing for variant-based styling including icon support.
    
    Enhancements:
    - Refactor text input components to use CVA for styling, improving
    flexibility and maintainability.
    
    Deployment:
    - Add Hasura configuration and schema files to set up database schema
    and metadata for the project.

--- kit/dapp/src/components/forms/controls/text-input.tsx
+++ kit/dapp/src/components/forms/controls/text-input.tsx
@@ -1,27 +1,66 @@
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input, type InputProps } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+import { type VariantProps, cva } from 'class-variance-authority';
+import type { ReactNode } from 'react';
 import type { Control, FieldValues, Path, RegisterOptions } from 'react-hook-form';
 
+const inputVariants = cva('', {
+  variants: {
+    variant: {
+      default: '',
+      icon: 'pl-8',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+const iconVariants = cva('absolute', {
+  variants: {
+    variant: {
+      default: 'hidden',
+      icon: 'top-8 left-2 flex h-[1.2rem] w-[1.2rem] items-center justify-center',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
 type TextInputProps<T extends FieldValues> = {
-  label?: string;
+  label: string;
   description?: string;
-} & Omit<InputProps, 'variant' | 'name'> & {
+  icon?: ReactNode;
+} & Omit<InputProps, 'name'> &
+  VariantProps<typeof inputVariants> & {
     name: Path<T>;
     control: Control<T>;
     shouldUnregister?: boolean;
     rules?: Pick<RegisterOptions<T, Path<T>>, 'required' | 'minLength' | 'maxLength' | 'pattern'>;
   };
 
-export function TextInput<T extends FieldValues>({ label, description, name, control, ...props }: TextInputProps<T>) {
+export function TextInput<T extends FieldValues>({
+  variant,
+  label,
+  description,
+  icon,
+  name,
+  control,
+  className,
+  ...props
+}: TextInputProps<T>) {
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
-        <FormItem>
+        <FormItem className="relative">
           {label && <FormLabel>{label}</FormLabel>}
+          {icon && <FormLabel className={iconVariants({ variant })}>{icon}</FormLabel>}
           <FormControl>
-            <Input {...field} {...props} />
+            <Input {...field} {...props} className={cn(inputVariants({ variant }), className)} />
           </FormControl>
           {description && <FormDescription>{description}</FormDescription>}
           <FormMessage />
