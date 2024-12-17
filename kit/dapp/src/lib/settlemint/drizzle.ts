a91a797650720f64a871f4d9414d2052d70a4d11
Roderik van der Veer <roderik@settlemint.com>
2024-12-17 11:55:00 +0100

    feat: introduce drizzle (#360)
    ## Summary by Sourcery
    
    Integrate Drizzle ORM into the project for improved database management
    and update SettleMint SDK dependencies to the latest version.
    
    New Features:
    - Introduce Drizzle ORM for database management in the project.
    
    Enhancements:
    - Update SettleMint SDK dependencies to version 0.6.44.
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ kit/dapp/src/lib/settlemint/drizzle.ts
@@ -0,0 +1,11 @@
+import { createDrizzleClient } from "@settlemint/sdk-hasura";
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
+
+export const drizzleClient: NodePgDatabase = createDrizzleClient({
+  databaseUrl: process.env.SETTLEMINT_HASURA_DATABASE_URL ?? "",
+  maxPoolSize: Number(process.env.SETTLEMINT_HASURA_DATABASE_MAX_POOL_SIZE),
+  idleTimeoutMillis: Number(process.env.SETTLEMINT_HASURA_DATABASE_IDLE_TIMEOUT),
+  connectionTimeoutMillis: Number(process.env.SETTLEMINT_HASURA_DATABASE_CONNECTION_TIMEOUT),
+  maxRetries: Number(process.env.SETTLEMINT_HASURA_DATABASE_MAX_RETRIES),
+  retryDelayMs: Number(process.env.SETTLEMINT_HASURA_DATABASE_RETRY_DELAY),
+});
