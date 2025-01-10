b2a312bd0c857ecb104b1db1419a0ef14c902db9
Patrick Mualaba <patrick.mualaba@live.be>
2025-01-10 22:22:38 +0100

    feat: select input component

--- kit/dapp/src/app/(private)/admin/tokens/_components/create-token-form/create-token-form.tsx
+++ kit/dapp/src/app/(private)/admin/tokens/_components/create-token-form/create-token-form.tsx
@@ -249,42 +249,6 @@ export function CreateTokenForm({ defaultValues }: CreateTokenFormProps) {
                   </SelectItem>
                 ))}
               </SelectInput>
-
-              <TextInput
-                control={form.control}
-                label="Name"
-                name="tokenName"
-                placeholder="e.g., My Stable Coin"
-                showRequired
-              />
-              {/* Symbol */}
-              <TextInput
-                control={form.control}
-                label="Symbol"
-                name="tokenSymbol"
-                placeholder="e.g., MSC"
-                showRequired
-              />
-              {/* Decimals */}
-              <NumericInput
-                control={form.control}
-                label="Decimals"
-                name="decimals"
-                placeholder="e.g., MSC"
-                showRequired
-              />
-              {/* ISIN */}
-              <TextInput control={form.control} label="ISIN" name="isin" placeholder="e.g., US1234567890" />
-              <CardTitle className="!mt-16">Stable coin configuration</CardTitle>
-              <CardDescription>Set parameters specific to your stable coin.</CardDescription>
-              {/* Collateral Proof Validity (Seconds) */}
-              <NumericInput
-                control={form.control}
-                label="Collateral Proof Validity (Seconds)"
-                name="collateralProofValidity"
-                placeholder="e.g., 3600"
-                showRequired
-              />
             </FormStep>
           </FormMultiStep>
         </CardContent>
