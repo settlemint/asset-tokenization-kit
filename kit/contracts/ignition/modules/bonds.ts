c128aba8b1bfd90d10146cfbf1f632a37f059b4d
Daan Poron <daan@poron.be>
2025-01-20 12:41:58 +0100

    f

--- kit/contracts/ignition/modules/bonds.ts
+++ kit/contracts/ignition/modules/bonds.ts
@@ -5,7 +5,7 @@ const BondsModule = buildModule('BondsModule', (m) => {
   const { bondFactory } = m.useModule(BondFactoryModule);
 
   const oneYearFromNow = Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60;
-  const createBondUSTB = m.call(bondFactory, 'create', ['US Treasury Bond', 'USTB', 8, oneYearFromNow], {
+  const createBondUSTB = m.call(bondFactory, 'create', ['US Treasury Bond', 'USTB', 2, oneYearFromNow], {
     id: 'createBondUSTB',
   });
   const readBondUSTBAddress = m.readEventArgument(createBondUSTB, 'BondCreated', 'token', {
