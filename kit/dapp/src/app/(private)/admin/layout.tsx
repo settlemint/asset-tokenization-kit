55327f9dcc4883565b6dd28e971453eb264d8ee1
Snigdha Singh <62167899+snigdha920@users.noreply.github.com>
2025-01-27 20:34:34 +0100

    feat: admin pages sidebar (#615)

--- kit/dapp/src/app/(private)/admin/layout.tsx
+++ kit/dapp/src/app/(private)/admin/layout.tsx
@@ -1,36 +1,52 @@
-import { PrivateSidebar, type SidebarData } from '@/components/layout/sidebar';
+import type { NavElement } from '@/components/layout/nav-main';
+import { PrivateSidebar } from '@/components/layout/sidebar';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { auth } from '@/lib/auth/auth';
+import { AlertTriangle, ArrowRightLeft, LayoutDashboard, Settings, Users } from 'lucide-react';
 import { headers } from 'next/headers';
 import type { PropsWithChildren } from 'react';
 import Header from '../../../components/layout/header';
-import { createMainSideBarData } from './_lib/dynamic-navigation';
+import { createTokenManagementNavGroup } from './_lib/dynamic-navigation';
 
 export default async function AdminLayout({ children }: PropsWithChildren) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   const role = (session?.user?.role ?? 'user') as 'admin' | 'issuer' | 'user';
-
-  const sidebarData: SidebarData = {
-    main: await createMainSideBarData(role),
-    secondary: [
-      {
-        title: 'Administration',
-        items: [
-          {
-            title: 'User Management',
-            iconName: 'Users',
-            url: '/admin/users',
-          },
-        ],
-      },
-    ],
-  };
+  const tokenManagementNavGroup = await createTokenManagementNavGroup(role);
+  const defaultNavItems: NavElement[] = [
+    {
+      label: 'Dashboard',
+      icon: <LayoutDashboard />,
+      path: '/admin',
+    },
+    {
+      label: 'Actions',
+      icon: <AlertTriangle />,
+      path: '/actions',
+      badge: '12',
+    },
+    ...(tokenManagementNavGroup ? [tokenManagementNavGroup] : []),
+    {
+      label: 'User management',
+      icon: <Users />,
+      path: '/users',
+    },
+    {
+      label: 'Transactions',
+      icon: <ArrowRightLeft />,
+      path: '/transactions',
+    },
+    {
+      label: 'Settings',
+      icon: <Settings />,
+      path: '/settings',
+    },
+  ];
 
   return (
     <SidebarProvider>
-      <PrivateSidebar role={role} mode="admin" data={sidebarData} className="group-data-[side=left]:border-0" />
+      <PrivateSidebar role={role} mode="admin" items={defaultNavItems} />
       <SidebarInset className="bg-sidebar">
         <Header />
         <main className="flex min-h-screen flex-1 flex-col gap-4 rounded-tl-xl bg-background p-6">{children}</main>
