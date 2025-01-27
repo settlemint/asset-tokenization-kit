786c75e27622d472ea38661d2575a576a5c5ae99
Patrick Mualaba <patrick.mualaba@live.be>
2025-01-27 21:46:30 +0100

    details tab

--- kit/dapp/src/components/blocks/asset-tabs/asset-details-grid.tsx
+++ kit/dapp/src/components/blocks/asset-tabs/asset-details-grid.tsx
@@ -30,73 +30,73 @@ export function AssetDetailsGrid<
       <CardContent className="grid grid-cols-6 gap-x-4 gap-y-8">
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Name</span>
-          <p className="text-md">{data.name}</p>
+          <div className="text-md">{data.name}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Symbol</span>
-          <p className="text-md">{data.symbol}</p>
+          <div className="text-md">{data.symbol}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">ISIN</span>
-          <p className="text-md">{data.isin}</p>
+          <div className="text-md">{data.isin}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Contract address</span>
-          <p className="text-md">
+          <div className="text-md">
             {' '}
             <span className="truncate text-sm">{shortHex(data.id, 12, 8)}</span>
-          </p>
+          </div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Creator</span>
-          <p className="flex items-center gap-2 text-md">
+          <div className="flex items-center gap-2 text-md">
             <AddressAvatar address={data.id} variant="small" />
             <span className="truncate text-sm">{shortHex(data.id, 12, 8)}</span>
-          </p>
+          </div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Deployed on</span>
-          <p>12/06/2024</p>
+          <div className="text-md">12/06/2024</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Total supply</span>
-          <p className="text-md">{data.totalSupply}</p>
+          <div className="text-md">{data.totalSupply}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Circulating supply</span>
-          <p className="text-md">{data.totalSupplyExact}</p>
+          <div className="text-md">{data.totalSupplyExact}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Burned supply</span>
-          <p className="text-md">0</p>
+          <div className="text-md">0</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">#Token holders</span>
-          <p className="text-md">123</p>
+          <div className="text-md">123</div>
         </div>
         <div className="col-span-2 space-y-1">
           <span className="font-medium text-muted-foreground text-sm">#Ownership concentration holders</span>
-          <p className="text-md">30%</p>
+          <div className="text-md">30%</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Proven collateral</span>
-          <p className="text-md">{data.collateralExact}</p>
+          <div className="text-md">{data.collateralExact}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Current collateral ratio</span>
-          <p className="text-md">{data.collateral}</p>
+          <div className="text-md">{data.collateral}</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Collateral proof expiration date</span>
-          <p className="text-md">12/06/2024 - 09:41 AM</p>
+          <div className="text-md">12/06/2024 - 09:41 AM</div>
         </div>
         <div className="space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Required collateral threshold</span>
-          <p className="text-md"> {'>'} 100 %</p>
+          <div className="text-md"> {'>'} 100 %</div>
         </div>
         <div className="col-span-2 space-y-1">
           <span className="font-medium text-muted-foreground text-sm">Collateral proof validity (seconds)</span>
-          <p>3600 seconds</p>
+          <div className="text-md">3600 seconds</div>
         </div>
       </CardContent>
     </Card>
