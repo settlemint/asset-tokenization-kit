8c2e5facb5c17dc97d4b709e46a2c37deecc773b
Snigdha Singh <snigdha920@gmail.com>
2025-01-28 14:41:12 +0100

    cleanup

--- kit/dapp/src/app/(private)/admin/(dashboard)/page.tsx
+++ kit/dapp/src/app/(private)/admin/(dashboard)/page.tsx
@@ -11,8 +11,6 @@ export default function AdminDashboard() {
         <ProcessedTransactionsStat />
       </div>
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   );
 }
