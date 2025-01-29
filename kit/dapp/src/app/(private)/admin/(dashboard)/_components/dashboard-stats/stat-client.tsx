78b0f7ee54a5db4309762e379617630a1881fb04
Snigdha Singh <snigdha920@gmail.com>
2025-01-29 18:30:57 +0100

    fix styling

--- kit/dapp/src/app/(private)/admin/(dashboard)/_components/dashboard-stats/stat-client.tsx
+++ kit/dapp/src/app/(private)/admin/(dashboard)/_components/dashboard-stats/stat-client.tsx
@@ -5,6 +5,14 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { DASHBOARD_STATS_QUERY_KEY } from './consts';
 import { getDashboardMetrics } from './data';
 
+const getVariantForDifference = (difference: string) => {
+  if (difference === '0') {
+    return 'neutral';
+  }
+
+  return difference.startsWith('-') ? 'negative' : 'positive';
+};
+
 export function DashboardStatsClient() {
   const { data } = useSuspenseQuery({
     queryKey: [DASHBOARD_STATS_QUERY_KEY],
@@ -16,25 +24,33 @@ export function DashboardStatsClient() {
   });
 
   return (
-    <div className="grid grid-cols-1 gap-4 divide-x-0 divide-y lg:grid-cols-3 lg:divide-x lg:divide-y-0">
+    <div className="grid grid-cols-1 gap-2 pb-2 lg:grid-cols-3">
       <Stat>
         <StatLabel>Assets supply</StatLabel>
         <StatValue>{data.assetsSupplyData.totalSupply.toLocaleString()}</StatValue>
-        <StatSubtext>
-          {data.assetsSupplyData.breakdown.map((item) => `${item.supply.toLocaleString()} ${item.type}`).join(' | ')}
+        <StatSubtext variant={getVariantForDifference(data.assetsSupplyData.difference24Hours)}>
+          {data.assetsSupplyData.sign}
+          {data.assetsSupplyData.difference24Hours}
         </StatSubtext>
+        <StatSubtext className="ml-2">Past 24 hours</StatSubtext>
       </Stat>
+
       <Stat>
         <StatLabel>Processed transactions</StatLabel>
         <StatValue>{data.processedTransactions.totalTransactions.toLocaleString()}</StatValue>
-        <StatSubtext>
-          {data.processedTransactions.transactionsInLast24Hours.toLocaleString()} in last 24 hours
+        <StatSubtext variant={getVariantForDifference(data.processedTransactions.difference24Hours.toString())}>
+          +{data.processedTransactions.difference24Hours}
         </StatSubtext>
+        <StatSubtext className="ml-2">Past 24 hours</StatSubtext>
       </Stat>
+
       <Stat>
         <StatLabel>Users</StatLabel>
         <StatValue>{data.usersData.totalUsers.toLocaleString()}</StatValue>
-        <StatSubtext>{data.usersData.usersInLast24Hours.toLocaleString()} in last 24 hours</StatSubtext>
+        <StatSubtext variant={getVariantForDifference(data.usersData.difference24Hours.toString())}>
+          +{data.usersData.difference24Hours}
+        </StatSubtext>
+        <StatSubtext className="ml-2">Past 24 hours</StatSubtext>
       </Stat>
     </div>
   );
--- kit/dapp/src/components/blocks/stat/stat.tsx
+++ kit/dapp/src/components/blocks/stat/stat.tsx
@@ -1,18 +1,42 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { type VariantProps, cva } from 'class-variance-authority';
 import type { ReactNode } from 'react';
 
-export function StatValue({ children, className }: { children: ReactNode; className?: string }) {
-  return <p className={cn('my-2 font-bold text-2xl', className)}>{children}</p>;
+type StatComponentProps = {
+  className?: string;
+  children: ReactNode;
+};
+
+export function Stat({ className, children }: StatComponentProps) {
+  return <div className={cn('rounded-xl border bg-card px-4 py-2 shadow', className)}>{children}</div>;
 }
 
-export function StatSubtext({ children, className }: { children: ReactNode; className?: string }) {
-  return <p className={cn('text-muted-foreground text-xs', className)}>{children}</p>;
+export function StatLabel({ className, children }: StatComponentProps) {
+  return <div className={cn('text-muted-foreground', className)}>{children}</div>;
 }
 
-export function StatLabel({ children, className }: { children: ReactNode; className?: string }) {
-  return <p className={cn('text-sm', className)}>{children}</p>;
+export function StatValue({ className, children }: StatComponentProps) {
+  return <div className={cn('my-2 font-bold text-3xl', className)}>{children}</div>;
 }
 
-export function Stat({ children }: { children: ReactNode }) {
-  return <div className="flex flex-col items-center justify-center py-4">{children}</div>;
+const statSubtextVariants = cva('font-medium', {
+  variants: {
+    variant: {
+      positive: 'font-medium text-emerald-700 dark:text-emerald-500',
+      negative: 'font-medium text-red-700 dark:text-red-500',
+      neutral: 'font-medium text-gray-500 dark:text-gray-400',
+      default: 'text-muted-foreground',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+type StatSubtextProps = StatComponentProps & VariantProps<typeof statSubtextVariants>;
+
+export function StatSubtext({ className, variant, children }: StatSubtextProps) {
+  return <span className={cn(statSubtextVariants({ variant, className }))}>{children}</span>;
 }
