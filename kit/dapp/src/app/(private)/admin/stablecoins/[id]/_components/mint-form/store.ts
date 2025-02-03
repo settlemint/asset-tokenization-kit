ea793e1c9633c52f68a57b4c2405d49fb9f27a4d
Patrick Mualaba <patrick.mualaba@live.be>
2025-02-03 15:31:07 +0100

    feat: mint form

--- kit/dapp/src/app/(private)/admin/stablecoins/[id]/_components/mint-form/store.ts
+++ kit/dapp/src/app/(private)/admin/stablecoins/[id]/_components/mint-form/store.ts
@@ -1,17 +1,15 @@
 'use server';
 
 import { getActiveOrganizationId, getAuthenticatedUser } from '@/lib/auth/auth';
-import { handleChallenge } from '@/lib/challenge';
 import { STABLE_COIN_FACTORY_ADDRESS } from '@/lib/contracts';
 import { actionClient } from '@/lib/safe-action';
 import { portalClient, portalGraphql } from '@/lib/settlemint/portal';
-import type { Address } from 'viem';
 import { CreateStablecoinOutputSchema } from './schema';
 import { CreateStablecoinFormSchema } from './schema';
 
 const MintStableCoin = portalGraphql(`
-  mutation MintStableCoin($address: String!, $to: String!, $amount: String!, $pincode: String!) {
-    StableCoinMint(address: $address, to: $to, amount: $amount, pincode: $pincode) {
+  mutation MintStableCoin($address: String!, $from: String!, $input: StableCoinMintInput!) {
+    StableCoinMint(address: $address, from: $from, input: $input) {
       transactionHash
     }
   }
@@ -26,18 +24,20 @@ export const createStablecoin = actionClient
 
     const data = await portalClient.request(MintStableCoin, {
       address: STABLE_COIN_FACTORY_ADDRESS,
-      to: address,
-      challengeResponse: await handleChallenge(user.wallet as Address, pincode),
-      amount,
-      metadata: {
-        organization: organizationId,
-        predictedAddress: predictedAddress.MintFactory?.predictAddress?.predicted,
+      from: user.wallet as string,
+      input: {
+        to: address,
+        amount: amount.toString(),
+        pincode,
+        metadata: {
+          organization: organizationId,
+        },
       },
     });
 
     const transactionHash = data.StableCoinMint?.transactionHash;
     if (!transactionHash) {
-      throw new Error('Failed to send the transaction to create the cryptocurrency');
+      throw new Error('Failed to send the transaction to mint the stablecoin');
     }
 
     return transactionHash;
