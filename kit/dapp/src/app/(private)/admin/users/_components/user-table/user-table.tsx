f5ffc705c6a346afca85bf3808fd738abba0760f
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-04 13:09:57 +0100

    usr management

--- kit/dapp/src/app/(private)/admin/users/_components/user-table/user-table.tsx
+++ kit/dapp/src/app/(private)/admin/users/_components/user-table/user-table.tsx
@@ -1,12 +1,10 @@
 import { getUsers } from '@/app/(private)/admin/users/_components/data';
 import { DataTable } from '@/components/blocks/data-table/data-table';
-import { getActiveOrganizationId } from '@/lib/auth/auth';
 import { columns } from './user-table-columns';
 import { icons } from './user-table-icons';
 
 export async function UserTable() {
-  const organizationId = await getActiveOrganizationId();
-  const users = await getUsers(organizationId);
+  const users = await getUsers();
 
   return <DataTable columns={columns} data={users} icons={icons} name="users" />;
 }
