288d91d6d83c0cd5c787eaa7097900fea2f7460b
Roderik van der Veer <roderik@settlemint.com>
2025-02-07 17:47:32 +0100

    fix

--- kit/dapp/src/components/blocks/percentage-progress/percentage-progress.tsx
+++ kit/dapp/src/components/blocks/percentage-progress/percentage-progress.tsx
@@ -1,34 +1,26 @@
 import { Progress } from '@/components/ui/progress';
-import { cn } from '@/lib/utils';
 interface PercentageProgressBarProps {
   percentage: number;
 }
 
+// Determine the color based on the percentage
+const getColor = (percent: number, alpha?: number) => {
+  if (percent < 50) {
+    return `bg-destructive${alpha ? `/${alpha}` : ''}`;
+  }
+  if (percent < 80) {
+    return `bg-warning${alpha ? `/${alpha}` : ''}`;
+  }
+  return `bg-success${alpha ? `/${alpha}` : ''}`;
+};
+
 export function PercentageProgressBar({ percentage }: PercentageProgressBarProps) {
-  // Ensure the percentage is between 0 and 100
-  const clampedPercentage = Math.min(100, Math.max(0, percentage));
-
-  // Determine the color based on the percentage
-  const getColor = (percent: number, alpha?: number) => {
-    if (percent < 50) {
-      return `bg-destructive${alpha ? `/${alpha}` : ''}`;
-    }
-    if (percent < 80) {
-      return `bg-warning${alpha ? `/${alpha}` : ''}`;
-    }
-    return `bg-success${alpha ? `/${alpha}` : ''}`;
-  };
-
   return (
     <div className="grid w-full grid-cols-4 items-center">
       <div className="col-span-3">
-        <Progress
-          value={clampedPercentage}
-          className={cn(getColor(clampedPercentage, 20))}
-          progressClassName={getColor(clampedPercentage)}
-        />
+        <Progress value={percentage} className={getColor(percentage, 20)} progressClassName={getColor(percentage)} />
       </div>
-      <div className="text-right">{clampedPercentage}%</div>
+      <div className="text-right">{percentage}%</div>
     </div>
   );
 }
