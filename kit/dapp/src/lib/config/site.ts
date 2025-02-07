dde1acfe59b30e7de143be214c7bd66d94c23a67
Roderik van der Veer <roderik@settlemint.com>
2025-02-07 08:34:20 +0100

    chore: url fallbacks

--- kit/dapp/src/lib/config/site.ts
+++ kit/dapp/src/lib/config/site.ts
@@ -14,7 +14,11 @@ interface SiteConfig {
  * The main site configuration
  */
 export const siteConfig = {
-  name: 'Asset Tokenization',
-  description: 'SettleMint Asset Tokenization Starter Kit',
-  url: process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000',
+  name: "Asset Tokenization",
+  description: "SettleMint Asset Tokenization Starter Kit",
+  url:
+    process.env.NEXT_PUBLIC_APP_URL ??
+    process.env.BETTER_AUTH_URL ??
+    process.env.NEXTAUTH_URL ??
+    "http://localhost:3000",
 } as const satisfies SiteConfig;
