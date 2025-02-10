ec5e2285d0f327b4df173460da2a781e6a067738
Snigdha Singh <snigdha920@gmail.com>
2025-02-10 19:04:13 +0100

    use PropsWithChildren

--- kit/dapp/src/app/(private)/admin/(dashboard)/_components/widgets/stat/stat-label.tsx
+++ kit/dapp/src/app/(private)/admin/(dashboard)/_components/widgets/stat/stat-label.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils';
-import type { ReactNode } from 'react';
+import type { PropsWithChildren } from 'react';
 
-export function StatLabel({ children, className }: { children: ReactNode; className?: string }) {
+export function StatLabel({ children, className }: PropsWithChildren<{ className?: string }>) {
   return <p className={cn('mt-6 text-sm', className)}>{children}</p>;
 }
--- kit/dapp/src/app/(private)/admin/(dashboard)/_components/widgets/stat/stat-subtext.tsx
+++ kit/dapp/src/app/(private)/admin/(dashboard)/_components/widgets/stat/stat-subtext.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils';
-import type { ReactNode } from 'react';
+import type { PropsWithChildren } from 'react';
 
-export function StatSubtext({ children, className }: { children: ReactNode; className?: string }) {
+export function StatSubtext({ children, className }: PropsWithChildren<{ className?: string }>) {
   return <p className={cn('text-muted-foreground text-sm', className)}>{children}</p>;
 }
