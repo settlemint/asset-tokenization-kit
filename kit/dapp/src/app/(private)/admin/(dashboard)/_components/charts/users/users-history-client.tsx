a3a4a59def854998aa6a12f049f82022476a6329
Snigdha Singh <snigdha920@gmail.com>
2025-02-11 18:55:54 +0100

    use area chart

--- kit/dapp/src/app/(private)/admin/(dashboard)/_components/charts/users/users-history-client.tsx
+++ kit/dapp/src/app/(private)/admin/(dashboard)/_components/charts/users/users-history-client.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { AreaChartComponent } from '@/components/ui/area-chart';
+import type { ChartConfig } from '@/components/ui/chart';
 import { type QueryKey, useSuspenseQuery } from '@tanstack/react-query';
 import { getRecentUsers } from './data';
 
@@ -6,13 +8,39 @@ interface UsersHistoryClientProps {
   queryKey: QueryKey;
 }
 
+export const USERS_CHART_CONFIG = {
+  desktop: {
+    label: 'Desktop',
+    color: 'hsl(var(--chart-1))',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: 'hsl(var(--chart-2))',
+  },
+} satisfies ChartConfig;
+
+const DEMO_DATA = [
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+];
+
 export function UsersHistoryClient({ queryKey }: UsersHistoryClientProps) {
   const { data } = useSuspenseQuery({
     queryKey: queryKey,
     queryFn: getRecentUsers,
     refetchInterval: 1000 * 5,
   });
-  console.log(data);
 
-  return <div>Users History</div>;
+  return (
+    <AreaChartComponent
+      data={DEMO_DATA}
+      config={USERS_CHART_CONFIG}
+      title="User Activity"
+      description="Showing total users for the last 7 days"
+      xAxis={{ key: 'month' }}
+    />
+  );
 }
---
+++ kit/dapp/src/components/ui/area-chart.tsx
@@ -0,0 +1,78 @@
+'use client';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+import type { ReactNode } from 'react';
+
+export interface AreaChartData {
+  [key: string]: string | number;
+}
+
+interface XAxisConfig {
+  key: string;
+  tickFormatter?: (value: string) => string;
+  tickMargin?: number;
+}
+
+interface AreaChartProps {
+  data: AreaChartData[];
+  config: ChartConfig;
+  title: string;
+  description?: string;
+  xAxis: XAxisConfig;
+  className?: string;
+  footer?: ReactNode;
+}
+
+const defaultTickFormatter = (value: string) => value.slice(0, 3);
+const defaultTickMargin = 8;
+
+export function AreaChartComponent({ data, config, title, description, xAxis, footer }: AreaChartProps) {
+  const dataKeys = Object.keys(config);
+  const { key, tickFormatter = defaultTickFormatter, tickMargin = defaultTickMargin } = xAxis;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        {description && <CardDescription>{description}</CardDescription>}
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={config}>
+          <AreaChart accessibilityLayer data={data}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey={key}
+              tickLine={false}
+              axisLine={false}
+              tickMargin={tickMargin}
+              tickFormatter={tickFormatter}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <defs>
+              {dataKeys.map((key) => (
+                <linearGradient key={key} id={`fill${key}`} x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor={`var(--color-${key})`} stopOpacity={0.8} />
+                  <stop offset="95%" stopColor={`var(--color-${key})`} stopOpacity={0.1} />
+                </linearGradient>
+              ))}
+            </defs>
+            {dataKeys.map((key) => (
+              <Area
+                key={key}
+                dataKey={key}
+                type="natural"
+                fill={`url(#fill${key})`}
+                fillOpacity={0.4}
+                stroke={`var(--color-${key})`}
+                stackId="a"
+              />
+            ))}
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+}
