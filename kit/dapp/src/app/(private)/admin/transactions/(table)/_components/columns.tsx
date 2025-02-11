a68c8ad2df9d1ebfcf7bcbe76e1ae7ee9697ecf5
Roderik van der Veer <roderik@settlemint.com>
2025-02-11 10:55:48 +0100

    feat: event detail sheet

--- kit/dapp/src/app/(private)/admin/transactions/(table)/_components/columns.tsx
+++ kit/dapp/src/app/(private)/admin/transactions/(table)/_components/columns.tsx
@@ -2,9 +2,14 @@
 import { DataTableColumnCell } from '@/components/blocks/data-table/data-table-column-cell';
 import { DataTableColumnHeader } from '@/components/blocks/data-table/data-table-column-header';
 import { EvmAddress } from '@/components/blocks/evm-address/evm-address';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 import { EvmAddressBalances } from '@/components/ui/evm-address-balances';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 import { createColumnHelper } from '@tanstack/react-table';
 import { Lock, PauseCircle, PlayCircle, Unlock } from 'lucide-react';
+import { Fragment } from 'react';
+import { isAddress } from 'viem';
 import type { NormalizedTransactionListItem } from './data';
 
 const columnHelper = createColumnHelper<NormalizedTransactionListItem>();
@@ -49,6 +54,66 @@ export const columns = [
       );
     },
     enableColumnFilter: true,
+  }),
+  columnHelper.display({
+    id: 'actions',
+    header: () => '',
+    cell: ({ row }) => (
+      <DataTableColumnCell>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="sm">
+              Details
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>{row.original.event}</SheetTitle>
+              <Card>
+                <CardContent className="pt-6">
+                  <dl className="grid grid-cols-[1fr_2fr] gap-4">
+                    <dt className="text-muted-foreground text-sm">Sender:</dt>
+                    <dd className="text-sm">
+                      <EvmAddress address={row.original.sender} name={row.original.senderName} />
+                    </dd>
+                    <dt className="text-muted-foreground text-sm">Asset:</dt>
+                    <dd className="text-sm">
+                      <EvmAddress
+                        address={row.original.asset}
+                        name={row.original.emitterName}
+                        symbol={row.original.emitterSymbol}
+                      />
+                    </dd>
+                    <dt className="text-muted-foreground text-sm">Date:</dt>
+                    <dd className="text-sm">{row.original.timestamp}</dd>
+                  </dl>
+                </CardContent>
+              </Card>
+              {Object.keys(row.original.details).length > 0 && (
+                <>
+                  <h2 className="font-semibold text-lg">Details</h2>
+                  <Card>
+                    <CardContent className="pt-6">
+                      <dl className="grid grid-cols-[1fr_2fr] gap-4">
+                        {Object.entries(row.original.details).map(([key, value]) => (
+                          <Fragment key={key}>
+                            <dt className="text-muted-foreground text-sm capitalize">{key}:</dt>
+                            <dd className="text-sm">{isAddress(value) ? <EvmAddress address={value} /> : value}</dd>
+                          </Fragment>
+                        ))}
+                      </dl>
+                    </CardContent>
+                  </Card>
+                </>
+              )}
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </DataTableColumnCell>
+    ),
+    meta: {
+      enableCsvExport: false,
+    },
   }),
 ];
 
