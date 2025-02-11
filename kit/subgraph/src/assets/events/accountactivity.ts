f91caf4784f8bf2aafa0182895a56033fe123c0c
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-11 10:02:39 +0100

    fix account activity

--- kit/subgraph/src/assets/events/accountactivity.ts
+++ kit/subgraph/src/assets/events/accountactivity.ts
@@ -1,41 +1,41 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { Account, AccountActivityEvent } from "../../../generated/schema";
 
-export enum AccountActivityEventName {
-  AssetCreated = "AssetCreated",
-  AssetTransferred = "AssetTransferred",
-  AssetMinted = "AssetMinted",
-  AssetBurned = "AssetBurned",
-  RoleGranted = "RoleGranted",
-  RoleRevoked = "RoleRevoked",
-  RoleAdminChanged = "RoleAdminChanged",
-  Approval = "Approval",
-  Paused = "Paused",
-  Unpaused = "Unpaused",
-  TokensFrozen = "TokensFrozen",
-  TokensUnfrozen = "TokensUnfrozen",
-  UserBlocked = "UserBlocked",
-  UserUnblocked = "UserUnblocked",
+export class AccountActivityEventName {
+  static AssetCreated: string = "AssetCreated";
+  static AssetTransferred: string = "AssetTransferred";
+  static AssetMinted: string = "AssetMinted";
+  static AssetBurned: string = "AssetBurned";
+  static RoleGranted: string = "RoleGranted";
+  static RoleRevoked: string = "RoleRevoked";
+  static RoleAdminChanged: string = "RoleAdminChanged";
+  static Approval: string = "Approval";
+  static Paused: string = "Paused";
+  static Unpaused: string = "Unpaused";
+  static TokensFrozen: string = "TokensFrozen";
+  static TokensUnfrozen: string = "TokensUnfrozen";
+  static UserBlocked: string = "UserBlocked";
+  static UserUnblocked: string = "UserUnblocked";
   // Bond specific events
-  BondMatured = "BondMatured",
-  BondRedeemed = "BondRedeemed",
+  static BondMatured: string = "BondMatured";
+  static BondRedeemed: string = "BondRedeemed";
   // Fixed yield specific events
-  YieldClaimed = "YieldClaimed",
+  static YieldClaimed: string = "YieldClaimed";
   // Fund specific events
-  ManagementFeeCollected = "ManagementFeeCollected",
-  PerformanceFeeCollected = "PerformanceFeeCollected",
-  TokenWithdrawn = "TokenWithdrawn",
+  static ManagementFeeCollected: string = "ManagementFeeCollected";
+  static PerformanceFeeCollected: string = "PerformanceFeeCollected";
+  static TokenWithdrawn: string = "TokenWithdrawn";
   // Bond and Fixed yield specific events
-  UnderlyingAssetTopUp = "UnderlyingAssetTopUp",
-  UnderlyingAssetWithdrawn = "UnderlyingAssetWithdrawn",
+  static UnderlyingAssetTopUp: string = "UnderlyingAssetTopUp";
+  static UnderlyingAssetWithdrawn: string = "UnderlyingAssetWithdrawn";
   // Stablecoin specific events
-  CollateralUpdated = "CollateralUpdated",
+  static CollateralUpdated: string = "CollateralUpdated";
 }
 
 export function accountActivityEvent(
   id: Bytes,
   account: Account,
-  eventName: AccountActivityEventName,
+  eventName: string,
   timestamp: BigInt,
   assetType: string | null = null,
   asset: Bytes | null = null
