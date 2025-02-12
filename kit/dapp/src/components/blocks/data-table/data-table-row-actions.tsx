e4bef5010cb89d6170548b6c3b4f407be450aeb4
pmualaba <patrick.mualaba@live.be>
2025-02-12 15:40:29 +0100

    fix: theming (#759)
    Co-authored-by: Roderik van der Veer <roderik@settlemint.com>

--- kit/dapp/src/components/blocks/data-table/data-table-row-actions.tsx
+++ kit/dapp/src/components/blocks/data-table/data-table-row-actions.tsx
@@ -39,7 +39,12 @@ export function DataTableRowActions({
   return (
     <div className={cn(dataTableRowActionsVariants({ variant, className }), className)} {...props}>
       {detailUrl && (
-        <Button variant="outline" size="sm" asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          asChild
+          className="hover:text-primary-foreground dark:hover:text-foreground"
+        >
           <Link href={detailUrl} prefetch>
             Details
           </Link>
@@ -48,7 +53,10 @@ export function DataTableRowActions({
       {children && (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+            <Button
+              variant="ghost"
+              className="flex h-8 w-8 p-0 hover:bg-theme-accent-background active:border-0 data-[state=open]:bg-muted dark:hover:text-foreground"
+            >
               <MoreHorizontal />
               <span className="sr-only">Open menu</span>
             </Button>
