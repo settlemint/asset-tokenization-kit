52ecc67bf2698b4f6c2c687c606521dc7e9bbd27
Silke Van den Broeck <silke@settlemint.com>
2025-02-14 14:47:12 +0100

    feat: add blocked and frozen status to the AssetBalance in subgraph

--- kit/subgraph/src/fetch/balance.ts
+++ kit/subgraph/src/fetch/balance.ts
@@ -1,5 +1,6 @@
 import { BigInt, Bytes } from '@graphprotocol/graph-ts';
 import { AssetBalance } from '../../generated/schema';
+import { Fund } from '../../generated/templates/Fund/Fund';
 import { toDecimals } from '../utils/decimals';
 
 export function fetchAssetBalance(asset: Bytes, account: Bytes, decimals: number): AssetBalance {
@@ -14,6 +15,12 @@ export function fetchAssetBalance(asset: Bytes, account: Bytes, decimals: number
     balance.value = toDecimals(balance.valueExact, decimals);
     balance.approvedExact = BigInt.zero();
     balance.approved = toDecimals(balance.approvedExact, decimals);
+
+    // Get blocklist and frozen status directly from the Fund contract
+    const fund = Fund.bind(asset);
+    balance.blocked = fund.blocked(account);
+    balance.frozen = fund.frozen(account);
+
     balance.save();
   }
 
