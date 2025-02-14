df1d8380eb4a952c9605a673cad59a139c7ff52e
Roderik van der Veer <roderik@settlemint.com>
2025-02-14 15:04:53 +0100

    fix: more comlumns on large screens

--- kit/dapp/src/components/blocks/detail-grid/detail-grid.tsx
+++ kit/dapp/src/components/blocks/detail-grid/detail-grid.tsx
@@ -4,7 +4,9 @@ import type { PropsWithChildren } from 'react';
 export function DetailGrid({ children }: PropsWithChildren) {
   return (
     <Card className="py-4">
-      <CardContent className="grid grid-cols-4 gap-x-4 gap-y-8">{children}</CardContent>
+      <CardContent className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {children}
+      </CardContent>
     </Card>
   );
 }
