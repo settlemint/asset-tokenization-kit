35b97e60ad1a639ee1090cdb037586845da730b4
Roderik van der Veer <roderik@settlemint.com>
2025-02-16 09:20:42 +0100

    fix

--- kit/dapp/src/components/blocks/asset-form/inputs/asset-form-users.tsx
+++ kit/dapp/src/components/blocks/asset-form/inputs/asset-form-users.tsx
@@ -112,6 +112,7 @@ function AssetFormUsersList({
 
   const { data, isLoading } = useQuery({
     queryKey: [`search-users-${debounced}`],
+    enabled: debounced.trim() !== '',
     queryFn: async () => {
       const sanitizedSearch = sanitizeSearchTerm(debounced);
 
