0dc4c99f2f719615bd73506e6ee33310f5dd5291
Snigdha Singh <snigdha920@gmail.com>
2025-02-17 18:28:51 +0100

    open empty sheet from transfer button

--- kit/dapp/src/app/(private)/portfolio/(dashboard)/_components/my-assets/my-assets-client.tsx
+++ kit/dapp/src/app/(private)/portfolio/(dashboard)/_components/my-assets/my-assets-client.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import { getMyAssets } from '@/app/(private)/portfolio/_components/my-assets/data';
 import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 import { type QueryKey, useSuspenseQuery } from '@tanstack/react-query';
 import { ArrowUpFromLine } from 'lucide-react';
-import { getMyAssets } from '../../../_components/my-assets/data';
 
 interface MyAssetsClientProps {
   queryKey: QueryKey;
@@ -24,9 +25,20 @@ export function MyAssetsClient({ queryKey }: MyAssetsClientProps) {
         <span className="mr-1 font-bold text-4xl">{totalValue}</span>
         <span>assets</span>
       </div>
-      <Button className="w-1/6">
-        <ArrowUpFromLine /> Transfer
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="w-1/6">
+            <ArrowUpFromLine className="mr-2 h-4 w-4" />
+            Transfer
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="min-w-[34rem]">
+          <SheetHeader>
+            <SheetTitle>Transfer Assets</SheetTitle>
+            <SheetDescription>Transfer your assets to another wallet address.</SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 }
