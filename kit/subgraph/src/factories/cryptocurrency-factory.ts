23f14b37db131da199e54ec1461d687a0dfa186d
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-17 17:32:47 +0100

    fix

--- kit/subgraph/src/factories/cryptocurrency-factory.ts
+++ kit/subgraph/src/factories/cryptocurrency-factory.ts
@@ -8,6 +8,7 @@ import { AssetType, EventName, FactoryType } from '../utils/enums';
 import { eventId } from '../utils/events';
 import { fetchFactory } from './fetch/factory';
 import { fetchAssetCount } from '../assets/fetch/asset-count';
+
 export function handleCryptoCurrencyCreated(event: CryptoCurrencyCreated): void {
   fetchFactory(event.address, FactoryType.cryptocurrency);
   const sender = fetchAccount(event.transaction.from);
