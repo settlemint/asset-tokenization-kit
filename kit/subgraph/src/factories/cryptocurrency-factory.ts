2a55a9fa9a62b711975caaf33ac0f2e51c96c8cf
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-17 17:32:00 +0100

    feat: index total asset count

--- kit/subgraph/src/factories/cryptocurrency-factory.ts
+++ kit/subgraph/src/factories/cryptocurrency-factory.ts
@@ -7,13 +7,17 @@ import { fetchAccount } from '../fetch/account';
 import { AssetType, EventName, FactoryType } from '../utils/enums';
 import { eventId } from '../utils/events';
 import { fetchFactory } from './fetch/factory';
-
+import { fetchAssetCount } from '../assets/fetch/asset-count';
 export function handleCryptoCurrencyCreated(event: CryptoCurrencyCreated): void {
   fetchFactory(event.address, FactoryType.cryptocurrency);
   const sender = fetchAccount(event.transaction.from);
   const asset = fetchCryptoCurrency(event.params.token);
   asset.creator = sender.id;
   asset.save();
+
+  const assetCount = fetchAssetCount(AssetType.cryptocurrency);
+  assetCount.count = assetCount.count + 1;
+  assetCount.save();
 
   assetCreatedEvent(eventId(event), event.block.timestamp, asset.id, sender.id);
   accountActivityEvent(sender, EventName.AssetCreated, event.block.timestamp, AssetType.cryptocurrency, asset.id);
