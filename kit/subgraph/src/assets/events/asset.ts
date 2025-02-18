39270291e3becfbe4e30b88d330601132c9ffb1d
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-18 16:29:06 +0100

    track account asset events

--- kit/subgraph/src/assets/events/asset.ts
+++ kit/subgraph/src/assets/events/asset.ts
@@ -1,5 +1,6 @@
 import { BigInt, Bytes } from '@graphprotocol/graph-ts';
 import { AssetEvent } from '../../../generated/schema';
+import { fetchAccount } from '../../fetch/account';
 
 export function assetEvent(
   id: Bytes,
@@ -14,5 +15,10 @@ export function assetEvent(
   assetEvent.emitter = emitter;
   assetEvent.sender = sender;
   assetEvent.save();
+
+  const account = fetchAccount(sender);
+  account.assetEventsCount = account.assetEventsCount + 1;
+  account.save();
+
   return assetEvent;
 }
