4333c4f769a04a3cb614443488fe5422b575f9a3
Roderik van der Veer <roderik@settlemint.com>
2025-02-18 17:29:10 +0100

    feat: improved homepage

--- kit/dapp/src/app/layout.tsx
+++ kit/dapp/src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             enableColorScheme
-            disableTransitionOnChange
             enableSystem
             value={{
               light: themeConfig.variant === 'settlemint' ? 'settlemint-light' : 'light',
