897cff761f109c3ab682f4b821d9e5f7ab21a838
Snigdha Singh <snigdha920@gmail.com>
2025-02-19 15:20:41 +0100

    logo home section link

--- kit/dapp/src/components/layout/nav-header.tsx
+++ kit/dapp/src/components/layout/nav-header.tsx
@@ -1,25 +1,36 @@
 'use client';
 
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from '@/components/ui/sidebar';
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { Logo } from '../blocks/logo/logo';
 
 export function NavHeader() {
-  const { state } = useSidebar();
+  const pathname = usePathname();
+  const content = (
+    <div className={cn('flex w-full items-center gap-3')}>
+      <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
+        <Logo variant="icon" />
+      </div>
+      <div className="mb-2 flex flex-col gap-0.5 leading-none">
+        <span className="font-bold text-lg">SettleMint</span>
+        <span className="-mt-1 text-md">Asset Tokenization</span>
+      </div>
+    </div>
+  );
 
   return (
-    <SidebarMenu className="">
+    <SidebarMenu>
       <SidebarMenuItem>
         <SidebarMenuButton size="lg" asChild>
-          <div className={cn(state === 'expanded' ? '-mt-2' : 'mt-0 ml-1')}>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
-              <Logo variant="icon" />
-            </div>
-            <div className="flex flex-col gap-0.5 leading-none">
-              <span className="font-bold text-lg">SettleMint</span>
-              <span className="-mt-1 text-md">Asset Tokenization</span>
-            </div>
-          </div>
+          {pathname.includes('/admin') ? (
+            <Link href="/admin">{content}</Link>
+          ) : pathname.includes('/portfolio') ? (
+            <Link href="/portfolio">{content}</Link>
+          ) : (
+            content
+          )}
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
