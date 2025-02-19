1aa2c51cec1d9e6b5669fdeab3683b81c0328444
Patrick Mualaba <patrick.mualaba@live.be>
2025-02-19 13:18:20 +0100

    feat: prettify charts

--- kit/dapp/src/components/blocks/charts/pie-chart.tsx
+++ kit/dapp/src/components/blocks/charts/pie-chart.tsx
@@ -52,7 +52,17 @@ export function PieChartComponent({
                 </radialGradient>
               ))}
             </defs>
-            <Pie data={data} dataKey={dataKey} nameKey={nameKey} strokeWidth={1.5}>
+            <Pie
+              data={data}
+              dataKey={dataKey}
+              nameKey={nameKey}
+              cx="50%"
+              cy="50%"
+              innerRadius={0}
+              outerRadius="80%"
+              fill="#8884d8"
+              strokeWidth={1.5}
+            >
               {data.map((entry, index) => (
                 <Cell
                   key={`cell-${index}`}
