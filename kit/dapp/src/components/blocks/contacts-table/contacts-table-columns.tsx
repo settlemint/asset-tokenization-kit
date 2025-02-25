3d4af9b42e61c50b726604896ae1f1f44b4dd3b5
Patrick Mualaba <patrick.mualaba@live.be>
2025-02-25 21:34:58 +0100

    add contact mutation

--- kit/dapp/src/components/blocks/contacts-table/contacts-table-columns.tsx
+++ kit/dapp/src/components/blocks/contacts-table/contacts-table-columns.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { AddressAvatar } from '@/components/blocks/address-avatar/address-avatar';
 import { DataTableColumnCell } from '@/components/blocks/data-table/data-table-column-cell';
 import { DataTableColumnHeader } from '@/components/blocks/data-table/data-table-column-header';
+import { EvmAddress } from '@/components/blocks/evm-address/evm-address';
+import { CopyToClipboard } from '@/components/ui/copy';
+import { EvmAddressBalances } from '@/components/ui/evm-address-balances';
+import { Skeleton } from '@/components/ui/skeleton';
 import { formatDate } from '@/lib/date';
 import { createColumnHelper } from '@tanstack/react-table';
 import { CheckCircle, Clock, XCircle } from 'lucide-react';
+import { Suspense } from 'react';
+import type { Address } from 'viem';
 import type { ContactsListItem } from './contacts-table-data';
 import { ContactDetailSheet } from './contacts-table-detail-sheet';
 
@@ -13,11 +20,36 @@ const columnHelper = createColumnHelper<ContactsListItem>();
 export const columns = [
   columnHelper.accessor('name', {
     header: ({ column }) => <DataTableColumnHeader column={column}>Name</DataTableColumnHeader>,
-    cell: ({ getValue }) => <DataTableColumnCell>{getValue()}</DataTableColumnCell>,
+    cell: ({ renderValue, row }) => (
+      <DataTableColumnCell>
+        <Suspense fallback={<Skeleton className="h-8 w-8 rounded-lg" />}>
+          <AddressAvatar
+            email={undefined}
+            address={row.original.wallet as Address}
+            imageUrl={undefined}
+            variant="small"
+          />
+        </Suspense>
+        <span>{renderValue()}</span>
+      </DataTableColumnCell>
+    ),
+    enableColumnFilter: false,
   }),
   columnHelper.accessor('wallet', {
     header: ({ column }) => <DataTableColumnHeader column={column}>Wallet</DataTableColumnHeader>,
-    cell: ({ getValue }) => <DataTableColumnCell>{getValue()}</DataTableColumnCell>,
+    cell: ({ getValue }) => (
+      <DataTableColumnCell>
+        {getValue() && (
+          <div className="flex items-center">
+            <EvmAddress address={getValue() as Address} prettyNames={false}>
+              <EvmAddressBalances address={getValue() as Address} />
+            </EvmAddress>
+            <CopyToClipboard value={getValue()} displayText={''} className="ml-2" />
+          </div>
+        )}
+      </DataTableColumnCell>
+    ),
+    enableColumnFilter: false,
   }),
   columnHelper.accessor('created_at', {
     header: ({ column }) => <DataTableColumnHeader column={column}>Created At</DataTableColumnHeader>,
