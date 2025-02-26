9117da5e6450f01c250f81fb6b08e07701c75edb
Silke Van den Broeck <silke@settlemint.com>
2025-02-26 09:08:24 +0100

    fix: type error

--- kit/dapp/src/app/(private)/portfolio/my-assets/bond/[id]/(details)/_components/redeem-form/store.ts
+++ kit/dapp/src/app/(private)/portfolio/my-assets/bond/[id]/(details)/_components/redeem-form/store.ts
@@ -6,7 +6,7 @@ import { type Address, parseUnits } from 'viem';
 import { RedeemOutputSchema, getRedeemFormSchema } from './schema';
 
 const BondRedeemMutation = portalGraphql(`
-mutation BondRedeem($address: String!, $from: String!, $challengeResponse: String!, $input: { amount: String! }) {
+mutation BondRedeem($address: String!, $from: String!, $challengeResponse: String!, $input: BondRedeemInput!) {
   BondRedeem(
     address: $address
     from: $from
@@ -18,18 +18,24 @@ mutation BondRedeem($address: String!, $from: String!, $challengeResponse: Strin
 }
 `);
 
+interface BondRedeemResponse {
+  BondRedeem: {
+    transactionHash: string;
+  } | null;
+}
+
 export const redeemBond = actionClient
   .schema(getRedeemFormSchema())
   .outputSchema(RedeemOutputSchema)
   .action(async ({ parsedInput: { address, amount, pincode, decimals }, ctx: { user } }) => {
-    const data = await portalClient.request(BondRedeemMutation, {
+    const data = (await portalClient.request(BondRedeemMutation, {
       address: address,
       from: user.wallet,
       challengeResponse: await handleChallenge(user.wallet as Address, pincode),
       input: {
         amount: parseUnits(amount.toString(), decimals).toString(),
       },
-    });
+    })) as BondRedeemResponse;
 
     const transactionHash = data.BondRedeem?.transactionHash;
     if (!transactionHash) {
