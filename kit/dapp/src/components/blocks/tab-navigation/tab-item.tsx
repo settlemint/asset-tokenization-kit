1d858403c6e95affd5bf3c6debd228bcb3d8b3f0
Roderik van der Veer <roderik@settlemint.com>
2025-02-27 23:21:20 +0100

    wip

--- kit/dapp/src/components/blocks/tab-navigation/tab-item.tsx
+++ kit/dapp/src/components/blocks/tab-navigation/tab-item.tsx
@@ -4,7 +4,6 @@ import {
   NavigationMenuLink,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
-import { Link } from '@/i18n/routing';
 import { cn } from '@/lib/utils';
 import type { FC } from 'react';
 
@@ -17,21 +16,20 @@ export interface TabItemProps {
 
 export const TabItem: FC<TabItemProps> = ({ href, name, badge, active }) => (
   <NavigationMenuItem>
-    <Link href={href}>
-      <NavigationMenuLink
-        className={cn(
-          navigationMenuTriggerStyle(),
-          '!bg-transparent relative rounded-none pb-4 text-md after:absolute after:bottom-0 after:left-0 after:h-0.5 after:bg-primary hover:text-foreground hover:after:w-full hover:after:transition-[width] hover:after:duration-200 hover:after:ease-in-out',
-          active ? '!text-primary font-bold after:w-full' : 'after:w-0'
-        )}
-      >
-        {name}
-        {badge !== undefined && (
-          <Badge variant="outline" className="ml-2 border-card">
-            {badge}
-          </Badge>
-        )}
-      </NavigationMenuLink>
-    </Link>
+    <NavigationMenuLink
+      href={href}
+      className={cn(
+        navigationMenuTriggerStyle(),
+        '!bg-transparent relative rounded-none pb-4 text-md after:absolute after:bottom-0 after:left-0 after:h-0.5 after:bg-primary hover:text-foreground hover:after:w-full hover:after:transition-[width] hover:after:duration-200 hover:after:ease-in-out',
+        active ? '!text-primary font-bold after:w-full' : 'after:w-0'
+      )}
+    >
+      {name}
+      {badge !== undefined && (
+        <Badge variant="outline" className="ml-2 border-card">
+          {badge}
+        </Badge>
+      )}
+    </NavigationMenuLink>
   </NavigationMenuItem>
 );
