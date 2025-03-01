98da8e36056c5489aa2ba009c3c9ec3c87965eee
Roderik van der Veer <roderik@settlemint.com>
2025-03-01 18:16:03 +0100

    fi

--- kit/subgraph/src/assets/calculations/collateral.ts
+++ kit/subgraph/src/assets/calculations/collateral.ts
@@ -1,7 +1,7 @@
 import { BigDecimal } from "@graphprotocol/graph-ts";
 import { StableCoin } from "../../../generated/schema";
 
-export function collateralCalculatedFields(stableCoin: StableCoin) {
+export function collateralCalculatedFields(stableCoin: StableCoin): StableCoin {
   const ratio = stableCoin.totalSupply.equals(BigDecimal.zero())
     ? BigDecimal.fromString("100")
     : stableCoin.collateral.div(stableCoin.totalSupply);
@@ -13,4 +13,5 @@ export function collateralCalculatedFields(stableCoin: StableCoin) {
   );
   stableCoin.freeCollateral = freeCollateral;
   stableCoin.freeCollateralExact = freeCollateralExact;
+  return stableCoin;
 }
