117338c9c42b7d9814cc4c124ee79cfcd6803c89
Roderik van der Veer <roderik@settlemint.com>
2025-03-01 18:20:26 +0100

    feat: move collateral calculations to the subgraph (#932)
    ## Summary by Sourcery
    
    Move collateral calculations to the subgraph to improve data consistency
    and reduce redundancy. This includes adding free collateral fields to
    the schema and updating the collateral ratio calculation.
    
    Enhancements:
    - Move collateral calculation logic into a dedicated function to improve
    code reuse and readability.
    - Add free collateral fields to the StableCoin schema to track the
    amount of collateral available after accounting for the total supply.
    - Update collateral ratio calculation to handle edge cases where the
    total supply is zero.

---
+++ kit/subgraph/src/assets/calculations/collateral.ts
@@ -0,0 +1,17 @@
+import { BigDecimal } from "@graphprotocol/graph-ts";
+import { StableCoin } from "../../../generated/schema";
+
+export function collateralCalculatedFields(stableCoin: StableCoin): StableCoin {
+  const ratio = stableCoin.totalSupply.equals(BigDecimal.zero())
+    ? BigDecimal.fromString("100")
+    : stableCoin.collateral.div(stableCoin.totalSupply);
+  stableCoin.collateralRatio = ratio;
+
+  const freeCollateral = stableCoin.collateral.minus(stableCoin.totalSupply);
+  const freeCollateralExact = stableCoin.collateralExact.minus(
+    stableCoin.totalSupplyExact
+  );
+  stableCoin.freeCollateral = freeCollateral;
+  stableCoin.freeCollateralExact = freeCollateralExact;
+  return stableCoin;
+}
