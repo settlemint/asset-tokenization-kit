b5417598f2635a19d37cfd819a35c5ed59926df0
Snigdha Singh <snigdha920@gmail.com>
2025-03-02 10:07:17 +0100

    update get assets widget

--- kit/dapp/src/app/[locale]/(private)/admin/(dashboard)/_components/widgets/assets.tsx
+++ kit/dapp/src/app/[locale]/(private)/admin/(dashboard)/_components/widgets/assets.tsx
@@ -1,29 +1,33 @@
-export function AssetsWidget() {
-  // const t = useTranslations('admin.dashboard.widgets');
-  // const { data } = useAssetActivity();
-  // const allAssetsSupply = data.reduce(
-  //   (acc, asset) => acc + asset.totalSupply,
-  //   0n
-  // );
+import { getAssetActivity } from '@/lib/queries/asset-activity/asset-activity';
+import { formatNumber } from '@/lib/utils/number';
+import { getTranslations } from 'next-intl/server';
+import { Widget } from './widget';
 
-  // const getAssetSupply = (assetType: (typeof data)[number]['assetType']) => {
-  //   return (
-  //     data.find((asset) => asset.assetType === assetType)?.totalSupply || 0
-  //   );
-  // };
+export async function AssetsWidget() {
+  const t = await getTranslations('admin.dashboard.widgets');
+  const data = await getAssetActivity();
+  const allAssetsSupply = data.reduce(
+    (acc, asset) => acc + asset.totalSupply,
+    0n
+  );
+
+  const getAssetSupply = (assetType: (typeof data)[number]['assetType']) => {
+    return (
+      data.find((asset) => asset.assetType === assetType)?.totalSupply || 0
+    );
+  };
 
   return (
-    // <Widget
-    //   label={t('assets.label')}
-    //   value={formatNumber(allAssetsSupply)}
-    //   subtext={t('assets.subtext', {
-    //     stableCoins: formatNumber(getAssetSupply('stablecoin')),
-    //     bonds: formatNumber(getAssetSupply('bond')),
-    //     cryptocurrencies: formatNumber(getAssetSupply('cryptocurrency')),
-    //     equities: formatNumber(getAssetSupply('equity')),
-    //     funds: formatNumber(getAssetSupply('fund')),
-    //   })}
-    // />
-    <div>Assets</div>
+    <Widget
+      label={t('assets.label')}
+      value={formatNumber(allAssetsSupply)}
+      subtext={t('assets.subtext', {
+        stableCoins: formatNumber(getAssetSupply('stablecoin')),
+        bonds: formatNumber(getAssetSupply('bond')),
+        cryptocurrencies: formatNumber(getAssetSupply('cryptocurrency')),
+        equities: formatNumber(getAssetSupply('equity')),
+        funds: formatNumber(getAssetSupply('fund')),
+      })}
+    />
   );
 }
