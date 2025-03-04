136ac5436e3f77981cdd15ff922d6dfcabc0fb80
Roderik van der Veer <roderik@settlemint.com>
2025-03-04 15:18:29 +0100

    feat: related items on stablecoin page

--- kit/dapp/src/app/[locale]/(private)/admin/stablecoins/_components/create-form/form.tsx
+++ kit/dapp/src/app/[locale]/(private)/admin/stablecoins/_components/create-form/form.tsx
@@ -6,27 +6,36 @@ import { createStablecoin } from "@/lib/mutations/stablecoin/create/create-actio
 import { CreateStablecoinSchema } from "@/lib/mutations/stablecoin/create/create-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
 import { Basics } from "./steps/basics";
 import { Configuration } from "./steps/configuration";
 import { Summary } from "./steps/summary";
 
 interface CreateStablecoinFormProps {
-  open: boolean;
-  onCloseAction: () => void;
+  open?: boolean;
+  onCloseAction?: () => void;
+  asButton?: boolean;
 }
 
 export function CreateStablecoinForm({
   open,
   onCloseAction,
+  asButton = false,
 }: CreateStablecoinFormProps) {
   const t = useTranslations("admin.stablecoins.create-form");
+  const [localOpen, setLocalOpen] = useState(false);
 
   return (
     <FormSheet
-      open={open}
-      onOpenChange={onCloseAction}
+      open={open || localOpen}
+      onOpenChange={(open) => {
+        onCloseAction?.();
+        setLocalOpen(open);
+      }}
       title={t("title")}
       description={t("description")}
+      asButton={asButton}
+      triggerLabel={t("trigger-label")}
     >
       <Form
         action={createStablecoin}
