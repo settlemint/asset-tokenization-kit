7414e51a4491ff7ce9d6d0ae0077b6c7245c461d
Snigdha Singh <snigdha920@gmail.com>
2025-03-09 21:07:22 +0100

    cleanup

--- kit/dapp/src/lib/queries/bond-factory/address-available.tsx
+++ kit/dapp/src/lib/queries/bond-factory/address-available.tsx
@@ -37,6 +37,5 @@ export const isAddressAvailable = cache(async (address: Address) => {
     data,
     "bond factory"
   );
-  console.log({ isAddressDeployed });
   return !isAddressDeployed.BondFactory.isAddressDeployed;
 });
