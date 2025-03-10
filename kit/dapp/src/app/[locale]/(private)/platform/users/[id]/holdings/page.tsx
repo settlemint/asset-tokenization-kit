dd00b2266e632f4b96d40389f4dcce7d8fcc72a3
Jan Bevers <12234016+janb87@users.noreply.github.com>
2025-03-10 17:41:39 +0100

    feat: user holding tab (#1037)
    <img width="1780" alt="Screenshot 2025-03-10 at 16 48 42"
    src="https://github.com/user-attachments/assets/17aecb5b-a163-44f8-8f1a-a31ecbd31360"
    />

---
+++ kit/dapp/src/app/[locale]/(private)/platform/users/[id]/holdings/page.tsx
@@ -0,0 +1,20 @@
+import UserAssetsTable from "@/components/blocks/user-assets-table/user-assets-table";
+import { getUserDetail } from "@/lib/queries/user/user-detail";
+import { getTranslations } from "next-intl/server";
+
+interface UserHoldingsPageProps {
+  params: Promise<{ locale: string; id: string }>;
+}
+
+export default async function UserHoldingsPage({
+  params,
+}: UserHoldingsPageProps) {
+  const { id, locale } = await params;
+  const t = await getTranslations({
+    locale,
+    namespace: "admin.users.holdings",
+  });
+  const user = await getUserDetail({ id });
+
+  return <UserAssetsTable wallet={user.wallet} title={t("title")} />;
+}
--- kit/dapp/src/app/[locale]/(private)/portfolio/(dashboard)/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/portfolio/(dashboard)/page.tsx
@@ -1,12 +1,12 @@
 import { getUser } from "@/lib/auth/utils";
-import { getMyAssetsBalance } from "@/lib/queries/asset-balance/asset-balance-my";
+import { geUserAssetsBalance } from "@/lib/queries/asset-balance/asset-balance-user";
 import type { Address } from "viem";
 import { Greeting } from "./_components/greeting/greeting";
 import { MyAssetsHeader } from "./_components/header/my-assets-header";
 
 export default async function PortfolioDashboard() {
   const user = await getUser();
-  const myAssetsBalance = await getMyAssetsBalance(user.wallet as Address);
+  const myAssetsBalance = await geUserAssetsBalance(user.wallet as Address);
   return (
     <>
       <div className="space-y-4">
