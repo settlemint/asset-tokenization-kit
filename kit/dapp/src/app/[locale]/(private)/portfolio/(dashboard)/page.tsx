64c38b2ea72dabb5fd6f1a19a97af8f9571eb1a1
Patrick Mualaba <patrick.mualaba@live.be>
2025-03-10 22:18:49 +0100

    fix: portfolio dashboard

--- kit/dapp/src/app/[locale]/(private)/portfolio/(dashboard)/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/portfolio/(dashboard)/page.tsx
@@ -1,12 +1,24 @@
 import { TransactionsHistory } from "@/components/blocks/transactions-table/transactions-history";
+import { PageHeader } from "@/components/layout/page-header";
 import { getUser } from "@/lib/auth/utils";
 import { geUserAssetsBalance } from "@/lib/queries/asset-balance/asset-balance-user";
 import { getTransactionsHistory } from "@/lib/queries/transactions/transactions-history";
+import { getTranslations } from "next-intl/server";
 import type { Address } from "viem";
 import { Greeting } from "./_components/greeting/greeting";
 import { MyAssetsHeader } from "./_components/header/my-assets-header";
 
-export default async function PortfolioDashboard() {
+export default async function PortfolioDashboard({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  const t = await getTranslations({
+    locale,
+    namespace: "portfolio",
+  });
+
   const user = await getUser();
   const myAssetsBalance = await geUserAssetsBalance(user.wallet as Address);
 
@@ -17,11 +29,18 @@ export default async function PortfolioDashboard() {
 
   return (
     <>
-      <div className="space-y-4">
+      <PageHeader
+        title={t("dashboard.page-title")}
+        section={t("dashboard.portfolio-management")}
+      />
+      <div className="grid grid-cols-1 gap-4 divide-x-0 divide-y lg:divide-x lg:divide-y-0">
         <Greeting />
         <MyAssetsHeader data={myAssetsBalance} />
         <TransactionsHistory
-          data={data}
+          data={data.map((tx) => ({
+            timestamp: tx.createdAt.toISOString(),
+            transaction: 1,
+          }))}
           chartOptions={{
             intervalType: "month",
             intervalLength: 1,
