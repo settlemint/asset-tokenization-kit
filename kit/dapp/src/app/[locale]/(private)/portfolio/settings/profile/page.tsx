e129328f4b49c3779de08d197ae77b03c78fe98b
Roderik van der Veer <roderik@settlemint.com>
2025-03-11 19:46:44 +0100

    fix: remove the params

--- kit/dapp/src/app/[locale]/(private)/portfolio/settings/profile/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/portfolio/settings/profile/page.tsx
@@ -41,7 +41,7 @@ export default async function SecuritySettingsPage() {
           }}
         />
         <div className="p-6 py-4 md:py-3 grid grid-cols-2 gap-4 bg-transparent border-none [&>*:first-child]:justify-self-start [&>*:last-child]:justify-self-end">
-          Passkeys go here
+          {/* <PasskeysCard /> */}
         </div>
       </div>
       <DeleteAccountCard
