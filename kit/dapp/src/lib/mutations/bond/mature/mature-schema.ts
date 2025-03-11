2faaaf3c1557c5aaf0025727e7a14887461f72e8
Silke Van den Broeck <silke@settlemint.com>
2025-03-11 15:28:41 +0100

    feat: add basic mature form without check

---
+++ kit/dapp/src/lib/mutations/bond/mature/mature-schema.ts
@@ -0,0 +1,8 @@
+import { type ZodInfer, z } from "@/lib/utils/zod";
+
+export const MatureFormSchema = z.object({
+  address: z.address(),
+  pincode: z.pincode(),
+});
+
+export type MatureFormInput = ZodInfer<typeof MatureFormSchema>;
