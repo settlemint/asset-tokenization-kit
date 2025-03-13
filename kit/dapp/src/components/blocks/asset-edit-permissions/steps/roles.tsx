20ffaa246c288909b03aae7acbf38e6ba5a6fa9e
Snigdha Singh <snigdha920@gmail.com>
2025-03-13 18:34:29 +0100

    dont use raw string

--- kit/dapp/src/components/blocks/asset-edit-permissions/steps/roles.tsx
+++ kit/dapp/src/components/blocks/asset-edit-permissions/steps/roles.tsx
@@ -23,69 +23,62 @@ export function Roles({ adminsCount }: RolesProps) {
 
   return (
     <FormStep title={t("title")} description={t("description")}>
-      <div className="space-y-6">
-        <div className="space-y-4">
-          <FormField
-            control={control}
-            name="roles"
-            render={() => (
-              <FormItem>
-                <div className="space-y-3">
-                  {roleItems.map((role) => {
-                    const info = ROLES[role];
-                    return (
-                      <FormField
-                        key={role}
-                        control={control}
-                        name="roles"
-                        disabled={
-                          adminsCount === 1 && role === "DEFAULT_ADMIN_ROLE"
-                        }
-                        render={({ field }) => (
-                          <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md">
-                            <FormControl>
-                              <Checkbox
-                                name="roles"
-                                checked={
-                                  field.value?.[info.contractRole] ?? false
-                                }
-                                disabled={
-                                  adminsCount === 1 &&
-                                  role === "DEFAULT_ADMIN_ROLE"
-                                }
-                                onCheckedChange={(checked) => {
-                                  const currentValue = field.value || {};
-                                  const updatedValue = checked
-                                    ? {
-                                        ...currentValue,
-                                        [info.contractRole]: true,
-                                      }
-                                    : {
-                                        ...currentValue,
-                                        [info.contractRole]: false,
-                                      };
+      <FormField
+        control={control}
+        name="roles"
+        render={() => (
+          <FormItem>
+            <div className="space-y-3">
+              {roleItems.map((role) => {
+                const info = ROLES[role];
+                return (
+                  <FormField
+                    key={role}
+                    control={control}
+                    name="roles"
+                    disabled={
+                      adminsCount === 1 &&
+                      role === ROLES.DEFAULT_ADMIN_ROLE.contractRole
+                    }
+                    render={({ field }) => (
+                      <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md">
+                        <FormControl>
+                          <Checkbox
+                            name="roles"
+                            checked={field.value?.[info.contractRole] ?? false}
+                            disabled={
+                              adminsCount === 1 &&
+                              role === ROLES.DEFAULT_ADMIN_ROLE.contractRole
+                            }
+                            onCheckedChange={(checked) => {
+                              const currentValue = field.value || {};
+                              const updatedValue = checked
+                                ? {
+                                    ...currentValue,
+                                    [info.contractRole]: true,
+                                  }
+                                : {
+                                    ...currentValue,
+                                    [info.contractRole]: false,
+                                  };
 
-                                  field.onChange(updatedValue);
-                                }}
-                              />
-                            </FormControl>
-                            <div className="space-y-1 leading-none">
-                              <FormLabel>{info.displayName}</FormLabel>
-                              <FormDescription>
-                                {info.description}
-                              </FormDescription>
-                            </div>
-                          </FormItem>
-                        )}
-                      />
-                    );
-                  })}
-                </div>
-              </FormItem>
-            )}
-          />
-        </div>
-      </div>
+                              field.onChange(updatedValue);
+                            }}
+                          />
+                        </FormControl>
+                        <div className="space-y-1 leading-none">
+                          <FormLabel>{info.displayName}</FormLabel>
+                          <FormDescription>{info.description}</FormDescription>
+                        </div>
+                      </FormItem>
+                    )}
+                  />
+                );
+              })}
+            </div>
+          </FormItem>
+        )}
+      />
     </FormStep>
   );
 }
