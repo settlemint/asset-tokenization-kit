8dfe21e3e64d61c604e1451f54900c016acf84c0
Jan Bevers <12234016+janb87@users.noreply.github.com>
2025-03-13 13:08:48 +0100

    fix: reduce memory usage of next image (#1091)
    ## Summary by Sourcery
    
    Bug Fixes:
    - Reduce memory usage by switching image formats from PNG to WebP and
    specifying image dimensions and quality.

--- kit/dapp/src/app/[locale]/(home)/_components/hero.tsx
+++ kit/dapp/src/app/[locale]/(home)/_components/hero.tsx
@@ -15,8 +15,8 @@ import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
 import Image, { type StaticImageData } from "next/image";
 import * as React from "react";
-import HeroDark from "./assets/hero-dark.png";
-import HeroLight from "./assets/hero-light.png";
+import HeroDark from "./assets/hero-dark.webp";
+import HeroLight from "./assets/hero-light.webp";
 
 interface HeroSectionProps extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
@@ -75,7 +75,9 @@ const ThemeImage = React.memo(function ThemeImage({
           priority
           placeholder="blur"
           alt="Dashboard preview"
-          fill
+          width={1050}
+          height={674}
+          quality={75}
         />
         <Image
           src={dark}
@@ -83,7 +85,9 @@ const ThemeImage = React.memo(function ThemeImage({
           alt="Dashboard preview"
           priority
           placeholder="blur"
-          fill
+          width={1050}
+          height={674}
+          quality={75}
         />
       </div>
     );
