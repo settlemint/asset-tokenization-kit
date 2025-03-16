85f457da9034b5f338c0413ab8c3e01405928d49
Roderik van der Veer <roderik@settlemint.com>
2025-03-16 14:17:06 +0100

    ff

--- kit/contracts/ignition/modules/main.ts
+++ kit/contracts/ignition/modules/main.ts
@@ -1,22 +1,24 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import BondsModule from "./bonds";
-import CryptoCurrenciesModule from "./crypto-currencies";
-import EquitiesModule from "./equities";
-import FixedYieldFactoryModule from "./fixed-yield-factory";
-import ForwarderModule from "./forwarder";
-import FundsModule from "./funds";
-import StableCoinsModule from "./stable-coins";
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+import BondsModule from './bonds';
+import CryptoCurrenciesModule from './crypto-currencies';
+import EquitiesModule from './equities';
+import FixedYieldFactoryModule from './fixed-yield-factory';
+import ForwarderModule from './forwarder';
+import FundsModule from './funds';
+import StableCoinsModule from './stable-coins';
+import TokenizedDepositsModule from './tokenized-deposits';
 
-const AssetTokenizationModule = buildModule("AssetTokenizationModule", (m) => {
+const AssetTokenizationModule = buildModule('AssetTokenizationModule', (m) => {
   m.useModule(ForwarderModule);
   const { ustb } = m.useModule(BondsModule);
   const { btc } = m.useModule(CryptoCurrenciesModule);
   const { usdc } = m.useModule(StableCoinsModule);
   const { aapl } = m.useModule(EquitiesModule);
   const { gmf } = m.useModule(FundsModule);
+  const { tEUR } = m.useModule(TokenizedDepositsModule);
   const { fixedYieldFactory } = m.useModule(FixedYieldFactoryModule);
 
-  return { ustb, btc, usdc, aapl, gmf, fixedYieldFactory };
+  return { ustb, btc, usdc, aapl, gmf, tEUR, fixedYieldFactory };
 });
 
 export default AssetTokenizationModule;
--- kit/subgraph/graph-remote-deploy.sh
+++ kit/subgraph/graph-remote-deploy.sh
@@ -10,6 +10,7 @@ restore_addresses() {
     STABLE_COIN_FACTORY_ADDRESS="0x5e771e1417100000000000000000000000000002"
     FUND_FACTORY_ADDRESS="0x5e771e1417100000000000000000000000000005"
     FIXED_YIELD_FACTORY_ADDRESS="0x5e771e1417100000000000000000000000000006"
+    TOKENIZED_DEPOSIT_FACTORY_ADDRESS="0x5e771e1417100000000000000000000000000007"
 
     yq -i "(.dataSources[] | select(.name == \"BondFactory\").source.address) = \"$BOND_FACTORY_ADDRESS\"" subgraph.yaml
     yq -i "(.dataSources[] | select(.name == \"CryptoCurrencyFactory\").source.address) = \"$CRYPTO_CURRENCY_FACTORY_ADDRESS\"" subgraph.yaml
@@ -17,6 +18,7 @@ restore_addresses() {
     yq -i "(.dataSources[] | select(.name == \"StableCoinFactory\").source.address) = \"$STABLE_COIN_FACTORY_ADDRESS\"" subgraph.yaml
     yq -i "(.dataSources[] | select(.name == \"FundFactory\").source.address) = \"$FUND_FACTORY_ADDRESS\"" subgraph.yaml
     yq -i "(.dataSources[] | select(.name == \"FixedYieldFactory\").source.address) = \"$FIXED_YIELD_FACTORY_ADDRESS\"" subgraph.yaml
+    yq -i "(.dataSources[] | select(.name == \"TokenizedDepositFactory\").source.address) = \"$TOKENIZED_DEPOSIT_FACTORY_ADDRESS\"" subgraph.yaml
     echo "Original addresses restored."
 }
 
@@ -29,6 +31,7 @@ EQUITY_FACTORY_ADDRESS=$(jq -r '."EquityFactoryModule#EquityFactory"' ../contrac
 STABLE_COIN_FACTORY_ADDRESS=$(jq -r '."StableCoinFactoryModule#StableCoinFactory"' ../contracts/ignition/deployments/asset-tokenization/deployed_addresses.json)
 FUND_FACTORY_ADDRESS=$(jq -r '."FundFactoryModule#FundFactory"' ../contracts/ignition/deployments/asset-tokenization/deployed_addresses.json)
 FIXED_YIELD_FACTORY_ADDRESS=$(jq -r '."FixedYieldFactoryModule#FixedYieldFactory"' ../contracts/ignition/deployments/asset-tokenization/deployed_addresses.json)
+TOKENIZED_DEPOSIT_FACTORY_ADDRESS=$(jq -r '."TokenizedDepositFactoryModule#TokenizedDepositFactory"' ../contracts/ignition/deployments/asset-tokenization/deployed_addresses.json)
 # Update the addresses in subgraph.yaml
 yq -i "(.dataSources[] | select(.name == \"BondFactory\").source.address) = \"$BOND_FACTORY_ADDRESS\"" subgraph.yaml
 yq -i "(.dataSources[] | select(.name == \"CryptoCurrencyFactory\").source.address) = \"$CRYPTO_CURRENCY_FACTORY_ADDRESS\"" subgraph.yaml
@@ -36,6 +39,7 @@ yq -i "(.dataSources[] | select(.name == \"EquityFactory\").source.address) = \"
 yq -i "(.dataSources[] | select(.name == \"StableCoinFactory\").source.address) = \"$STABLE_COIN_FACTORY_ADDRESS\"" subgraph.yaml
 yq -i "(.dataSources[] | select(.name == \"FundFactory\").source.address) = \"$FUND_FACTORY_ADDRESS\"" subgraph.yaml
 yq -i "(.dataSources[] | select(.name == \"FixedYieldFactory\").source.address) = \"$FIXED_YIELD_FACTORY_ADDRESS\"" subgraph.yaml
+yq -i "(.dataSources[] | select(.name == \"TokenizedDepositFactory\").source.address) = \"$TOKENIZED_DEPOSIT_FACTORY_ADDRESS\"" subgraph.yaml
 # Print addresses for debugging
 echo "Addresses being used:"
 echo "BondFactory: $BOND_FACTORY_ADDRESS"
@@ -44,6 +48,7 @@ echo "EquityFactory: $EQUITY_FACTORY_ADDRESS"
 echo "StableCoinFactory: $STABLE_COIN_FACTORY_ADDRESS"
 echo "FundFactory: $FUND_FACTORY_ADDRESS"
 echo "FixedYieldFactory: $FIXED_YIELD_FACTORY_ADDRESS"
+echo "TokenizedDepositFactory: $TOKENIZED_DEPOSIT_FACTORY_ADDRESS"
 echo "---"
 
 bun graph codegen
