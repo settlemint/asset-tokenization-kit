15af00d08f233303a861d3c7212761c171d1aea0
Patrick Mualaba <patrick.mualaba@live.be>
2025-03-17 16:33:02 +0100

    fix hasura types

--- kit/dapp/src/app/[locale]/(private)/portfolio/my-contacts/(table)/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/portfolio/my-contacts/(table)/page.tsx
@@ -1,25 +1,22 @@
-import MyContactsTable from "@/components/blocks/my-contacts-table/my-contacts-table";
-import { PageHeader } from "@/components/layout/page-header";
-import { getUser } from "@/lib/auth/utils";
-import { getTranslations } from "next-intl/server";
-import type { Address } from "viem";
+import MyContactsTable from '@/components/blocks/my-contacts-table/my-contacts-table';
+import { PageHeader } from '@/components/layout/page-header';
+import { getUser } from '@/lib/auth/utils';
+import { getTranslations } from 'next-intl/server';
+import type { Address } from 'viem';
+import { MyContactsActions } from './_components/actions';
 
-interface MyContactsPageProps {
-  params: Promise<{ locale: string }>;
-}
-
-export default async function MyContactsPage({ params }: MyContactsPageProps) {
-  const { locale } = await params;
-  const t = await getTranslations({
-    locale,
-    namespace: "portfolio.my-contacts",
-  });
+export default async function MyContactsPage() {
+  const t = await getTranslations('portfolio.my-contacts');
   const user = await getUser();
 
   return (
     <>
-      <PageHeader title={t("title")} section={t("description")} />
-      <MyContactsTable wallet={user.wallet as Address} title={t("title")} />
+      <PageHeader
+        title={t('title')}
+        section={t('description')}
+        button={<MyContactsActions />}
+      />
+      <MyContactsTable wallet={user.wallet as Address} title={t('title')} />
     </>
   );
 }
