a1d81811d34958dcb989aef80f05d1d6c4a55e27
Snigdha Singh <snigdha920@gmail.com>
2025-03-18 14:15:08 +0100

    fix grant role translations

--- kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/_components/manage-dropdown/grant-role-form/steps/roles.tsx
+++ kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/_components/manage-dropdown/grant-role-form/steps/roles.tsx
@@ -1,3 +1,6 @@
+import { FormStep } from "@/components/blocks/form/form-step";
+import { FormCheckbox } from "@/components/blocks/form/inputs/form-checkbox";
+import { ROLES, type RoleKey } from "@/lib/config/roles";
 import type { GrantRoleInput } from "@/lib/mutations/asset/access-control/grant-role/grant-role-schema";
 import { useTranslations } from "next-intl";
 import { useFormContext } from "react-hook-form";
@@ -7,22 +10,21 @@ export function AdminRoles() {
   const t = useTranslations("private.assets.details.forms");
 
   return (
-    <></>
-    // <FormStep title={t("title")} description={t("description")}>
-    //   <div className="space-y-3">
-    //     {(Object.entries(ROLES) as [RoleKey, (typeof ROLES)[RoleKey]][]).map(
-    //       ([key, role]) => (
-    //         <FormCheckbox
-    //           key={key}
-    //           name={`roles.${role.contractRole}`}
-    //           control={control}
-    //           label={role.displayName}
-    //           description={role.description}
-    //         />
-    //       )
-    //     )}
-    //   </div>
-    // </FormStep>
+    <FormStep title={t("roles.title")} description={t("roles.description")}>
+      <div className="space-y-3">
+        {(Object.entries(ROLES) as [RoleKey, (typeof ROLES)[RoleKey]][]).map(
+          ([key, role]) => (
+            <FormCheckbox
+              key={key}
+              name={`roles.${role.contractRole}`}
+              control={control}
+              label={role.displayName}
+              description={role.description}
+            />
+          )
+        )}
+      </div>
+    </FormStep>
   );
 }
 
--- kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/_components/manage-dropdown/grant-role-form/steps/summary.tsx
+++ kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/_components/manage-dropdown/grant-role-form/steps/summary.tsx
@@ -1,4 +1,7 @@
+import { AssetRolePill } from "@/components/blocks/asset-role-pill/asset-role-pill";
+import { EvmAddress } from "@/components/blocks/evm-address/evm-address";
 import { FormStep } from "@/components/blocks/form/form-step";
+import { FormSummaryDetailItem } from "@/components/blocks/form/summary/item";
 import type { GrantRoleInput } from "@/lib/mutations/asset/access-control/grant-role/grant-role-schema";
 import { useTranslations } from "next-intl";
 import { useFormContext, useWatch } from "react-hook-form";
@@ -20,26 +23,22 @@ export function Summary({ address }: SummaryProps) {
       title={t("title.grant-role")}
       description={t("description.grant-role")}
     >
-      {/* <FormSummaryDetailItem
-          label={t("asset-label")}
-          value={<EvmAddress address={address} />}
-        />
+      <FormSummaryDetailItem
+        label={t("asset-label")}
+        value={<EvmAddress address={address} />}
+      />
 
-        <FormSummaryDetailItem
-          label={t("admin-address-label")}
-          value={
-            values.userAddress ? (
-              <EvmAddress address={values.userAddress} />
-            ) : (
-              "-"
-            )
-          }
-        />
+      <FormSummaryDetailItem
+        label={t("account-label.default")}
+        value={
+          values.userAddress ? <EvmAddress address={values.userAddress} /> : "-"
+        }
+      />
 
-        <FormSummaryDetailItem
-          label={t("roles-label")}
-          value={<AssetRolePill roles={values.roles} />}
-        /> */}
+      <FormSummaryDetailItem
+        label={t("roles-label")}
+        value={<AssetRolePill roles={values.roles} />}
+      />
     </FormStep>
   );
 }
