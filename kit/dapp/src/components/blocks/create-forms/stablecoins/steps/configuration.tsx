8a36b7cb392d8a91c324fd7313502c8fba06d587
Snigdha Singh <snigdha920@gmail.com>
2025-03-20 01:44:21 +0100

    prettify

--- kit/dapp/src/components/blocks/create-forms/stablecoins/steps/configuration.tsx
+++ kit/dapp/src/components/blocks/create-forms/stablecoins/steps/configuration.tsx
@@ -1,6 +1,7 @@
 import { FormStep } from "@/components/blocks/form/form-step";
 import { FormInput } from "@/components/blocks/form/inputs/form-input";
 import { FormSelect } from "@/components/blocks/form/inputs/form-select";
+import { FormLabel } from "@/components/ui/form";
 import type { CurrencyCode } from "@/lib/db/schema-settings";
 import type { CreateStablecoinInput } from "@/lib/mutations/stablecoin/create/create-schema";
 import { timeUnits } from "@/lib/utils/zod";
@@ -20,30 +21,33 @@ export function Configuration({ baseCurrency }: ConfigurationProps) {
     label: t(`parameters.common.time-units.${value}`, { fallback: value }),
   }));
 
-  const DurationUnitSelect = (
-    <FormSelect
-      name="collateralLivenessTimeUnit"
-      control={control}
-      options={timeUnitOptions}
-      defaultValue="months"
-      className="w-[110px] border-0 shadow-none"
-    />
-  );
-
   return (
     <FormStep
       title={t("configuration.stablecoins.title")}
       description={t("configuration.stablecoins.description")}
     >
       <div className="grid grid-cols-2 gap-6">
-        <FormInput
-          control={control}
-          type="number"
-          name="collateralLivenessValue"
-          label={t("parameters.common.collateral-proof-validity-label")}
-          postfix={DurationUnitSelect}
-          required
-        />
+        <div>
+          <FormLabel>
+            {t("parameters.common.collateral-proof-validity-label")}
+          </FormLabel>
+          <div className="pt-3 grid grid-cols-2">
+            <FormInput
+              control={control}
+              type="number"
+              name="collateralLivenessValue"
+              required
+              className="rounded-r-none border-r-0 focus:border-r focus:ring-0"
+            />
+            <FormSelect
+              name="collateralLivenessTimeUnit"
+              control={control}
+              options={timeUnitOptions}
+              defaultValue="months"
+              className="rounded-l-none border-l-0 focus:border-l focus:ring-0"
+            />
+          </div>
+        </div>
         <FormInput
           control={control}
           name="valueInBaseCurrency"
