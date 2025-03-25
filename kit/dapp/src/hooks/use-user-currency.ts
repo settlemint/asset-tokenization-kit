f667e5154456c8ee54fe32ce0736e5525d2755bf
Snigdha Singh <snigdha920@gmail.com>
2025-03-25 12:49:43 +0100

    call get users directly

--- kit/dapp/src/hooks/use-user-currency.ts
+++ kit/dapp/src/hooks/use-user-currency.ts
@@ -1,7 +1,8 @@
 "use client";
 
+import { authClient } from "@/lib/auth/client";
 import { DEFAULT_SETTINGS, type CurrencyCode } from "@/lib/db/schema-settings";
-import { getCurrentUserCurrency } from "@/lib/queries/user/user-currency";
+import { getUserDetail } from "@/lib/queries/user/user-detail";
 import { useEffect, useState } from "react";
 
 export function useUserCurrency(): {
@@ -10,17 +11,21 @@ export function useUserCurrency(): {
   const [value, setValue] = useState<CurrencyCode>(
     DEFAULT_SETTINGS.baseCurrency
   );
-  const loadCurrency = async () => {
-    try {
-      const result = await getCurrentUserCurrency();
-      setValue(result);
-    } catch (error) {
-      console.error("Failed to load setting:", error);
-    }
-  };
+  const { data } = authClient.useSession();
 
   useEffect(() => {
+    const loadCurrency = async () => {
+      try {
+        if (!data?.user) return;
+        const userDetails = await getUserDetail({ id: data.user.id });
+        const currency = userDetails?.currency;
+        setValue(currency);
+      } catch (error) {
+        console.error("Failed to load setting:", error);
+      }
+    };
     void loadCurrency();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { userCurrency: value };
