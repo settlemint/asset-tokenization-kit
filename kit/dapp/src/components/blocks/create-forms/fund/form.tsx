f7b46c0ed62652d9aeec32f4d971f9397b9ddf4c
Silke Van den Broeck <silke@settlemint.com>
2025-04-02 11:59:06 +0200

    fix: remove unnecessary change

--- kit/dapp/src/components/blocks/create-forms/fund/form.tsx
+++ kit/dapp/src/components/blocks/create-forms/fund/form.tsx
@@ -55,8 +55,6 @@ export function CreateFundForm({
             amount: 1,
             currency: userDetails.currency,
           },
-          verificationType: "pincode",
-          predictedAddress: "0x0000000000000000000000000000000000000000",
           assetAdmins: [],
         }}
         onAnyFieldChange={({ clearErrors }) => {
