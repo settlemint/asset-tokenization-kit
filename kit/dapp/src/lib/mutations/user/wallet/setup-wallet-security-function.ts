5e0ee8b03eaab0d8848313d7eeca32330419eb9a
janb87 <12234016+janb87@users.noreply.github.com>
2025-04-07 19:10:45 +0200

    better validation

--- kit/dapp/src/lib/mutations/user/wallet/setup-wallet-security-function.ts
+++ kit/dapp/src/lib/mutations/user/wallet/setup-wallet-security-function.ts
@@ -1,4 +1,5 @@
 import { authClient } from "@/lib/auth/client";
+import { ApiError } from "next/dist/server/api-utils";
 import { setPincodeFunction } from "../set-pincode-function";
 import {
   WalletSecurityMethodOptions,
@@ -10,20 +11,24 @@ export async function setupWalletSecurityFunction({
 }: {
   parsedInput: SetupWalletSecurityInput;
 }) {
-  if (
-    method === WalletSecurityMethodOptions.TwoFactorAuthentication &&
-    firstOtp
-  ) {
+  if (method === WalletSecurityMethodOptions.TwoFactorAuthentication) {
+    if (!firstOtp) {
+      throw new ApiError(400, "First OTP is required");
+    }
     await authClient.twoFactor.verifyTotp({
       code: firstOtp,
     });
-  }
-  if (method === WalletSecurityMethodOptions.Pincode && pincode) {
+  } else if (method === WalletSecurityMethodOptions.Pincode) {
+    if (!pincode) {
+      throw new ApiError(400, "Pincode is required");
+    }
     await setPincodeFunction({
       parsedInput: {
         pincode,
       },
     });
+  } else {
+    throw new ApiError(400, `Invalid method: '${method}'`);
   }
   return {
     success: true,
