fcaed2738860f79d34e809f1fbf10a5bf39ef34c
Roderik van der Veer <roderik@settlemint.com>
2025-04-08 10:02:43 +0200

    feat: mint using erc3643 (#1423)
    ## Summary by Sourcery
    
    Enhance token minting process by integrating ERC3643 identity registry
    functionality, allowing identity registration during wallet identity
    deployment.
    
    New Features:
    - Add support for registering identities with the token's identity
    registry during wallet identity deployment
    
    Enhancements:
    - Modify identity deployment process to include identity registration
    with the token's identity registry

--- kit/contracts/test/ERC3643.t.sol
+++ kit/contracts/test/ERC3643.t.sol
@@ -19,6 +19,9 @@ import { ITREXFactory } from "../contracts/shared/erc3643/factory/ITREXFactory.s
 import { IdFactory } from "../contracts/shared/onchainid/factory/IdFactory.sol";
 import { TREXFactory } from "../contracts/shared/erc3643/factory/TREXFactory.sol";
 import { Gateway } from "../contracts/shared/onchainid/gateway/Gateway.sol";
+import { IIdentityRegistry } from "../contracts/shared/erc3643/registry/interface/IIdentityRegistry.sol";
+import { IERC3643IdentityRegistry } from "../contracts/shared/erc3643/ERC-3643/IERC3643IdentityRegistry.sol";
+import { IIdentity } from "../contracts/shared/onchainid/interface/IIdentity.sol";
 
 contract GatewayTest is Test {
     TREXImplementationAuthority public tokenImplementationAuthority;
@@ -27,7 +30,7 @@ contract GatewayTest is Test {
     address public predeployer = makeAddr("Predeployer");
     address public platformAdmin = makeAddr("Platform Admin");
 
-    address public issuer1 = makeAddr("Issuer 1");
+    address public organization1 = makeAddr("Organization 1");
 
     address public tokenAgent1 = makeAddr("Token Agent 1");
     address public tokenAgent2 = makeAddr("Token Agent 2");
@@ -80,10 +83,8 @@ contract GatewayTest is Test {
         returns (TREXGateway gateway, Gateway identityGateway)
     {
         vm.startPrank(platformAdmin_);
-        address[] memory identityManagers = new address[](1);
-        identityManagers[0] = identityAgent1;
         (gateway, identityGateway) =
-            platformFactory.createPlatform(platformAdmin_, tokenImplementationAuthority_, identityManagers);
+            platformFactory.createPlatform(platformAdmin_, tokenImplementationAuthority_, new address[](0));
         vm.stopPrank();
     }
 
@@ -189,39 +190,103 @@ contract GatewayTest is Test {
         // console.log("modularComplianceAddress", modularComplianceAddress);
     }
 
-    function test_OnboardFirstAdmin() public {
-        (TREXGateway gateway,) = onboardFirstAdmin(platformAdmin, address(tokenImplementationAuthority));
-        assertTrue(gateway.getPublicDeploymentStatus());
+    function createIdentity(Gateway identityGateway_, address clientWalletAddress_, uint8 countryCode_) public {
+        vm.startPrank(identityAgent1);
+        IIdentity id = IIdentity(identityGateway_.deployIdentityForWallet(clientWalletAddress_));
+        IERC3643IdentityRegistry identityRegistry = Token(tokenAddress).identityRegistry();
+        // country numbers from https://en.wikipedia.org/wiki/ISO_3166-1_numeric
+        identityRegistry.registerIdentity(clientWalletAddress_, id, countryCode_);
+        vm.stopPrank();
     }
 
+    /*
+    * @dev First user is detected logging in to the platform
+    *      The OTP/Pincode is checked and is found to be missing
+    *      During the sign up the basic user information is already entered, which is enough for a platform admin
+    *
+    *      The user is presented with the onboarding wizard
+    *      Step 1: Set your pincode/otp
+    *      Step 2: Deploy the platform (in wizard portal call), the gateway starts disabled!
+    *      Step 3: Select the default platform currency (EUR),
+    *           on setting this we can kick of the exchange rate fetcher,
+    *           when we have more, add a choice of provider for this
+    *      On completion of the wizard, the platform admin is presented with the dasbhoard in platform management
+    *      Which features a big next steps section.
+    *
+    *      Some ideas, could we deploy an entire setup beforehand and have a "claim" action?
+    *      And how would me we make follow up deploys when devs start customizing more user friendly?
+    */
+    function test_OnboardFirstAdmin() public {
+        onboardFirstAdmin(platformAdmin, address(tokenImplementationAuthority));
+    }
+
+    /*
+    * @dev First item on the next steps list
+    *       Item 1: Configure the gateway
+    *           - configure deployment fee and enable it if desired
+    *           - enable public deployment
+    *           - this happens on a settings page
+    *       Item 2: Invite organizations
+    *           - add a new organization with some fields (like name but also gateway discount if applicable)
+    *           - tie this into the organization plugin from better auth
+    *           - we need to give an organization a key of their own. the org admins can use it (how to handle the
+    OTP/pincode here?) <- this does not feel right
+    *           - there is also an upload CSV version of this to do so in batch
+    *           - This happen via an action button on the organization list page (also needs a detail page with tabs)
+    *           - There is an alternative option in the list of organizations, where an org that is created via a user
+    registering that it can be "enabled" as issuer organization
+    *       Invited organization user has a next steps section in the issuer dashboard
+    *       Current dashboard needs to be filtered by organization, full dashboard moves into platform management
+    * @dev First item on the next steps list for the organization admin
+    *       Item 1: Invite token agents
+    *           - agents are a role on the admin plugin
+    *           - and are stored in the AgentManager contract
+    *           - again also allow CSV upload with invite, add one by one, and change the role of organization users
+    *       Item 2: Invite identity agents
+    *           - same as above
+    *           - the org admin and identity agents see a section for user management
+    *           - it should hold only the users for this org <- we need to make a choice here, is the ATK multi org or
+    not? Single org would make a lot of sense too
+    */
     function test_OnboardIssuer() public {
         (TREXGateway gateway,) = onboardFirstAdmin(platformAdmin, address(tokenImplementationAuthority));
-        onboardIssuer(gateway, platformAdmin, issuer1);
+        onboardIssuer(gateway, platformAdmin, organization1);
     }
 
+    /*
+    * @dev If a user is an agent, they can deploy a token suite
+    *        - using the asset designer
+    *        - useing the full modal design
+    *        - token agents are again users from the org, same as identity admins
+    *        - BUT both lists of agents need to tie into the plugin system so that plugins can have a key and do
+    administrative actions (collateral, kyc, redemption, etc)
+    */
     function test_DeployTokenSuite() public {
         (TREXGateway gateway,) = onboardFirstAdmin(platformAdmin, address(tokenImplementationAuthority));
-        onboardIssuer(gateway, platformAdmin, issuer1);
+        onboardIssuer(gateway, platformAdmin, organization1);
 
         address[] memory tokenAgents = new address[](1);
         tokenAgents[0] = tokenAgent1;
         address[] memory identityManagers = new address[](1);
         identityManagers[0] = identityAgent1;
-        deployTokenSuite(gateway, issuer1, tokenAgents, identityManagers);
+        deployTokenSuite(gateway, organization1, tokenAgents, identityManagers);
     }
 
+    /*
+    * @dev there needs to be tabs in the details to manage agents
+    */
     function test_AddSecondIssuer() public {
         (TREXGateway gateway,) = onboardFirstAdmin(platformAdmin, address(tokenImplementationAuthority));
-        onboardIssuer(gateway, platformAdmin, issuer1);
+        onboardIssuer(gateway, platformAdmin, organization1);
 
         address[] memory tokenAgents = new address[](1);
         tokenAgents[0] = tokenAgent1;
         address[] memory identityManagers = new address[](1);
         identityManagers[0] = identityAgent1;
-        deployTokenSuite(gateway, issuer1, tokenAgents, identityManagers);
+        deployTokenSuite(gateway, organization1, tokenAgents, identityManagers);
 
         // Now add a second agent to the token
-        vm.startPrank(issuer1); // The issuer1 is the owner of the token
+        vm.startPrank(organization1); // The issuer1 is the owner of the token
         Token(tokenAddress).addAgent(tokenAgent2);
         vm.stopPrank();
 
@@ -229,28 +294,36 @@ contract GatewayTest is Test {
         assertTrue(Token(tokenAddress).isAgent(tokenAgent2));
     }
 
+    /**
+     * @dev - holders can be invited one by one or by CSV upload
+     *      - existing wallets can also be assigned as holders
+     *      - holders get a KYC step in the onboarding process
+     *      - identity agents need to approve the KYC info before the user can interact with the token(s), i suggest we
+     * share the identity storage between tokens in the same org (or ask the user what they want to do)
+     *      - plugins can do the same
+     *      - as a claim requirement for the token i would go for a simple selection, kyc ok, age, region
+     *      - should we create the identity on the fly when we create a wallet?
+     */
     function test_Mint() public {
         (TREXGateway gateway, Gateway identityGateway) =
             onboardFirstAdmin(platformAdmin, address(tokenImplementationAuthority));
-        onboardIssuer(gateway, platformAdmin, issuer1);
+        onboardIssuer(gateway, platformAdmin, organization1);
 
         address[] memory tokenAgents = new address[](1);
         tokenAgents[0] = tokenAgent1;
         address[] memory identityManagers = new address[](1);
         identityManagers[0] = identityAgent1;
-        deployTokenSuite(gateway, issuer1, tokenAgents, identityManagers);
+        deployTokenSuite(gateway, organization1, tokenAgents, identityManagers);
 
         // Now add a second agent to the token
-        vm.startPrank(issuer1); // The issuer1 is the owner of the token
+        vm.startPrank(organization1); // The issuer1 is the owner of the token
         Token(tokenAddress).addAgent(tokenAgent2);
         vm.stopPrank();
 
         // Verify that tokenAgent2 is now an agent
         assertTrue(Token(tokenAddress).isAgent(tokenAgent2));
 
-        vm.startPrank(tokenAgent1);
-        identityGateway.deployIdentityForWallet(client1);
-        vm.stopPrank();
+        createIdentity(identityGateway, client1, 56); // 56 is Belgium
 
         vm.startPrank(tokenAgent2);
         // Mint 1000 tokens to the owner
