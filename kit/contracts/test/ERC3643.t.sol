d2652a96d0f6b3d8f076195413748ff6ef8625aa
Roderik van der Veer <roderik@settlemint.com>
2025-04-09 16:07:54 +0200

    fix: license

--- kit/contracts/test/ERC3643.t.sol
+++ kit/contracts/test/ERC3643.t.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 /**
- *     T-REX is a suite of smart contracts implementing the ERC-3643 standard and
- *     developed by Tokeny to manage and transfer financial assets on EVM blockchains
+ *     The SMART protocol is a suite of smart contracts implementing the ERC-3643
+ *     standard, the T-REX standard implementation and extended and reworked by
+ *     SettleMint to manage and transfer a wide variety of financial assets on EVM
+ *     blockchains.
  *
- *     Copyright (C) 2023, Tokeny sàrl.
+ *     Copyright (C) 2025, SettleMint NV
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
