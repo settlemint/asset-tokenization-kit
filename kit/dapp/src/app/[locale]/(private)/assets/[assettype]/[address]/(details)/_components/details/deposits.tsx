f028b9a4db0b7dfe6603c21c0f4c514e9285660a
Jan Bevers <12234016+janb87@users.noreply.github.com>
2025-04-10 11:11:52 +0200

    feat: make use of suspense to improve initial rendering time (#1492)
    ## Summary by Sourcery
    
    Improve initial rendering performance by using Suspense and adding
    fallback skeletons for asynchronous components
    
    New Features:
    - Introduced DetailGridSkeleton for loading states
    - Created separate Charts and TransactionsChart components to enable
    more granular loading
    
    Enhancements:
    - Refactored multiple components to remove Suspense imports and improve
    code structure
    - Added skeleton loading states for better user experience during data
    fetching

--- kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/(details)/_components/details/deposits.tsx
+++ kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/(details)/_components/details/deposits.tsx
@@ -5,7 +5,6 @@ import { getAssetBalanceDetail } from "@/lib/queries/asset-balance/asset-balance
 import { getDepositDetail } from "@/lib/queries/deposit/deposit-detail";
 import { formatNumber } from "@/lib/utils/number";
 import { getLocale, getTranslations } from "next-intl/server";
-import { Suspense } from "react";
 import type { Address } from "viem";
 
 interface DepositsDetailsProps {
@@ -32,86 +31,79 @@ export async function DepositsDetails({
       : null;
 
   return (
-    <Suspense>
-      <DetailGrid>
-        <DetailGridItem label={t("name")}>{deposit.name}</DetailGridItem>
-        <DetailGridItem label={t("symbol")}>{deposit.symbol}</DetailGridItem>
-        {deposit.isin && (
-          <DetailGridItem label={t("isin")}>{deposit.isin}</DetailGridItem>
-        )}
-        <DetailGridItem label={t("contract-address")}>
-          <EvmAddress
-            address={deposit.id}
-            prettyNames={false}
-            hoverCard={false}
-            copyToClipboard={true}
-          />
-        </DetailGridItem>
-        <DetailGridItem label={t("creator")}>
-          <EvmAddress
-            address={deposit.creator.id}
-            hoverCard={false}
-            copyToClipboard={true}
-          />
-        </DetailGridItem>
-        <DetailGridItem label={t("decimals")}>
-          {deposit.decimals}
-        </DetailGridItem>
-        <DetailGridItem label={t("total-supply")} info={t("total-supply-info")}>
-          {formatNumber(deposit.totalSupply, {
+    <DetailGrid>
+      <DetailGridItem label={t("name")}>{deposit.name}</DetailGridItem>
+      <DetailGridItem label={t("symbol")}>{deposit.symbol}</DetailGridItem>
+      {deposit.isin && (
+        <DetailGridItem label={t("isin")}>{deposit.isin}</DetailGridItem>
+      )}
+      <DetailGridItem label={t("contract-address")}>
+        <EvmAddress
+          address={deposit.id}
+          prettyNames={false}
+          hoverCard={false}
+          copyToClipboard={true}
+        />
+      </DetailGridItem>
+      <DetailGridItem label={t("creator")}>
+        <EvmAddress
+          address={deposit.creator.id}
+          hoverCard={false}
+          copyToClipboard={true}
+        />
+      </DetailGridItem>
+      <DetailGridItem label={t("decimals")}>{deposit.decimals}</DetailGridItem>
+      <DetailGridItem label={t("total-supply")} info={t("total-supply-info")}>
+        {formatNumber(deposit.totalSupply, {
+          token: deposit.symbol,
+          locale: locale,
+        })}
+      </DetailGridItem>
+      {/* Show balance only when requested and available */}
+      {showBalance && balanceData && (
+        <DetailGridItem label={t("balance")}>
+          {formatNumber(balanceData.value, {
             token: deposit.symbol,
             locale: locale,
           })}
         </DetailGridItem>
-        {/* Show balance only when requested and available */}
-        {showBalance && balanceData && (
-          <DetailGridItem label={t("balance")}>
-            {formatNumber(balanceData.value, {
-              token: deposit.symbol,
-              locale: locale,
-            })}
-          </DetailGridItem>
-        )}
-        <DetailGridItem label={t("total-burned")} info={t("total-burned-info")}>
-          {formatNumber(deposit.totalBurned, {
-            token: deposit.symbol,
-            locale: locale,
-          })}
-        </DetailGridItem>
-        <DetailGridItem
-          label={t("total-holders")}
-          info={t("total-holders-info")}
-        >
-          {formatNumber(deposit.totalHolders, {
-            decimals: 0,
-            locale: locale,
-          })}
-        </DetailGridItem>
-        <DetailGridItem
-          label={t("ownership-concentration")}
-          info={t("ownership-concentration-info")}
-        >
-          {formatNumber(deposit.concentration, {
-            percentage: true,
-            decimals: 2,
-            locale: locale,
-          })}
-        </DetailGridItem>
-        <DetailGridItem label={t("price")}>
-          {formatNumber(deposit.price.amount, {
-            currency: deposit.price.currency,
-            decimals: 2,
-            locale: locale,
-          })}
-        </DetailGridItem>
-        <DetailGridItem label={t("total-value")}>
-          {formatNumber(deposit.price.amount * deposit.totalSupply, {
-            currency: deposit.price.currency,
-            decimals: 2,
-            locale: locale,
-          })}
-        </DetailGridItem>
-      </DetailGrid>
-    </Suspense>
+      )}
+      <DetailGridItem label={t("total-burned")} info={t("total-burned-info")}>
+        {formatNumber(deposit.totalBurned, {
+          token: deposit.symbol,
+          locale: locale,
+        })}
+      </DetailGridItem>
+      <DetailGridItem label={t("total-holders")} info={t("total-holders-info")}>
+        {formatNumber(deposit.totalHolders, {
+          decimals: 0,
+          locale: locale,
+        })}
+      </DetailGridItem>
+      <DetailGridItem
+        label={t("ownership-concentration")}
+        info={t("ownership-concentration-info")}
+      >
+        {formatNumber(deposit.concentration, {
+          percentage: true,
+          decimals: 2,
+          locale: locale,
+        })}
+      </DetailGridItem>
+      <DetailGridItem label={t("price")}>
+        {formatNumber(deposit.price.amount, {
+          currency: deposit.price.currency,
+          decimals: 2,
+          locale: locale,
+        })}
+      </DetailGridItem>
+      <DetailGridItem label={t("total-value")}>
+        {formatNumber(deposit.price.amount * deposit.totalSupply, {
+          currency: deposit.price.currency,
+          decimals: 2,
+          locale: locale,
+        })}
+      </DetailGridItem>
+    </DetailGrid>
   );
 }
