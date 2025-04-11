861ea4428897ae2dfdcc47ec22cfed6e4fa276e8
Roderik van der Veer <roderik@settlemint.com>
2025-04-11 15:19:15 +0200

    fix: color of chart skeleton

--- kit/dapp/src/components/blocks/charts/chart-card-skeleton.tsx
+++ kit/dapp/src/components/blocks/charts/chart-card-skeleton.tsx
@@ -5,11 +5,11 @@ export function ChartCardSkeleton() {
   return (
     <Card>
       <CardHeader>
-        <Skeleton className="h-6 w-3/4" />
-        <Skeleton className="mt-1 h-4 w-1/2" />
+        <Skeleton className="h-6 w-3/4 bg-muted/50" />
+        <Skeleton className="mt-1 h-4 w-1/2 bg-muted/50" />
       </CardHeader>
       <CardContent>
-        <Skeleton className="h-48 w-full" />
+        <Skeleton className="h-48 w-full bg-muted/50" />
       </CardContent>
     </Card>
   );
