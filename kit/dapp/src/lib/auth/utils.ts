7e7c98bf7dd7303732dd6fdb3d89fea1c300ce66
Roderik van der Veer <roderik@settlemint.com>
2025-04-11 08:26:06 +0200

    cache

--- kit/dapp/src/lib/auth/utils.ts
+++ kit/dapp/src/lib/auth/utils.ts
@@ -1,5 +1,7 @@
 import { auth } from "@/lib/auth/auth";
 import { redirectToSignIn } from "@/lib/auth/redirect";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 import { headers } from "next/headers";
 import type { User } from "./types";
 
@@ -9,15 +11,19 @@ import type { User } from "./types";
  * @throws {AuthError} If user is not authenticated
  */
 export async function getUser() {
+  return getSession({ headers: await headers() });
+}
+
+async function getSession({ headers }: { headers: ReadonlyHeaders }) {
+  "use cache";
+  cacheTag("session");
   try {
     const session = await auth.api.getSession({
-      headers: await headers(),
+      headers,
     });
-
     if (!session?.user) {
       return redirectToSignIn();
     }
-
     return session.user as User;
   } catch (err) {
     const error = err as Error;
