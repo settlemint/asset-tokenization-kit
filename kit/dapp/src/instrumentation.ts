57323302b1bf6206049aa04ed8b6a2bb9a4759e3
Roderik van der Veer <roderik@settlemint.com>
2025-04-11 11:52:12 +0200

    feat: improve the instrumentation

--- kit/dapp/src/instrumentation.ts
+++ kit/dapp/src/instrumentation.ts
@@ -4,6 +4,15 @@ export function register() {
   if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
     registerOTel({
       serviceName: process.env.NEXT_PUBLIC_APP_ID || "ATK",
+      autoDetectResources: true,
+      instrumentationConfig: {
+        fetch: {
+          attributesFromRequestHeaders: {
+            "request.operation_name": "X-GraphQL-Operation-Name",
+            "request.operation_type": "X-GraphQL-Operation-Type",
+          },
+        },
+      },
     });
   }
 }
