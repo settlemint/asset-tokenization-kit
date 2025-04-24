aae2ef163eea560663c7da714520bfdccee0a9d4
Snigdha Singh <snigdha920@gmail.com>
2025-04-24 15:28:36 +0200

    setup actions page

---
+++ kit/dapp/src/app/[locale]/(private)/actions/page.tsx
@@ -0,0 +1,43 @@
+import { PageHeader } from "@/components/layout/page-header";
+import { metadata } from "@/lib/config/metadata";
+import type { Metadata } from "next";
+import type { Locale } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({
+    locale,
+    namespace: "actions.page",
+  });
+
+  return {
+    title: {
+      ...metadata.title,
+      default: t("actions"),
+    },
+    description: t("actions"),
+  };
+}
+
+export default async function ActionsPage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = await params;
+  const t = await getTranslations({
+    locale,
+    namespace: "actions.page",
+  });
+
+  return (
+    <>
+      <PageHeader title={t("actions")} />
+    </>
+  );
+}
