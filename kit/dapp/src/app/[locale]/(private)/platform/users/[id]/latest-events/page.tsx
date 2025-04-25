99e0c57a174df22c82e898065dabf97affc081da
Roderik van der Veer <roderik@settlemint.com>
2025-04-25 18:57:44 +0200

    fix: user assets table (#1738)

--- kit/dapp/src/app/[locale]/(private)/platform/users/[id]/latest-events/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/platform/users/[id]/latest-events/page.tsx
@@ -1,9 +1,6 @@
 import { AssetEventsSkeleton } from "@/components/blocks/asset-events-table/asset-events-skeleton";
 import { AssetEventsTable } from "@/components/blocks/asset-events-table/asset-events-table";
-import { Button } from "@/components/ui/button";
-import { Link } from "@/i18n/routing";
 import { getUserDetail } from "@/lib/queries/user/user-detail";
-import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 
 interface LatestTransactionsPageProps {
@@ -14,29 +11,11 @@ export default async function LatestEventsPage({
   params,
 }: LatestTransactionsPageProps) {
   const { id } = await params;
-  const t = await getTranslations("private.users.latest-events");
   const user = await getUserDetail({ id });
 
   return (
-    <>
-      <Suspense fallback={<AssetEventsSkeleton />}>
-        <AssetEventsTable
-          disableToolbarAndPagination={true}
-          limit={10}
-          sender={user.wallet}
-        />
-      </Suspense>
-      <Link
-        prefetch
-        href={`/assets/activity/events?sender=${encodeURIComponent(
-          user.wallet
-        )}`}
-        className="flex justify-end"
-      >
-        <Button variant="secondary" className="mt-4">
-          {t("view-all")}
-        </Button>
-      </Link>
-    </>
+    <Suspense fallback={<AssetEventsSkeleton />}>
+      <AssetEventsTable sender={user.wallet} />
+    </Suspense>
   );
 }
