a6f8efa6184a4cc145152065eed400bc51a72231
Snigdha Singh <snigdha920@gmail.com>
2025-04-29 11:47:50 +0200

    mature bond action wire

---
+++ kit/dapp/src/app/[locale]/(private)/actions/pending/_components/columns.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { MatureForm } from "@/components/blocks/bonds/mature-form/form";
+import { EvmAddress } from "@/components/blocks/evm-address/evm-address";
+import { EvmAddressBalances } from "@/components/blocks/evm-address/evm-address-balances";
+import type { Action } from "@/lib/actions/pending";
+import { formatDate } from "@/lib/utils/date";
+import { createColumnHelper } from "@tanstack/react-table";
+import { useLocale, useTranslations } from "next-intl";
+import { getAddress, isAddress } from "viem";
+
+const columnHelper = createColumnHelper<Action>();
+
+export function columns() {
+  // https://next-intl.dev/docs/environments/server-client-components#shared-components
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const t = useTranslations("actions.pending");
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const locale = useLocale();
+
+  return [
+    columnHelper.accessor("actionType", {
+      header: t("action-type-header"),
+      cell: ({ getValue }) => t(getValue()),
+    }),
+    columnHelper.accessor("subject", {
+      header: t("subject"),
+      cell: ({ getValue }) => {
+        const subject = getValue();
+        if (isAddress(subject)) {
+          return (
+            <EvmAddress address={subject} copyToClipboard={true} verbose={true}>
+              <EvmAddressBalances address={subject} />
+            </EvmAddress>
+          );
+        }
+
+        return subject;
+      },
+    }),
+    columnHelper.accessor("id", {
+      header: t("description-header"),
+      cell: ({ row }) => t(`${row.original.actionType}-description`),
+    }),
+    columnHelper.accessor("activeAtMs", {
+      header: t("active-on-header"),
+      cell: ({ row }) =>
+        formatDate(row.original.activeAtMs.toString(), {
+          locale,
+        }),
+    }),
+    columnHelper.display({
+      id: "actions",
+      cell: ({ row }) => {
+        const actionType = row.original.actionType;
+
+        if (actionType === "bond-mature") {
+          const subject = row.original.subject;
+          return <MatureForm address={getAddress(subject)} asButton />;
+        }
+        return <></>;
+      },
+    }),
+  ];
+}
--- kit/dapp/src/app/[locale]/(private)/actions/pending/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/actions/pending/page.tsx
@@ -1,7 +1,10 @@
+import { DataTable } from "@/components/blocks/data-table/data-table";
+import { getPendingActions } from "@/lib/actions/pending";
 import { metadata } from "@/lib/config/metadata";
 import type { Metadata } from "next";
 import type { Locale } from "next-intl";
 import { getTranslations } from "next-intl/server";
+import { columns } from "./_components/columns";
 
 export async function generateMetadata({
   params,
@@ -23,20 +26,8 @@ export async function generateMetadata({
   };
 }
 
-export default async function ActionsPage({
-  params,
-}: {
-  params: Promise<{ locale: Locale }>;
-}) {
-  const { locale } = await params;
-  const t = await getTranslations({
-    locale,
-    namespace: "actions",
-  });
+export default async function ActionsPage() {
+  const actions = await getPendingActions();
 
-  return (
-    <>
-      <div>Something</div>
-    </>
-  );
+  return <DataTable columns={columns} data={actions} name="actions" />;
 }
