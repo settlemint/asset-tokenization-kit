6ec88f54aab1c62e8a0106e76a4c5d08924e6429
Robbe Verhelst <robbe.verhelst@gmail.com>
2025-04-29 22:39:11 +0200

    chore: update Solidity pragma version to 0.8.28 across all contracts and tests, and remove .gitmodules file

--- kit/contracts/contracts/extensions/ERC20HistoricalBalances.sol
+++ kit/contracts/contracts/extensions/ERC20HistoricalBalances.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: FSL-1.1-MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 import { Checkpoints } from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
