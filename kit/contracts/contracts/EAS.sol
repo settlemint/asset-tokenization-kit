9b575bd65a7e6dbbee188bd2748831888dbdc4e5
Robbe Verhelst <robbe.verhelst@gmail.com>
2025-04-30 10:16:35 +0200

    refactor: simplify meta-transaction support by removing redundant overrides in EAS and EASSchemaRegistry contracts

--- kit/contracts/contracts/EAS.sol
+++ kit/contracts/contracts/EAS.sol
@@ -3,32 +3,18 @@ pragma solidity ^0.8.28;
 
 import { EAS as BaseEAS, ISchemaRegistry } from "@ethereum-attestation-service/eas-contracts/EAS.sol";
 import { ERC2771Context } from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-import { Context } from "@openzeppelin/contracts/utils/Context.sol";
 
 contract EAS is BaseEAS, ERC2771Context {
     constructor(
         ISchemaRegistry registry,
         address forwarder
-    ) BaseEAS(registry) ERC2771Context(forwarder) { }
+    ) BaseEAS(registry) ERC2771Context(forwarder) {}
 
-    /// @notice Returns the message sender in the context of meta-transactions
-    /// @dev Overrides both Context and ERC2771Context to support meta-transactions
-    /// @return The address of the message sender
-    function _msgSender() internal view override(Context, ERC2771Context) returns (address) {
+    function _msgSender() internal view virtual override(ERC2771Context) returns (address) {
         return super._msgSender();
     }
 
-    /// @notice Returns the message data in the context of meta-transactions
-    /// @dev Overrides both Context and ERC2771Context to support meta-transactions
-    /// @return The message data
-    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
+    function _msgData() internal view virtual override(ERC2771Context) returns (bytes calldata) {
         return super._msgData();
     }
-
-    /// @notice Returns the length of the context suffix for meta-transactions
-    /// @dev Overrides both Context and ERC2771Context to support meta-transactions
-    /// @return The length of the context suffix
-    function _contextSuffixLength() internal view override(Context, ERC2771Context) returns (uint256) {
-        return super._contextSuffixLength();
-    }
-}
+} 
--- kit/contracts/contracts/EASSchemaRegistry.sol
+++ kit/contracts/contracts/EASSchemaRegistry.sol
@@ -3,29 +3,15 @@ pragma solidity ^0.8.28;
 
 import { SchemaRegistry } from "@ethereum-attestation-service/eas-contracts/SchemaRegistry.sol";
 import { ERC2771Context } from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-import { Context } from "@openzeppelin/contracts/utils/Context.sol";
 
 contract EASSchemaRegistry is SchemaRegistry, ERC2771Context {
-    constructor(address forwarder) ERC2771Context(forwarder) { }
+    constructor(address forwarder) ERC2771Context(forwarder) {}
 
-    /// @notice Returns the message sender in the context of meta-transactions
-    /// @dev Overrides both Context and ERC2771Context to support meta-transactions
-    /// @return The address of the message sender
-    function _msgSender() internal view override(Context, ERC2771Context) returns (address) {
+    function _msgSender() internal view virtual override(ERC2771Context) returns (address) {
         return super._msgSender();
     }
 
-    /// @notice Returns the message data in the context of meta-transactions
-    /// @dev Overrides both Context and ERC2771Context to support meta-transactions
-    /// @return The message data
-    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
+    function _msgData() internal view virtual override(ERC2771Context) returns (bytes calldata) {
         return super._msgData();
-    }
-
-    /// @notice Returns the length of the context suffix for meta-transactions
-    /// @dev Overrides both Context and ERC2771Context to support meta-transactions
-    /// @return The length of the context suffix
-    function _contextSuffixLength() internal view override(Context, ERC2771Context) returns (uint256) {
-        return super._contextSuffixLength();
     }
 }
