da8025a9abfbcfc326cf131081273f5db6ea98ea
Snigdha Singh <snigdha920@gmail.com>
2025-05-02 14:49:18 +0200

    remove unused methods

--- kit/dapp/src/lib/queries/transactions/wait-for-transaction.ts
+++ kit/dapp/src/lib/queries/transactions/wait-for-transaction.ts
@@ -1,19 +1,7 @@
 "use server";
 
 import { portalClient, portalGraphql } from "@/lib/settlemint/portal";
-import { t, type StaticDecode } from "@/lib/utils/typebox";
-import type { FragmentOf } from "gql.tada";
-import { ReceiptFragment, ReceiptFragmentSchema } from "./transaction-fragment";
-
-/**
- * Constants for transaction monitoring
- */
-const POLLING_DEFAULTS = {
-  /** Default timeout in milliseconds (3 minutes) */
-  TIMEOUT_MS: 3 * 60 * 1000,
-  /** Default polling interval in milliseconds */
-  INTERVAL_MS: 500,
-} as const;
+import { ReceiptFragment } from "./transaction-fragment";
 
 const GetTransaction = portalGraphql(
   `
@@ -27,66 +15,6 @@ const GetTransaction = portalGraphql(
 `,
   [ReceiptFragment]
 );
-
-type TransactionReceipt = FragmentOf<typeof ReceiptFragment>;
-
-/**
- * Configuration options for transaction monitoring
- */
-interface TransactionMonitoringOptions {
-  /** Timeout in milliseconds before giving up */
-  timeoutMs?: number;
-  /** Polling interval in milliseconds */
-  pollingIntervalMs?: number;
-}
-
-/**
- * Waits for a single transaction to be mined
- * @internal Use waitForTransactions for external calls
- */
-export async function waitForSingleTransaction(
-  transactionHash: string,
-  options: TransactionMonitoringOptions = {}
-): Promise<TransactionReceipt> {
-  const timeoutMs = options.timeoutMs ?? POLLING_DEFAULTS.TIMEOUT_MS;
-  const pollingIntervalMs =
-    options.pollingIntervalMs ?? POLLING_DEFAULTS.INTERVAL_MS;
-
-  const startTime = Date.now();
-
-  let receipt: TransactionReceipt | null = null;
-
-  while (Date.now() - startTime < timeoutMs) {
-    try {
-      const result = await portalClient.request(GetTransaction, {
-        transactionHash,
-      });
-
-      if (result.getTransaction?.receipt) {
-        // We have a receipt, means the transaction was mined
-        receipt = result.getTransaction.receipt;
-        break;
-      }
-    } catch (error) {
-      console.error(
-        `Error while waiting for transaction ${transactionHash}:`,
-        error
-      );
-      // Continue polling even if there's an error
-    }
-
-    // Wait for the specified polling interval
-    await new Promise((resolve) => setTimeout(resolve, pollingIntervalMs));
-  }
-
-  if (!receipt) {
-    throw new Error(
-      `Transaction ${transactionHash} was not mined within the timeout period`
-    );
-  }
-
-  return receipt;
-}
 
 /**
  * Waits for multiple transactions to be mined
@@ -135,15 +63,3 @@ export async function waitForTransactions(
 
   return Promise.all(promises).then(() => Promise.resolve());
 }
-
-const WaitForTransactionsResponseSchema = t.Object({
-  receipts: t.Array(ReceiptFragmentSchema, {
-    description:
-      "Array of transaction receipts for all the processed transactions",
-  }),
-  lastTransaction: ReceiptFragmentSchema,
-});
-
-export type WaitForTransactionsResponse = StaticDecode<
-  typeof WaitForTransactionsResponseSchema
->;
