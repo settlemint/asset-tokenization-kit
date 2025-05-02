38361d947ff3e555b3346865b98b3541da633835
Silke Van den Broeck <silke@settlemint.com>
2025-05-02 15:47:30 +0200

    fix: not using vertical space optimally

--- kit/dapp/src/components/blocks/asset-designer/step-wizard/step-wizard.tsx
+++ kit/dapp/src/components/blocks/asset-designer/step-wizard/step-wizard.tsx
@@ -35,7 +35,7 @@ export function StepWizard({
   const currentStepIndex = steps.findIndex((step) => step.id === currentStepId);
 
   return (
-    <div className="flex h-[75vh] flex-col" tabIndex={-1}>
+    <div className="flex h-full min-h-[65vh] flex-col" tabIndex={-1}>
       <div className="flex flex-1 overflow-hidden p-6" tabIndex={-1}>
         {/* Sidebar / Steps */}
         <div
@@ -201,7 +201,7 @@ export function StepWizard({
         </div>
 
         {/* Content area - REMOVED FOCUS SCOPE */}
-        <div className="flex flex-col flex-1 overflow-hidden bg-background ml-6 rounded-lg p-10">
+        <div className="flex flex-col flex-1 overflow-hidden bg-background ml-6 rounded-lg p-10 pr-14">
           {children}
         </div>
       </div>
