21a6d51a5e34d0c7e8292f30199f5db6e0e680f9
Roderik van der Veer <roderik@settlemint.com>
2025-05-07 12:16:29 +0200

    feat: new homepage

--- kit/dapp/src/app/[locale]/(home)/page.tsx
+++ kit/dapp/src/app/[locale]/(home)/page.tsx
@@ -1,63 +1,21 @@
-import { BentoFeatureSection } from "@/app/[locale]/(home)/_components/features";
-import { Footer } from "@/app/[locale]/(home)/_components/footer";
-import type { Metadata } from "next";
-import { type Locale, useTranslations } from "next-intl";
-import { getTranslations } from "next-intl/server";
-import { HeroSection } from "./_components/hero";
-import { ThemeImage } from "./_components/theme-image";
-import HeroDark from "./assets/hero-dark.webp";
-import HeroLight from "./assets/hero-light.webp";
+import { BackgroundDecoration } from "./_components/background-decoration";
+import { Features } from "./_components/features";
+import { FooterSection } from "./_components/footer";
+import { HeroHeader } from "./_components/header";
+import { HeroVisualSection } from "./_components/hero-visual-section";
+import { Pricing } from "./_components/pricing";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ locale: Locale }>;
-}): Promise<Metadata> {
-  const { locale } = await params;
-  const t = await getTranslations({
-    locale,
-    namespace: "layout",
-  });
-
-  return {
-    title: t("header.app-name"),
-    description: t("header.app-description"),
-  };
-}
-
-export default function Home() {
-  const t = useTranslations("homepage");
-
+export default function HeroSection() {
   return (
-    <div className="relative flex min-h-[100dvh] flex-col">
-      <HeroSection
-        subtitle={t("hero-subtitle-regular")}
-        description={t("hero-description")}
-        ctaText={"bunx @settlemint/sdk-cli@latest create"}
-        ctaHref={"https://github.com/settlemint/asset-tokenization-kit"}
-        buttons={{
-          main: {
-            text: t("portfolio"),
-            href: "/portfolio",
-          },
-          secondary: {
-            text: t("issuer-portal"),
-            href: "/assets",
-          },
-          tertiary: {
-            text: t("documentation"),
-            href: "https://console.settlemint.com/documentation/",
-          },
-        }}
-      />
-
-      <div className="relative z-10 mx-auto mt-8 max-w-[1050px] px-4 md:px-10">
-        <ThemeImage light={HeroLight} dark={HeroDark} />
-      </div>
-
-      <BentoFeatureSection />
-
-      <Footer />
-    </div>
+    <>
+      <HeroHeader />
+      <main className="overflow-hidden">
+        <BackgroundDecoration />
+        <HeroVisualSection />
+        <Features />
+        <Pricing />
+      </main>
+      <FooterSection />
+    </>
   );
 }
