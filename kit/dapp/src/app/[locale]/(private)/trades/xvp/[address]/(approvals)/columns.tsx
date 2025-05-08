74e77f9eb17ac24d7f1fdcea0fe5a9825358b4e5
Snigdha Singh <snigdha920@gmail.com>
2025-05-08 12:17:33 +0200

    tweak details

--- kit/dapp/src/app/[locale]/(private)/trades/xvp/[address]/(approvals)/columns.tsx
+++ kit/dapp/src/app/[locale]/(private)/trades/xvp/[address]/(approvals)/columns.tsx
@@ -3,39 +3,25 @@
 import { EvmAddress } from "@/components/blocks/evm-address/evm-address";
 import { ApprovalStatusBadge } from "@/components/blocks/xvp-status/approval-status-badge";
 import type { XvPSettlementApproval } from "@/lib/queries/xvp/xvp-schema";
-import { formatDate } from "@/lib/utils/date";
 import { createColumnHelper } from "@tanstack/react-table";
-import { useLocale, useTranslations } from "next-intl";
+import { useTranslations } from "next-intl";
 
 const columnHelper = createColumnHelper<XvPSettlementApproval>();
 
 export function columns() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const t = useTranslations("trade-management.xvp");
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const locale = useLocale();
 
   return [
     columnHelper.accessor("account.id", {
-      header: t("columns.approver"),
-      cell: ({ getValue }) => <EvmAddress address={getValue()} />,
+      header: t("columns.user"),
+      cell: ({ getValue }) => (
+        <EvmAddress address={getValue()} copyToClipboard />
+      ),
     }),
     columnHelper.accessor("approved", {
       header: t("columns.status"),
       cell: ({ getValue }) => <ApprovalStatusBadge hasApproved={getValue()} />,
-    }),
-    columnHelper.accessor("timestamp", {
-      header: t("columns.approved-at"),
-      cell: ({ getValue, row }) => {
-        const timestamp = getValue();
-        if (row.original.approved && timestamp) {
-          return formatDate(timestamp.toString(), {
-            locale,
-            type: "relative",
-          });
-        }
-        return "-";
-      },
     }),
   ];
 }
