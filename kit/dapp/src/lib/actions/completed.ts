3888dabaf4445d94f925036cd3b1cdcc927545eb
Snigdha Singh <snigdha920@gmail.com>
2025-05-09 13:54:26 +0200

    remove existing actions setup

--- kit/dapp/src/lib/actions/completed.ts
+++ kit/dapp/src/lib/actions/completed.ts
@@ -1,27 +1,9 @@
 import { withTracing } from "@/lib/utils/tracing";
-import { getBondMaturedEventList } from "../queries/bond/bond-matured-event";
-import { actionTypes } from "./types";
 
 export const getCompletedActions = withTracing(
   "queries",
   "getCompletedActions",
   async () => {
-    const bondMatureCompletedActions = await getBondMatureCompletedActions();
-    return [...bondMatureCompletedActions];
-  }
-);
-const getBondMatureCompletedActions = withTracing(
-  "queries",
-  "getBondMatureCompletedActions",
-  async () => {
-    const bondMaturedEvents = await getBondMaturedEventList();
-
-    return bondMaturedEvents.map((event) => ({
-      id: event.id,
-      subject: event.emitter.id,
-      actionType: actionTypes.BOND_MATURITY,
-      completedAtMs: event.blockTimestamp,
-      sender: event.sender.id,
-    }));
+    return [];
   }
 );
--- kit/dapp/src/lib/actions/incomplete.ts
+++ kit/dapp/src/lib/actions/incomplete.ts
@@ -1,9 +1,5 @@
-import { SETTING_KEYS } from "@/lib/db/schema-settings";
 import { withTracing } from "@/lib/utils/tracing";
-import { isBefore } from "date-fns";
-import { DEFAULT_SETTINGS } from "../db/schema-settings";
-import { getBondList } from "../queries/bond/bond-list";
-import { actionTypes, type ActionType } from "./types";
+import type { ActionType } from "./types";
 
 type PendingAction = {
   id: string;
@@ -16,55 +12,9 @@ export const getIncompleteActions = withTracing(
   "queries",
   "getIncompleteActions",
   async () => {
-    const { pending: bondPending, upcoming: bondUpcoming } =
-      await getIncompleteBondActions();
     return {
-      pending: [...bondPending],
-      upcoming: [...bondUpcoming],
+      pending: [],
+      upcoming: [],
     };
-  }
-);
-
-const getIncompleteBondActions = withTracing(
-  "queries",
-  "getIncompleteBondActions",
-  async () => {
-    const bonds = await getBondList(
-      DEFAULT_SETTINGS[SETTING_KEYS.BASE_CURRENCY]
-    );
-    const nowTimestampMs = Date.now();
-
-    const bondsNotMatured = bonds.filter((bond) => !bond.isMatured);
-
-    return bondsNotMatured.reduce<{
-      upcoming: PendingAction[];
-      pending: PendingAction[];
-    }>(
-      (acc, bond) => {
-        const maturityTimestamp = Number(bond.maturityDate);
-
-        if (isBefore(maturityTimestamp * 1000, nowTimestampMs)) {
-          acc.pending.push({
-            id: bond.id,
-            subject: bond.id,
-            actionType: actionTypes.BOND_MATURITY,
-            activeAtMs: maturityTimestamp,
-          });
-        } else {
-          acc.upcoming.push({
-            id: bond.id,
-            subject: bond.id,
-            actionType: actionTypes.BOND_MATURITY,
-            activeAtMs: maturityTimestamp,
-          });
-        }
-
-        return acc;
-      },
-      {
-        upcoming: [],
-        pending: [],
-      }
-    );
   }
 );
