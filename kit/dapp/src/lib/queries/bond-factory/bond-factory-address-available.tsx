d5963a5c8f4deef2a1a8f4e79a016e13274a2f55
Snigdha Singh <snigdha920@gmail.com>
2025-05-10 22:24:02 +0200

    fix offchain asset creation

--- kit/dapp/src/lib/queries/bond-factory/bond-factory-address-available.tsx
+++ kit/dapp/src/lib/queries/bond-factory/bond-factory-address-available.tsx
@@ -6,7 +6,6 @@ import {
 } from "@/lib/settlemint/the-graph";
 import { withTracing } from "@/lib/utils/tracing";
 import { safeParse } from "@/lib/utils/typebox";
-import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 import type { Address } from "viem";
 import { BondExistsSchema } from "./bond-factory-schema";
 
@@ -28,8 +27,8 @@ export const isAddressAvailable = withTracing(
   "queries",
   "isAddressAvailable",
   async (address: Address) => {
-    "use cache";
-    cacheTag("asset");
+    // "use cache";
+    // cacheTag("asset");
     const data = await theGraphClientKit.request(BondExists, {
       token: address,
     });
