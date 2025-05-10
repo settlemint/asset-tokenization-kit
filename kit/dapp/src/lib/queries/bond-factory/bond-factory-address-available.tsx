3740c31051333ec626693bb9a65ca31111eba7da
Snigdha Singh <snigdha920@gmail.com>
2025-05-10 22:25:26 +0200

    cleanup

--- kit/dapp/src/lib/queries/bond-factory/bond-factory-address-available.tsx
+++ kit/dapp/src/lib/queries/bond-factory/bond-factory-address-available.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/lib/settlemint/the-graph";
 import { withTracing } from "@/lib/utils/tracing";
 import { safeParse } from "@/lib/utils/typebox";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 import type { Address } from "viem";
 import { BondExistsSchema } from "./bond-factory-schema";
 
@@ -27,8 +28,8 @@ export const isAddressAvailable = withTracing(
   "queries",
   "isAddressAvailable",
   async (address: Address) => {
-    // "use cache";
-    // cacheTag("asset");
+    "use cache";
+    cacheTag("asset");
     const data = await theGraphClientKit.request(BondExists, {
       token: address,
     });
