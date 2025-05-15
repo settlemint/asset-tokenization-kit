8fab38cb498003c72234e24c21d56158eb469605
janb87 <12234016+janb87@users.noreply.github.com>
2025-05-15 10:51:58 +0200

    cleanup

--- kit/dapp/src/lib/utils/server-only.ts
+++
@@ -1,6 +0,0 @@
-if (typeof window !== "undefined") {
-  throw new Error(
-    "This module cannot be imported from a Client Component module. " +
-      "It should only be used from a Server Component."
-  );
-}
