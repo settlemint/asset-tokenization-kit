550e96a775cee0e59f888969914c112761e5101e
Snigdha Singh <snigdha920@gmail.com>
2025-05-15 14:51:50 +0200

    flow summary

--- kit/dapp/src/app/[locale]/(private)/trades/xvp/_components/create-xvp/steps/summary.tsx
+++ kit/dapp/src/app/[locale]/(private)/trades/xvp/_components/create-xvp/steps/summary.tsx
@@ -1,12 +1,12 @@
-import { EvmAddress } from "@/components/blocks/evm-address/evm-address";
 import { FormStep } from "@/components/blocks/form/form-step";
 import { FormSummaryDetailCard } from "@/components/blocks/form/summary/card";
 import { FormSummaryDetailItem } from "@/components/blocks/form/summary/item";
+import { FlowCard } from "@/components/blocks/xvp/flow-card";
 import type { CreateXvpInput } from "@/lib/mutations/xvp/create/create-schema";
 import { formatDate } from "@/lib/utils/date";
-import { formatNumber } from "@/lib/utils/number";
-import { ArrowUpRight, Lock } from "lucide-react";
+import { ArrowRightLeft, Lock } from "lucide-react";
 import { useLocale, useTranslations } from "next-intl";
+import { Fragment } from "react";
 import { useFormContext } from "react-hook-form";
 
 export function Summary() {
@@ -17,43 +17,28 @@ export function Summary() {
 
   return (
     <FormStep title={t("summary.title")} description={t("summary.description")}>
-      {values.flows?.map((flow, index) => (
-        <FormSummaryDetailCard
-          key={index}
-          title={t("asset-flows.flow", { index: index + 1 })}
-          description={t("asset-flows.description")}
-          icon={<ArrowUpRight className="size-3 text-primary-foreground" />}
-        >
-          <FormSummaryDetailItem
-            label={t("asset-flows.asset")}
-            value={
-              flow.asset && (
-                <EvmAddress address={flow.asset.id} showAssetType={true} />
-              )
-            }
-          />
-          <FormSummaryDetailItem
-            label={t("asset-flows.amount")}
-            value={
-              flow.amount && flow.asset
-                ? formatNumber(flow.amount, {
-                    locale,
-                    decimals: flow.asset.decimals,
-                    token: flow.asset.symbol,
-                  })
-                : null
-            }
-          />
-          <FormSummaryDetailItem
-            label={t("asset-flows.from")}
-            value={flow.from ? <EvmAddress address={flow.from} /> : null}
-          />
-          <FormSummaryDetailItem
-            label={t("asset-flows.to")}
-            value={flow.to ? <EvmAddress address={flow.to} /> : null}
-          />
-        </FormSummaryDetailCard>
-      ))}
+      <FormSummaryDetailCard
+        title={t("asset-flows.flows")}
+        description={t("asset-flows.flows-summary")}
+        icon={<ArrowRightLeft className="size-3 text-primary-foreground" />}
+      >
+        {values.flows?.map((flow, index) => (
+          <Fragment key={index}>
+            {flow.from && flow.to && flow.asset && (
+              <FlowCard
+                from={flow.from}
+                to={flow.to}
+                amount={flow.amount}
+                asset={{
+                  symbol: flow.asset.symbol,
+                  decimals: flow.asset.decimals,
+                  address: flow.asset.id,
+                }}
+              />
+            )}
+          </Fragment>
+        ))}
+      </FormSummaryDetailCard>
 
       <FormSummaryDetailCard
         title={t("configuration.title")}
