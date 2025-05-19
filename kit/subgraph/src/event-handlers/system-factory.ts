b3fd3713726e242c631aa425b11ea00acd443233
Roderik van der Veer <roderik@settlemint.com>
2025-05-19 08:35:38 +0200

    system

--- kit/subgraph/src/event-handlers/system-factory.ts
+++ kit/subgraph/src/event-handlers/system-factory.ts
@@ -1,6 +1,10 @@
 import { SMARTSystemCreated } from "../../generated/SystemFactory/SystemFactory";
+import { System } from "../../generated/templates";
+import { fetchSystem } from "../fetch/system";
 import { processEvent } from "../shared/event/event";
 
 export function handleSMARTSystemCreated(event: SMARTSystemCreated): void {
   processEvent(event, "SystemCreated");
+  fetchSystem(event.params.systemAddress);
+  System.create(event.params.systemAddress);
 }
---
+++ kit/subgraph/src/event-handlers/system.ts
@@ -0,0 +1,80 @@
+import {
+  Bootstrapped,
+  ComplianceImplementationUpdated,
+  EtherWithdrawn,
+  IdentityFactoryImplementationUpdated,
+  IdentityImplementationUpdated,
+  IdentityRegistryImplementationUpdated,
+  IdentityRegistryStorageImplementationUpdated,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  TokenIdentityImplementationUpdated,
+  TrustedIssuersRegistryImplementationUpdated,
+} from "../../generated/templates/System/System";
+import { roleAdminChangedHandler } from "../shared/accesscontrol/role-admin-changed";
+import { roleGrantedHandler } from "../shared/accesscontrol/role-granted";
+import { roleRevokedHandler } from "../shared/accesscontrol/role-revoked";
+import { processEvent } from "../shared/event/event";
+
+export function handleBootstrapped(event: Bootstrapped): void {
+  processEvent(event, "Bootstrapped");
+}
+
+export function handleComplianceImplementationUpdated(
+  event: ComplianceImplementationUpdated
+): void {
+  processEvent(event, "ComplianceImplementationUpdated");
+}
+
+export function handleEtherWithdrawn(event: EtherWithdrawn): void {
+  processEvent(event, "EtherWithdrawn");
+}
+
+export function handleIdentityFactoryImplementationUpdated(
+  event: IdentityFactoryImplementationUpdated
+): void {
+  processEvent(event, "IdentityFactoryImplementationUpdated");
+}
+
+export function handleIdentityImplementationUpdated(
+  event: IdentityImplementationUpdated
+): void {
+  processEvent(event, "IdentityImplementationUpdated");
+}
+
+export function handleIdentityRegistryImplementationUpdated(
+  event: IdentityRegistryImplementationUpdated
+): void {
+  processEvent(event, "IdentityRegistryImplementationUpdated");
+}
+
+export function handleIdentityRegistryStorageImplementationUpdated(
+  event: IdentityRegistryStorageImplementationUpdated
+): void {
+  processEvent(event, "IdentityRegistryStorageImplementationUpdated");
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChanged): void {
+  roleAdminChangedHandler(event);
+}
+
+export function handleRoleGranted(event: RoleGranted): void {
+  roleGrantedHandler(event, event.params.role, event.params.account);
+}
+
+export function handleRoleRevoked(event: RoleRevoked): void {
+  roleRevokedHandler(event, event.params.role, event.params.account);
+}
+
+export function handleTokenIdentityImplementationUpdated(
+  event: TokenIdentityImplementationUpdated
+): void {
+  processEvent(event, "TokenIdentityImplementationUpdated");
+}
+
+export function handleTrustedIssuersRegistryImplementationUpdated(
+  event: TrustedIssuersRegistryImplementationUpdated
+): void {
+  processEvent(event, "TrustedIssuersRegistryImplementationUpdated");
+}
