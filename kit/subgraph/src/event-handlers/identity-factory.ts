7fa8e5c4053c710bc5093602ab35e5c974eb5032
Roderik van der Veer <roderik@settlemint.com>
2025-05-19 16:10:34 +0200

    feat: index identities

--- kit/subgraph/src/event-handlers/identity-factory.ts
+++ kit/subgraph/src/event-handlers/identity-factory.ts
@@ -6,6 +6,8 @@ import {
   RoleRevoked,
   TokenIdentityCreated,
 } from "../../generated/templates/IdentityFactory/IdentityFactory";
+import { fetchIdentity } from "../fetch/identity";
+import { fetchTokenIdentity } from "../fetch/token-identity";
 import { roleAdminChangedHandler } from "../shared/accesscontrol/role-admin-changed";
 import { roleGrantedHandler } from "../shared/accesscontrol/role-granted";
 import { roleRevokedHandler } from "../shared/accesscontrol/role-revoked";
@@ -13,6 +15,7 @@ import { processEvent } from "../shared/event/event";
 
 export function handleIdentityCreated(event: IdentityCreated): void {
   processEvent(event, "IdentityCreated");
+  fetchIdentity(event.params.identity);
 }
 
 export function handleInitialized(event: Initialized): void {
@@ -33,4 +36,5 @@ export function handleRoleRevoked(event: RoleRevoked): void {
 
 export function handleTokenIdentityCreated(event: TokenIdentityCreated): void {
   processEvent(event, "TokenIdentityCreated");
+  fetchTokenIdentity(event.params.identity);
 }
