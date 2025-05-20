a2813ab0fc4b1aa5d476301d1a545428040e684c
Silke Van den Broeck <silke@settlemint.com>
2025-05-20 10:41:18 +0200

    fix: error handling of getRegulationDetail query

--- kit/dapp/src/lib/queries/regulations/regulation-detail.ts
+++ kit/dapp/src/lib/queries/regulations/regulation-detail.ts
@@ -85,49 +85,55 @@ export const getRegulationDetail = withTracing(
   "queries",
   "getRegulationDetail",
   async ({ assetId, regulationType }: RegulationDetailProps) => {
-    const baseResponse = await hasuraClient.request(
-      RegulationDetail,
-      {
-        assetId,
-        regulationType,
-      },
-      {
-        "X-GraphQL-Operation-Name": "RegulationDetail",
-        "X-GraphQL-Operation-Type": "query",
+    try {
+      const baseResponse = await hasuraClient.request(
+        RegulationDetail,
+        {
+          assetId,
+          regulationType,
+        },
+        {
+          "X-GraphQL-Operation-Name": "RegulationDetail",
+          "X-GraphQL-Operation-Type": "query",
+        }
+      );
+
+      if (baseResponse.regulation_configs.length === 0) {
+        return null;
       }
-    );
 
-    if (baseResponse.regulation_configs.length === 0) {
-      return null;
-    }
+      const baseConfig = baseResponse.regulation_configs[0];
 
-    const baseConfig = baseResponse.regulation_configs[0];
+      const specificQuery = RegulationTypeQueries[regulationType];
+      const configField = RegulationTypeConfigFields[regulationType];
 
-    const specificQuery = RegulationTypeQueries[regulationType];
-    const configField = RegulationTypeConfigFields[regulationType];
-
-    if (!specificQuery || !configField) {
-      return null;
-    }
-
-    const specificResponse = await hasuraClient.request(
-      specificQuery,
-      {
-        regulationConfigId: baseConfig.id,
-      },
-      {
-        "X-GraphQL-Operation-Name": `${regulationType}RegulationDetail`,
-        "X-GraphQL-Operation-Type": "query",
+      if (!specificQuery || !configField) {
+        return null;
       }
-    );
 
-    if (specificResponse[configField].length === 0) {
+      const specificResponse = await hasuraClient.request(
+        specificQuery,
+        {
+          regulationConfigId: baseConfig.id,
+        },
+        {
+          "X-GraphQL-Operation-Name": `${regulationType}RegulationDetail`,
+          "X-GraphQL-Operation-Type": "query",
+        }
+      );
+
+      if (specificResponse[configField].length === 0) {
+        return null;
+      }
+
+      return {
+        ...baseConfig,
+        [`${regulationType}_regulation_config`]:
+          specificResponse[configField][0],
+      };
+    } catch (error) {
+      console.error("Error fetching regulation detail:", error);
       return null;
     }
-
-    return {
-      ...baseConfig,
-      [`${regulationType}_regulation_config`]: specificResponse[configField][0],
-    };
   }
 );
