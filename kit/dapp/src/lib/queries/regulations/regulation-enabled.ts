4594955d162e44cdb36b17d09637f4dbcefc3933
Silke Van den Broeck <silke@settlemint.com>
2025-05-20 10:42:25 +0200

    fix: error handling of isRegulationEnabled query

--- kit/dapp/src/lib/queries/regulations/regulation-enabled.ts
+++ kit/dapp/src/lib/queries/regulations/regulation-enabled.ts
@@ -33,18 +33,23 @@ export const isRegulationEnabled = withTracing(
   "queries",
   "isRegulationEnabled",
   async (assetId: string, regulationType: string) => {
-    const response = await hasuraClient.request(
-      RegulationEnabled,
-      {
-        assetId,
-        regulationType,
-      },
-      {
-        "X-GraphQL-Operation-Name": "RegulationEnabled",
-        "X-GraphQL-Operation-Type": "query",
-      }
-    );
+    try {
+      const response = await hasuraClient.request(
+        RegulationEnabled,
+        {
+          assetId,
+          regulationType,
+        },
+        {
+          "X-GraphQL-Operation-Name": "RegulationEnabled",
+          "X-GraphQL-Operation-Type": "query",
+        }
+      );
 
-    return response.regulation_configs.length > 0;
+      return response.regulation_configs.length > 0;
+    } catch (error) {
+      console.error("Error checking if regulation is enabled:", error);
+      return false;
+    }
   }
 );
