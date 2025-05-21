96a610b7266d4d04df2188a10d60a121042c3c5d
Silke Van den Broeck <silke@settlemint.com>
2025-05-21 13:14:28 +0200

    fix: rename

--- kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/regulations/mica/page.tsx
+++ kit/dapp/src/app/[locale]/(private)/assets/[assettype]/[address]/regulations/mica/page.tsx
@@ -2,7 +2,7 @@ import { getRegulationDetail } from "@/lib/queries/regulations/regulation-detail
 import type { AssetType } from "@/lib/utils/typebox/asset-types";
 import type { Locale } from "next-intl";
 import type { Address } from "viem";
-import { MicaRegulationLayout } from "./components/layout";
+import { MicaRegulationLayout } from "./_components/layout";
 
 interface PageProps {
   params: Promise<{ locale: Locale; address: Address; assettype: AssetType }>;
