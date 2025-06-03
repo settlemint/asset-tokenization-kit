8c3d7ca612b89f187562bdc143c2d2e1a4de4b32
janb87 <12234016+janb87@users.noreply.github.com>
2025-06-03 10:47:48 +0200

    template

--- kit/subgraph/src/capped/fetch/capped.ts
+++ kit/subgraph/src/capped/fetch/capped.ts
@@ -1,5 +1,6 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { TokenCapped } from "../../../generated/schema";
+import { Capped as CappedTemplate } from "../../../generated/templates";
 import { fetchToken } from "../../token/fetch/token";
 import { setBigNumber } from "../../utils/bignumber";
 
@@ -11,6 +12,7 @@ export function fetchCapped(address: Address): TokenCapped {
     const token = fetchToken(address);
     setBigNumber(capped, "cap", BigInt.zero(), token.decimals);
     capped.save();
+    CappedTemplate.create(address);
   }
 
   return capped;
