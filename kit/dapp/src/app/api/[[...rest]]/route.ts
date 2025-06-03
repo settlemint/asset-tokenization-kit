68ff6034c4f2de192d1b17ee43c18783e7892069
Roderik van der Veer <roderik@settlemint.com>
2025-06-03 12:35:34 +0200

    error handling

--- kit/dapp/src/app/api/[[...rest]]/route.ts
+++ kit/dapp/src/app/api/[[...rest]]/route.ts
@@ -4,18 +4,55 @@ import { createContext } from "@/lib/orpc/routes/context/context";
 import { router } from "@/lib/orpc/routes/router";
 import { OpenAPIHandler } from "@orpc/openapi/fetch";
 import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
+import { onError, ORPCError, ValidationError } from "@orpc/server";
 import { CORSPlugin } from "@orpc/server/plugins";
 import {
   experimental_ZodSmartCoercionPlugin as ZodSmartCoercionPlugin,
   experimental_ZodToJsonSchemaConverter as ZodToJsonSchemaConverter,
 } from "@orpc/zod/zod4";
 import { NextRequest } from "next/server";
+import { z, ZodError } from "zod/v4";
 import pkgjson from "../../../../package.json";
 
 const handler = new OpenAPIHandler(router, {
+  clientInterceptors: [
+    onError((error) => {
+      if (
+        error instanceof ORPCError &&
+        error.code === "BAD_REQUEST" &&
+        error.cause instanceof ValidationError
+      ) {
+        const zodError = new ZodError(error.cause.issues as z.core.$ZodIssue[]);
+
+        throw new ORPCError("INPUT_VALIDATION_FAILED", {
+          status: 422,
+          data: z.treeifyError(zodError),
+          cause: error.cause,
+        });
+      }
+
+      if (
+        error instanceof ORPCError &&
+        error.code === "INTERNAL_SERVER_ERROR" &&
+        error.cause instanceof ValidationError
+      ) {
+        const zodError = new ZodError(error.cause.issues as z.core.$ZodIssue[]);
+
+        throw new ORPCError("OUTPUT_VALIDATION_FAILED", {
+          status: 522,
+          data: z.treeifyError(zodError),
+          cause: error.cause,
+        });
+      }
+    }),
+  ],
   plugins: [
     new CORSPlugin({
+      allowMethods: ["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH"],
+      allowHeaders: ["Content-Type", "x-api-key"],
       exposeHeaders: ["Content-Disposition"],
+      credentials: true,
+      origin: (origin) => origin ?? "http://localhost:3000",
     }),
     new ZodSmartCoercionPlugin(),
     new OpenAPIReferencePlugin({
--- kit/dapp/src/lib/orpc/routes/procedures/public.contract.ts
+++ kit/dapp/src/lib/orpc/routes/procedures/public.contract.ts
@@ -1,3 +1,23 @@
 import { oc } from "@orpc/contract";
+import z from "zod/v4";
 
-export const pc = oc;
+export const pc = oc.errors({
+  INPUT_VALIDATION_FAILED: {
+    status: 422,
+    message: "Input validation failed",
+    data: z.object({
+      errors: z.array(z.string()),
+    }),
+  },
+  OUTPUT_VALIDATION_FAILED: {
+    status: 522,
+    message: "Output validation failed",
+    data: z.object({
+      errors: z.array(z.string()),
+    }),
+  },
+  INTERNAL_SERVER_ERROR: {
+    status: 500,
+    message: "Internal server error",
+  },
+});
