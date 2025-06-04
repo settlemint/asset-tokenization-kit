79712d60cd537c932acf71aabe83c3186afa7568
janb87 <12234016+janb87@users.noreply.github.com>
2025-06-04 16:42:58 +0200

    no subgraph

--- kit/subgraph/src/fixed-yield-schedule-factory/fixed-yield-schedule-factory.ts
+++
@@ -1,8 +0,0 @@
-import { SMARTFixedYieldScheduleCreated } from "../../generated/templates/FixedYieldScheduleFactory/FixedYieldScheduleFactory";
-import { fetchEvent } from "../event/fetch/event";
-
-export function handleSMARTFixedYieldScheduleCreated(
-  event: SMARTFixedYieldScheduleCreated
-): void {
-  fetchEvent(event, "FixedYieldScheduleCreated");
-}
