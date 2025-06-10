5e7a50eef1cfb7eae0f9ff3576ddd38d3626109e
janb87 <12234016+janb87@users.noreply.github.com>
2025-06-10 14:28:49 +0200

    tests

---
+++ kit/subgraph/test/assets/bond.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from "bun:test";
+import { theGraphClient, theGraphGraphql } from "../utils/thegraph-client";
+
+describe("Bonds", () => {
+  it("should fetch a list of all bonds", async () => {
+    const query = theGraphGraphql(
+      `query($where: Token_filter) {
+        tokens(where: $where, orderBy: name) {
+          name
+          type
+          bond {
+           isMatured
+            faceValue
+          }
+        }
+      }
+    `
+    );
+    const response = await theGraphClient.request(query, {
+      where: {
+        bond_not: null,
+      },
+    });
+    expect(response.tokens.length).toBe(1);
+    expect(response.tokens).toEqual([
+      {
+        name: "Euro Bonds",
+        type: "bond",
+        bond: { isMatured: false, faceValue: "0.000123" },
+      },
+    ]);
+  });
+});
