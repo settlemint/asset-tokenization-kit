ef513d2dbc2202a886e8630f15bf9747a4007419
Daan Poron <daan@poron.be>
2025-06-12 10:35:46 +0200

    feat: identity and address allow and block list compliance modules (#2382)

--- kit/contracts/test/Constants.sol
+++ kit/contracts/test/Constants.sol
@@ -10,4 +10,14 @@ library TestConstants {
     uint16 public constant COUNTRY_CODE_BE = 56;
     uint16 public constant COUNTRY_CODE_JP = 392;
     uint16 public constant COUNTRY_CODE_US = 840;
+    uint16 public constant COUNTRY_CODE_GB = 826;
+    uint16 public constant COUNTRY_CODE_NL = 528;
+    uint16 public constant COUNTRY_CODE_FR = 250;
+    uint16 public constant COUNTRY_CODE_DE = 276;
+    uint16 public constant COUNTRY_CODE_IT = 380;
+    uint16 public constant COUNTRY_CODE_ES = 724;
+    uint16 public constant COUNTRY_CODE_PT = 620;
+    uint16 public constant COUNTRY_CODE_SE = 752;
+    uint16 public constant COUNTRY_CODE_NO = 578;
+    uint16 public constant COUNTRY_CODE_DK = 208;
 }
