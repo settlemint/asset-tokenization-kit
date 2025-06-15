bd715ee0beb1925b073a27b453e4bdd475a19623
Roderik van der Veer <roderik@settlemint.com>
2025-06-16 00:34:35 +0200

    wip

--- kit/dapp-v2/vite.config.ts
+++ kit/dapp-v2/vite.config.ts
@@ -1,9 +1,6 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { generateDeclarations } from "intl-t/declarations";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-
-void generateDeclarations("./src/locales/");
 
 export default defineConfig({
   server: {
