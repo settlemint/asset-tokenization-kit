610f3c59f7c45f337dd5bdc1cefe9ac3495ef697
Snigdha Singh <snigdha920@gmail.com>
2025-06-18 19:18:19 +0200

    actually deploy the push airdrop

--- kit/contracts/scripts/hardhat/addons/airdrop/index.ts
+++ kit/contracts/scripts/hardhat/addons/airdrop/index.ts
@@ -1,5 +1,6 @@
 import type { AirdropMerkleTree } from "../../entities/airdrop/merkle-tree";
 import type { Asset } from "../../entities/asset";
+import { createPushAirdrop } from "./push-airdrop";
 import { createVestingAirdrop } from "./vesting-airdrop";
 
 export const createAirdrops = async (
@@ -7,8 +8,10 @@ export const createAirdrops = async (
   merkleTree: AirdropMerkleTree
 ) => {
   const vestingAirdrop = await createVestingAirdrop(asset, merkleTree);
+  const pushAirdrop = await createPushAirdrop(asset, merkleTree);
 
   return {
     vestingAirdrop,
+    pushAirdrop,
   };
 };
