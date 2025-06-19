f03e9bf3c893f8d9c32e37780004005591a2e842
Snigdha Singh <snigdha920@gmail.com>
2025-06-19 18:03:55 +0200

    process tokens transferred events for airdrops

---
+++ kit/subgraph/src/addons/airdrop/airdrop.ts
@@ -0,0 +1,82 @@
+import {
+  type AirdropBatchTokensTransferred,
+  type AirdropTokensTransferred,
+} from "../../../generated/templates/PushAirdrop/PushAirdrop";
+import { fetchEvent } from "../../event/fetch/event";
+import { setBigNumber } from "../../utils/bignumber";
+import { getTokenDecimals } from "../../utils/token-decimals";
+import { fetchAirdrop } from "./fetch/airdrop";
+import { fetchAirdropAllocation } from "./fetch/airdrop-allocation";
+import { fetchAirdropRecipient } from "./fetch/airdrop-recipient";
+
+export function handleAirdropTokensTransferred(
+  event: AirdropTokensTransferred
+): void {
+  fetchEvent(event, "AirdropTokensTransferred");
+  const airdrop = fetchAirdrop(event.address);
+  const tokenDecimals = getTokenDecimals(airdrop.token);
+  setBigNumber(
+    airdrop,
+    "amountTransferred",
+    event.params.amount.plus(airdrop.amountTransferredExact),
+    tokenDecimals
+  );
+  airdrop.save();
+
+  const airdropRecipient = fetchAirdropRecipient(
+    event.address,
+    event.params.recipient
+  );
+  setBigNumber(
+    airdropRecipient,
+    "amountTransferred",
+    event.params.amount.plus(airdropRecipient.amountTransferredExact),
+    tokenDecimals
+  );
+  airdropRecipient.save();
+
+  const airdropAllocation = fetchAirdropAllocation(
+    event.address,
+    event.params.index
+  );
+  airdropAllocation.recipient = airdropRecipient.id;
+  setBigNumber(
+    airdropAllocation,
+    "amountTransferred",
+    event.params.amount.plus(airdropAllocation.amountTransferredExact),
+    tokenDecimals
+  );
+  airdropAllocation.save();
+}
+
+export function handleAirdropBatchTokensTransferred(
+  event: AirdropBatchTokensTransferred
+): void {
+  fetchEvent(event, "AirdropBatchTokensTransferred");
+  const airdrop = fetchAirdrop(event.address);
+  const tokenDecimals = getTokenDecimals(airdrop.token);
+  const indices = event.params.indices;
+  for (let i = 0; i < indices.length; i++) {
+    const index = indices.at(i);
+    const amount = event.params.amounts.at(i);
+    const recipient = event.params.recipients.at(i);
+
+    const airdropRecipient = fetchAirdropRecipient(airdrop.id, recipient);
+    setBigNumber(
+      airdropRecipient,
+      "amountTransferred",
+      amount.plus(airdropRecipient.amountTransferredExact),
+      tokenDecimals
+    );
+    airdropRecipient.save();
+
+    const airdropAllocation = fetchAirdropAllocation(airdrop.id, index);
+    setBigNumber(
+      airdropAllocation,
+      "amountTransferred",
+      amount.plus(airdropAllocation.amountTransferredExact),
+      tokenDecimals
+    );
+    airdropAllocation.save();
+  }
+}
