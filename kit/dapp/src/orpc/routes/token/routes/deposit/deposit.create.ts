ab6d0885df210eb8dd029b7016dcf6dbea6f6da6
Snigdha Singh <snigdha920@gmail.com>
2025-07-01 11:40:00 +0200

    hardcode challenge for create deposit

--- kit/dapp/src/orpc/routes/token/routes/deposit/deposit.create.ts
+++ kit/dapp/src/orpc/routes/token/routes/deposit/deposit.create.ts
@@ -3,7 +3,7 @@ import {
   ethereumHash,
   getEthereumHash,
 } from "@/lib/zod/validators/ethereum-hash";
-import { tokenFactoryPermissionMiddleware } from "@/orpc/middlewares/auth/token-factory-permission.middleware";
+import { handleChallenge } from "@/orpc/helpers/challenge-response";
 import { portalMiddleware } from "@/orpc/middlewares/services/portal.middleware";
 import { systemMiddleware } from "@/orpc/middlewares/system/system.middleware";
 import { tokenFactoryMiddleware } from "@/orpc/middlewares/system/token-factory.middleware";
@@ -11,11 +11,13 @@ import { onboardedRouter } from "@/orpc/procedures/onboarded.router";
 import { z } from "zod/v4";
 
 const CREATE_DEPOSIT_MUTATION = portalGraphql(`
-  mutation CreateTokenMutation($address: String!, $from: String!, $input: ATKDepositFactoryImplementationCreateDepositInput!) {
+  mutation CreateTokenMutation($address: String!, $from: String!, $input: ATKDepositFactoryImplementationCreateDepositInput!, $verificationId: String, $challengeResponse: String!) {
     CreateToken: ATKDepositFactoryImplementationCreateDeposit(
       address: $address
       from: $from
       input: $input
+      verificationId: $verificationId
+      challengeResponse: $challengeResponse
     ) {
       transactionHash
     }
@@ -26,7 +28,7 @@ export const depositCreate = onboardedRouter.token.depositCreate
   .use(portalMiddleware)
   .use(systemMiddleware)
   .use(tokenFactoryMiddleware("deposit"))
-  .use(tokenFactoryPermissionMiddleware(["deployer"]))
+  // .use(tokenFactoryPermissionMiddleware(["deployer"]))
   .handler(async ({ input, context }) => {
     const sender = context.auth.user;
 
@@ -42,6 +44,10 @@ export const depositCreate = onboardedRouter.token.depositCreate
           initialModulePairs_: [],
           requiredClaimTopics_: [],
         },
+        ...(await handleChallenge(sender, {
+          code: "111111",
+          type: "pincode",
+        })),
       },
       z.object({
         CreateToken: z.object({
