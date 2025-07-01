f410f8271393f88241967648a5b52a8a375134dc
Snigdha Singh <snigdha920@gmail.com>
2025-07-01 22:44:43 +0200

    cleanup

--- kit/dapp/src/orpc/routes/token/token.router.ts
+++ kit/dapp/src/orpc/routes/token/token.router.ts
@@ -1,7 +1,6 @@
 import { bondCreate } from "@/orpc/routes/token/routes/bond/bond.create";
 import { depositCreate } from "@/orpc/routes/token/routes/deposit/deposit.create";
 import { factoryCreate } from "@/orpc/routes/token/routes/factory.create";
-import { factoryGrantRole } from "@/orpc/routes/token/routes/factory.grant-role";
 import { factoryList } from "@/orpc/routes/token/routes/factory.list";
 import { factoryRead } from "@/orpc/routes/token/routes/factory.read";
 import { list } from "@/orpc/routes/token/routes/token.list";
@@ -12,7 +11,6 @@ const routes = {
   bondCreate,
   depositCreate,
   factoryCreate,
-  factoryGrantRole,
   factoryList,
   factoryRead,
   list,
