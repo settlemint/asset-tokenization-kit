55e076b4ca74dcb4024d9eeb9cfaadbd06092935
janb87 <12234016+janb87@users.noreply.github.com>
2025-07-02 12:27:56 +0200

    claude generated

--- kit/subgraph/src/token-extensions/collateral/utils/collateral-utils.ts
+++ kit/subgraph/src/token-extensions/collateral/utils/collateral-utils.ts
@@ -2,6 +2,8 @@ import { Address, log } from "@graphprotocol/graph-ts";
 import { IdentityClaim } from "../../../../generated/schema";
 import { fetchIdentity } from "../../../identity/fetch/identity";
 import { fetchCollateral } from "../fetch/collateral";
+import { fetchToken } from "../../../token/fetch/token";
+import { updateTokenCollateralStats } from "../../../stats/utils/token-collateral-stats";
 
 export function isCollateralClaim(claim: IdentityClaim): boolean {
   return claim.name == "collateral";
@@ -22,4 +24,8 @@ export function updateCollateral(collateralClaim: IdentityClaim): void {
   collateral.identityClaim = collateralClaim.id;
 
   collateral.save();
+
+  // Update TokenCollateralStats
+  const token = fetchToken(Address.fromBytes(identity.token!));
+  updateTokenCollateralStats(token);
 }
