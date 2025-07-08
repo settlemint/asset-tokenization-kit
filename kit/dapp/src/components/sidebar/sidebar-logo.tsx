b0c8db0f7dca63fcdcb6002daceec9e448b856b3
Snigdha Singh <snigdha920@gmail.com>
2025-07-08 16:27:56 +0200

    link logo to homepage

--- kit/dapp/src/components/sidebar/sidebar-logo.tsx
+++ kit/dapp/src/components/sidebar/sidebar-logo.tsx
@@ -4,6 +4,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Link } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 
 /**
@@ -18,17 +19,19 @@ export function SidebarLogo() {
           size="lg"
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <div className="flex aspect-square size-8 items-center justify-center">
-            <Logo variant="icon" />
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="font-bold text-lg text-primary-foreground">
-              SettleMint
-            </span>
-            <span className="-mt-1 overflow-hidden truncate text-ellipsis text-md text-sm leading-snug text-primary-foreground dark:text-foreground ">
-              {t("appName")}
-            </span>
-          </div>
+          <Link to="/" className="flex items-center gap-2">
+            <div className="flex aspect-square size-8 items-center justify-center">
+              <Logo variant="icon" />
+            </div>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="font-bold text-lg text-primary-foreground">
+                SettleMint
+              </span>
+              <span className="-mt-1 overflow-hidden truncate text-ellipsis text-md text-sm leading-snug text-primary-foreground dark:text-foreground ">
+                {t("appName")}
+              </span>
+            </div>
+          </Link>
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
