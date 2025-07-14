326193004b241273fbb4df6c55dbc6cff64b406d
janb87 <12234016+janb87@users.noreply.github.com>
2025-07-14 17:08:57 +0200

    move to state

--- kit/dapp/src/components/onboarding/simplified/onboarding-layout.tsx
+++ kit/dapp/src/components/onboarding/simplified/onboarding-layout.tsx
@@ -3,18 +3,17 @@ import type {
   StepDefinition,
   StepGroup,
 } from "@/components/multistep-form/types";
-import { OnboardingStep } from "@/components/onboarding/simplified/state-machine";
+import {
+  OnboardingStep,
+  OnboardingStepGroup,
+  onboardingSteps,
+} from "@/components/onboarding/simplified/state-machine";
 import { createLogger } from "@settlemint/sdk-utils/logging";
+import { useStore } from "@tanstack/react-store";
 import { useCallback, useMemo, type FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 
 const logger = createLogger();
-
-const enum OnboardingStepGroup {
-  wallet = "wallet",
-  system = "system",
-  identity = "identity",
-}
 
 type OnboardingStepDefintion = Omit<StepDefinition, "id"> & {
   id: OnboardingStep;
@@ -26,120 +25,98 @@ export const OnboardingLayout: FunctionComponent<{
   currentStep: OnboardingStep;
 }> = ({ children, currentStep }) => {
   const { t } = useTranslation(["onboarding", "general"]);
+  const steps = useStore(onboardingSteps, (state) => state);
 
-  const groups = useMemo((): StepGroup[] => {
-    return [
-      {
-        id: OnboardingStepGroup.wallet,
-        title: t("wallet.title"),
-        description: t("wallet.description"),
-        collapsible: true,
-        defaultExpanded: currentStep === OnboardingStep.wallet,
-      },
-      {
-        id: OnboardingStepGroup.system,
-        title: t("system.title"),
-        description: t("system.description"),
-        collapsible: true,
-        defaultExpanded: currentStep === OnboardingStep.system,
-      },
-      {
-        id: OnboardingStepGroup.identity,
-        title: t("steps.identity.title"),
-        description: t("steps.identity.description"),
-        collapsible: true,
-        defaultExpanded: currentStep === OnboardingStep.identity,
-      },
-    ];
-  }, [t, currentStep]);
-
-  const steps = useMemo((): OnboardingStepDefintion[] => {
-    const steps: OnboardingStepDefintion[] = [
-      // Wallet steps
-      {
-        id: OnboardingStep.wallet,
+  const stepTranslationMappings = useMemo<
+    Record<OnboardingStep, { title: string; description: string }>
+  >(
+    () => ({
+      [OnboardingStep.wallet]: {
         title: t("steps.wallet.title"),
         description: t("steps.wallet.description"),
-        groupId: OnboardingStepGroup.wallet,
       },
-      {
-        id: OnboardingStep.walletSecurity,
+      [OnboardingStep.walletSecurity]: {
         title: t("steps.security.title"),
         description: t("steps.security.description"),
-        groupId: OnboardingStepGroup.wallet,
       },
-      {
-        id: OnboardingStep.walletRecoveryCodes,
+      [OnboardingStep.walletRecoveryCodes]: {
         title: "Recovery Codes",
         description: "Save your wallet recovery codes",
-        groupId: OnboardingStepGroup.wallet,
       },
-      // 1. Deploy core system
-      {
-        id: OnboardingStep.system,
+      [OnboardingStep.system]: {
         title: t("steps.system.title"),
         description: t("steps.system.description"),
-        groupId: OnboardingStepGroup.system,
       },
-      /* TODO: implement these steps again
-      // 2. Configure platform settings
-      {
-        id: "configure-platform-settings",
-        title: t("platform.title"),
-        description: t("platform.description"),
+      [OnboardingStep.identity]: {
+        title: t("steps.identity.title"),
+        description: t("steps.identity.description"),
       },
-      // 3. Select supported assets
-      {
-        id: "asset-selection",
-        title: t("steps.assets.title"),
-        description: t("steps.assets.description"),
-        groupId: "system",
+    }),
+    [t]
+  );
+
+  const groupTranslationMappings = useMemo<
+    Record<OnboardingStepGroup, { title: string; description: string }>
+  >(
+    () => ({
+      [OnboardingStepGroup.wallet]: {
+        title: t("wallet.title"),
+        description: t("wallet.description"),
       },
-      // 4. Enable platform addons
-      {
-        id: "enable-platform-addons",
-        title: "Configure Platform Add-ons",
-        description: "Enhance your platform with optional features",
-        groupId: "system",
+      [OnboardingStepGroup.system]: {
+        title: t("system.title"),
+        description: t("system.description"),
       },
-      // 1. Create your ONCHAINID
-      {
-        id: "create-onchainid",
-        title: "Create your ONCHAINID",
-        description: "Generate your blockchain identity for compliance",
-        groupId: "identity",
-        fields: [],
-        onStepComplete: async () => Promise.resolve(),
+      [OnboardingStepGroup.identity]: {
+        title: t("steps.identity.title"),
+        description: t("steps.identity.description"),
       },
-      // 2. Add personal information
-      {
-        id: "add-personal-information",
-        title: "Add personal information",
-        description: "Provide KYC information for compliance verification",
-        groupId: "identity",
-      },
-      // 3. Finish onboarding
-      {
-        id: "finish",
-        title: "Finish",
-        description: "Review and confirm your onboarding details",
-        groupId: "identity",
-      },
-      */
-    ];
+    }),
+    [t]
+  );
+
+  const groups = useMemo((): StepGroup[] => {
+    return steps.reduce<StepGroup[]>((acc, step) => {
+      const existingGroup = acc.find(
+        (group) => group.id === step.groupId.toString()
+      );
+      if (!existingGroup) {
+        acc.push({
+          id: step.groupId,
+          title: groupTranslationMappings[step.groupId].title,
+          description: groupTranslationMappings[step.groupId].description,
+          collapsible: true,
+          defaultExpanded: currentStep === step.step,
+        });
+      } else {
+        existingGroup.defaultExpanded = currentStep === step.step;
+      }
+      return acc;
+    }, []);
+  }, [steps, groupTranslationMappings, currentStep]);
+
+  const stepsWithTranslations = useMemo((): OnboardingStepDefintion[] => {
+    const withTranslations = steps.map(
+      (step): OnboardingStepDefintion => ({
+        ...step,
+        id: step.step,
+        title: stepTranslationMappings[step.step].title,
+        description: stepTranslationMappings[step.step].description,
+      })
+    );
 
     // Inject the children of the current step
-    const activeStep = steps.find((step) => step.id === currentStep);
+    const activeStep = withTranslations.find((step) => step.id === currentStep);
     if (activeStep) {
       activeStep.component = () => <>{children}</>;
     }
 
-    return steps;
-  }, [t, currentStep, children]);
+    return withTranslations;
+  }, [currentStep, children, steps, stepTranslationMappings]);
 
   const defaultStepIndex = useMemo(() => {
-    return steps.findIndex((step) => step.id === currentStep);
-  }, [steps, currentStep]);
+    return stepsWithTranslations.findIndex((step) => step.id === currentStep);
+  }, [stepsWithTranslations, currentStep]);
 
   const onComplete = useCallback(() => {
     logger.info("completed");
@@ -150,7 +127,7 @@ export const OnboardingLayout: FunctionComponent<{
       name="onboarding"
       title="Let's get you set up!"
       description="We'll set up your wallet and will configure your identity on the blockchain to use this platform."
-      steps={steps}
+      steps={stepsWithTranslations}
       groups={groups}
       onComplete={onComplete}
       defaultStepIndex={defaultStepIndex}
