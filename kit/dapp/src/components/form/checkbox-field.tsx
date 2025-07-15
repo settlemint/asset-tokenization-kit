3272d7fc15b8ca52d4ac8f7872da7c3534f63a69
Snigdha Singh <snigdha920@gmail.com>
2025-07-15 11:22:01 +0200

    fix(form): remove redundant onclick to fix checkbox event bubbling

--- kit/dapp/src/components/form/checkbox-field.tsx
+++ kit/dapp/src/components/form/checkbox-field.tsx
@@ -19,12 +19,7 @@ export function CheckboxField({
 
   return (
     <FieldLayout>
-      <div
-        className="flex items-center space-x-2"
-        onClick={() => {
-          field.handleChange(!field.state.value);
-        }}
-      >
+      <div className="flex items-center space-x-2">
         <Checkbox
           id={field.name}
           checked={field.state.value}
