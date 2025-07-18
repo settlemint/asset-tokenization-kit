c82434a4c0679ef6a98d795a387e2f174629acb6
Snigdha Singh <snigdha920@gmail.com>
2025-07-18 13:00:44 +0200

    fix select asset type

--- kit/dapp/src/components/asset-designer/select-asset-type.tsx
+++ kit/dapp/src/components/asset-designer/select-asset-type.tsx
@@ -1,10 +1,9 @@
-import { assetDesignerFormOptions } from "@/components/asset-designer/shared-form";
-import { useAssetDesignerSteps } from "@/components/asset-designer/steps";
-import { getNextStepId } from "@/components/stepper/utils";
+import {
+  assetDesignerFormOptions,
+  onStepSubmit,
+} from "@/components/asset-designer/shared-form";
 import { withForm } from "@/hooks/use-app-form";
 import { useSettings } from "@/hooks/use-settings";
-import type { AssetFactoryTypeId } from "@/lib/zod/validators/asset-types";
-import { getAssetTypeFromFactoryTypeId } from "@/lib/zod/validators/asset-types";
 import { orpc } from "@/orpc/orpc-client";
 import { useQuery } from "@tanstack/react-query";
 import { useMemo } from "react";
@@ -13,10 +12,11 @@ import { Button } from "../ui/button";
 
 export const SelectAssetType = withForm({
   ...assetDesignerFormOptions,
-  props: {},
-  render: function Render({ form }) {
+  props: {
+    onStepSubmit,
+  },
+  render: function Render({ form, onStepSubmit }) {
     const { t } = useTranslation(["asset-designer", "asset-types"]);
-    const steps = useAssetDesignerSteps();
     const [systemAddress] = useSettings("SYSTEM_ADDRESS");
     const { data: systemDetails } = useQuery({
       ...orpc.system.read.queryOptions({
