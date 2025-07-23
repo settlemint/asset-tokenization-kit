5682b47f6c05bc92f2a68ab1388f03588020fd17
Patrick Mualaba <patrick.mualaba@live.be>
2025-07-23 22:53:25 +0200

    fix: styling onboarding wizard

--- kit/dapp/src/routes/_private/onboarding.tsx
+++ kit/dapp/src/routes/_private/onboarding.tsx
@@ -35,7 +35,7 @@ function OnboardingLayout() {
       </div>
 
       <div className="flex flex-col justify-center items-center h-[calc(100vh-200px)] px-16 lg:px-[5vw] 2xl:px-[16vw]">
-        <Card className="p-0 gap-0 h-full">
+        <Card className="p-0 gap-0 h-full max-h-[80vh]">
           <CardContent className="p-0 m-0 h-full">
             <Outlet />
           </CardContent>
