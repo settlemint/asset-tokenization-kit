e3223e95b4ccbb862f947bf6a62ba73b5c66e108
janb87 <12234016+janb87@users.noreply.github.com>
2025-07-24 16:38:48 +0200

    fix

--- kit/dapp/src/orpc/routes/token/routes/mutations/create/token.create.ts
+++ kit/dapp/src/orpc/routes/token/routes/mutations/create/token.create.ts
@@ -1,7 +1,6 @@
 import { handleChallenge } from "@/orpc/helpers/challenge-response";
 import { tokenFactoryPermissionMiddleware } from "@/orpc/middlewares/auth/token-factory-permission.middleware";
 import { portalMiddleware } from "@/orpc/middlewares/services/portal.middleware";
-import { theGraphMiddleware } from "@/orpc/middlewares/services/the-graph.middleware";
 import { systemMiddleware } from "@/orpc/middlewares/system/system.middleware";
 import { onboardedRouter } from "@/orpc/procedures/onboarded.router";
 import { tokenCreateHandlerMap } from "@/orpc/routes/token/routes/mutations/create/helpers/handler-map";
@@ -9,7 +8,6 @@ import type { TokenCreateSchema } from "@/orpc/routes/token/routes/mutations/cre
 import { TOKEN_PERMISSIONS } from "@/orpc/routes/token/token.permissions";
 
 export const create = onboardedRouter.token.create
-  .use(theGraphMiddleware)
   .use(portalMiddleware)
   .use(systemMiddleware)
   .use(
