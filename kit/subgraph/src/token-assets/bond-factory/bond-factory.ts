9f190e338b1cdb70c87c3da8681188ed4dbe7a24
Daan Poron <daan@poron.be>
2025-07-28 12:18:46 +0200

    cleanup

--- kit/subgraph/src/token-assets/bond-factory/bond-factory.ts
+++ kit/subgraph/src/token-assets/bond-factory/bond-factory.ts
@@ -1,9 +1,7 @@
-import { Bytes } from "@graphprotocol/graph-ts";
 import { BondCreated } from "../../../generated/templates/BondFactory/BondFactory";
 import { fetchAccount } from "../../account/fetch/account";
 import { fetchEvent } from "../../event/fetch/event";
 import { fetchToken } from "../../token/fetch/token";
-import { fetchTopicScheme } from "../../topic-scheme-registry/fetch/topic-scheme";
 import {
   ActionName,
   createAction,
@@ -19,12 +17,6 @@ export function handleBondCreated(event: BondCreated): void {
   token.name = event.params.name;
   token.symbol = event.params.symbol;
   token.decimals = event.params.decimals;
-  const requiredClaimTopics = new Array<Bytes>();
-  for (let i = 0; i < event.params.requiredClaimTopics.length; i++) {
-    const topicSchema = fetchTopicScheme(event.params.requiredClaimTopics[i]);
-    requiredClaimTopics.push(topicSchema.id);
-  }
-  token.requiredClaimTopics = requiredClaimTopics;
   token.save();
 
   const bond = fetchBond(event.params.tokenAddress);
