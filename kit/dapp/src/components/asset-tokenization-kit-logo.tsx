4995ee5586adb2b492dad2d5f69c09d4e22e3649
Snigdha Singh <snigdha920@gmail.com>
2025-07-29 16:39:28 +0200

    leave confirmation modal

--- kit/dapp/src/components/asset-tokenization-kit-logo.tsx
+++ kit/dapp/src/components/asset-tokenization-kit-logo.tsx
@@ -4,18 +4,11 @@ export function AssetTokenizationKitLogo() {
   const { t } = useTranslation(["general"]);
 
   return (
-    <div className="flex w-full items-center gap-3">
-      <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
-        <Logo variant="icon" forcedColorMode="dark" />
+    <div className="flex items-center gap-2 !text-md font-semibold text-foreground">
+      <div className="size-5">
+        <Logo variant="icon" />
       </div>
-      <div className="flex flex-col text-foreground leading-none">
-        <span className="font-bold text-lg text-primary-foreground">
-          {t("general:appName")}
-        </span>
-        <span className="-mt-1 overflow-hidden truncate text-ellipsis text-md text-sm leading-snug text-primary-foreground dark:text-foreground">
-          {t("general:appDescription")}
-        </span>
-      </div>
+      <div>{t("general:appName")}</div>
     </div>
   );
 }
