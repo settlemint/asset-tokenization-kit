4b513f67f140f848b7b5df7a44a00ce7b27f6631
Daan Poron <daan@poron.be>
2025-08-01 12:42:38 +0200

    f

--- kit/contracts/test/system/ATKSystem.t.sol
+++ kit/contracts/test/system/ATKSystem.t.sol
@@ -27,6 +27,7 @@ import { SMARTComplianceModuleParamPair } from
     "../../contracts/smart/interface/structs/SMARTComplianceModuleParamPair.sol";
 import { IATKTypedImplementationRegistry } from "../../contracts/system/IATKTypedImplementationRegistry.sol";
 import { IATKCompliance } from "../../contracts/system/compliance/IATKCompliance.sol";
+import { IATKSystemAccessManaged } from "../../contracts/system/access-manager/IATKSystemAccessManaged.sol";
 
 // Import system errors
 import {
@@ -236,6 +237,7 @@ contract ATKSystemTest is Test {
         bytes memory initData = abi.encodeWithSelector(
             systemImplementation.initialize.selector,
             admin,
+            impls.systemAccessManager,
             impls.compliance,
             impls.identityRegistry,
             impls.identityRegistryStorage,
@@ -248,8 +250,7 @@ contract ATKSystemTest is Test {
             impls.identityVerificationModule,
             impls.tokenFactoryRegistry,
             impls.complianceModuleRegistry,
-            impls.addonRegistry,
-            impls.systemAccessManager
+            impls.addonRegistry
         );
         ERC1967Proxy proxy = new ERC1967Proxy(address(systemImplementation), initData);
         return IATKSystem(address(proxy));
@@ -286,7 +287,7 @@ contract ATKSystemTest is Test {
         // Compliance module should be set via compliance module registry
 
         // Admin should have default admin role
-        assertTrue(IAccessControl(address(atkSystem)).hasRole(ATKRoles.DEFAULT_ADMIN_ROLE, admin));
+        assertTrue(IATKSystemAccessManaged(address(atkSystem)).hasSystemRole(ATKRoles.DEFAULT_ADMIN_ROLE, admin));
     }
 
     function test_SetComplianceImplementation_OnlyAdmin_AccessControl() public {
@@ -443,7 +444,7 @@ contract ATKSystemTest is Test {
     function test_SupportsInterface() public view {
         assertTrue(atkSystem.supportsInterface(type(IATKSystem).interfaceId));
         assertTrue(atkSystem.supportsInterface(type(IERC165).interfaceId));
-        assertTrue(atkSystem.supportsInterface(type(IAccessControl).interfaceId));
+        assertTrue(atkSystem.supportsInterface(type(IATKSystemAccessManaged).interfaceId));
         assertFalse(atkSystem.supportsInterface(bytes4(0xffffffff)));
     }
 
@@ -456,6 +457,7 @@ contract ATKSystemTest is Test {
         bytes memory initData1 = abi.encodeWithSelector(
             systemImplementation.initialize.selector,
             admin,
+            impls.systemAccessManager,
             impls.compliance,
             impls.identityRegistry,
             impls.identityRegistryStorage,
@@ -468,8 +470,7 @@ contract ATKSystemTest is Test {
             impls.identityVerificationModule,
             impls.tokenFactoryRegistry,
             impls.complianceModuleRegistry,
-            impls.addonRegistry,
-            impls.systemAccessManager
+            impls.addonRegistry
         );
         vm.expectRevert(abi.encodeWithSelector(ComplianceImplementationNotSet.selector));
         new ERC1967Proxy(address(systemImplementation), initData1);
@@ -480,6 +481,7 @@ contract ATKSystemTest is Test {
         bytes memory initData2 = abi.encodeWithSelector(
             systemImplementation.initialize.selector,
             admin,
+            impls.systemAccessManager,
             impls.compliance,
             impls.identityRegistry,
             impls.identityRegistryStorage,
@@ -492,8 +494,7 @@ contract ATKSystemTest is Test {
             impls.identityVerificationModule,
             impls.tokenFactoryRegistry,
             impls.complianceModuleRegistry,
-            impls.addonRegistry,
-            impls.systemAccessManager
+            impls.addonRegistry
         );
         vm.expectRevert(abi.encodeWithSelector(IdentityRegistryImplementationNotSet.selector));
         new ERC1967Proxy(address(systemImplementation), initData2);
@@ -507,6 +508,7 @@ contract ATKSystemTest is Test {
         bytes memory initData = abi.encodeWithSelector(
             systemImplementation.initialize.selector,
             admin,
+            impls.systemAccessManager,
             impls.compliance,
             impls.identityRegistry,
             impls.identityRegistryStorage,
@@ -519,8 +521,7 @@ contract ATKSystemTest is Test {
             impls.identityVerificationModule,
             impls.tokenFactoryRegistry,
             impls.complianceModuleRegistry,
-            impls.addonRegistry,
-            impls.systemAccessManager
+            impls.addonRegistry
         );
         vm.expectRevert(
             abi.encodeWithSelector(
