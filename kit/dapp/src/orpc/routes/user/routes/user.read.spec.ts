b6bdaf64a8275bd0fa09fb03aa2947e52913c61f
Snigdha Singh <snigdha920@gmail.com>
2025-08-08 10:37:59 +0200

    forbidden string over regexp

--- kit/dapp/src/orpc/routes/user/routes/user.read.spec.ts
+++ kit/dapp/src/orpc/routes/user/routes/user.read.spec.ts
@@ -135,7 +135,7 @@ describe("User read", () => {
         client.user.read({
           userId: testUserData.id,
         })
-      ).rejects.toThrow(/FORBIDDEN/);
+      ).rejects.toThrow("FORBIDDEN");
     });
 
     it("regular user without 'user:list' permission cannot read other users by wallet", async () => {
