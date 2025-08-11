200ac6199edf6511dd2c117d65e9de91dec5e08c
Snigdha Singh <snigdha920@gmail.com>
2025-08-11 19:31:04 +0200

    remove use client

--- kit/dapp/src/components/compliance/details/smart-identity/smart-identity-verification-module-detail.tsx
+++ kit/dapp/src/components/compliance/details/smart-identity/smart-identity-verification-module-detail.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   ComplianceDetailActions,
   ComplianceDetailBreadcrumb,
--- kit/dapp/src/components/selectable-card/selectable-card.tsx
+++ kit/dapp/src/components/selectable-card/selectable-card.tsx
@@ -1,4 +1,4 @@
-"use client";
+"";
 
 import { cn } from "@/lib/utils";
 import { Slot } from "@radix-ui/react-slot";
