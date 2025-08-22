6155dc011e8caae81b88cb2b94fc85e1ce573a07
Snigdha Singh <snigdha920@gmail.com>
2025-08-22 16:25:05 +0200

    re-use select component

--- kit/dapp/src/components/form/select-time-interval-field.tsx
+++ kit/dapp/src/components/form/select-time-interval-field.tsx
@@ -1,41 +1,11 @@
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useFieldContext } from "@/hooks/use-form-contexts";
-import { cn } from "@/lib/utils";
+  SelectField,
+  type SelectFieldProps,
+} from "@/components/form/select-field";
 import { timeIntervals } from "@atk/zod/time-interval";
-import {
-  errorClassNames,
-  FieldDescription,
-  FieldErrors,
-  FieldLabel,
-  FieldLayout,
-} from "./field";
 import { useTranslation } from "react-i18next";
 
-export interface SelectTimeIntervalFieldProps {
-  label: string;
-  description?: string;
-  required?: boolean;
-  placeholder?: string;
-  disabled?: boolean;
-  className?: string;
-}
-
-export function SelectTimeIntervalField({
-  label,
-  description,
-  required = false,
-  placeholder,
-  disabled = false,
-  className,
-}: SelectTimeIntervalFieldProps) {
-  // The `Field` infers that it should have a `value` type of `string`
-  const field = useFieldContext<string>();
+export function SelectTimeIntervalField({ label, ...props }: SelectFieldProps) {
   const { t } = useTranslation(["common"]);
 
   const options = timeIntervals.map((interval) => ({
@@ -43,30 +13,5 @@ export function SelectTimeIntervalField({
     value: interval,
   }));
 
-  return (
-    <FieldLayout>
-      <FieldLabel htmlFor={field.name} label={label} required={required} />
-      <FieldDescription description={description} />
-      <Select
-        value={field.state.value}
-        onValueChange={field.handleChange}
-        disabled={disabled || field.state.meta.isValidating}
-      >
-        <SelectTrigger
-          id={field.name}
-          className={cn(errorClassNames(field.state.meta), className)}
-        >
-          <SelectValue placeholder={placeholder} />
-        </SelectTrigger>
-        <SelectContent>
-          {options.map((option) => (
-            <SelectItem key={option.value} value={option.value}>
-              {option.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      <FieldErrors {...field.state.meta} />
-    </FieldLayout>
-  );
+  return <SelectField label={label} options={options} {...props} />;
 }
