dddacd57c0594fee2eeab0417f04e16436cf4161
Snigdha Singh <snigdha920@gmail.com>
2025-09-03 11:29:43 +0200

    number field fix rerender

--- kit/dapp/src/components/form/number-field.tsx
+++ kit/dapp/src/components/form/number-field.tsx
@@ -1,6 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { useFieldContext } from "@/hooks/use-form-contexts";
 import { cn } from "@/lib/utils";
+import React from "react";
 import {
   errorClassNames,
   FieldDescription,
@@ -26,23 +27,30 @@ export function NumberField({
   // The `Field` infers type based on usage - could be number or string
   const field = useFieldContext<number>();
 
+  const renderInput = React.useCallback(
+    ({ className }: { className?: string }) => {
+      return (
+        <Input
+          id={field.name}
+          value={field.state.value}
+          type="number"
+          inputMode="decimal"
+          onChange={(e) => {
+            field.handleChange(e.target.valueAsNumber);
+          }}
+          className={cn(className, errorClassNames(field.state.meta))}
+        />
+      );
+    },
+    [field]
+  );
+
   return (
     <FieldLayout>
       <FieldLabel htmlFor={field.name} label={label} required={required} />
       <FieldDescription description={description} />
       <FieldWithAddons startAddon={startAddon} endAddon={endAddon}>
-        {({ className }) => (
-          <Input
-            id={field.name}
-            value={field.state.value}
-            type="number"
-            inputMode="decimal"
-            onChange={(e) => {
-              field.handleChange(e.target.valueAsNumber);
-            }}
-            className={cn(className, errorClassNames(field.state.meta))}
-          />
-        )}
+        {renderInput}
       </FieldWithAddons>
       <FieldErrors {...field.state.meta} />
     </FieldLayout>
