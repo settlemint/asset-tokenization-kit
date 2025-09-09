b576c0e7384ace5a1cd33d0846af207b1e606434
Jan Bevers <jan@settlemint.com>
2025-09-09 11:02:42 +0200

    transfer test

--- kit/dapp/src/orpc/routes/token/routes/mutations/transfer/token.transfer.spec.ts
+++ kit/dapp/src/orpc/routes/token/routes/mutations/transfer/token.transfer.spec.ts
@@ -3,18 +3,21 @@ import { createToken } from "@test/fixtures/token";
 import {
   DEFAULT_ADMIN,
   DEFAULT_INVESTOR,
+  DEFAULT_ISSUER,
   DEFAULT_PINCODE,
   getUserData,
   signInWithUser,
 } from "@test/fixtures/user";
 import { from } from "dnum";
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 
 describe("Token transfer", () => {
-  it("can transfer tokens", async () => {
+  let depositToken: Awaited<ReturnType<typeof createToken>>;
+
+  beforeAll(async () => {
     const headers = await signInWithUser(DEFAULT_ADMIN);
     const client = getOrpcClient(headers);
-    const depositToken = await createToken(
+    depositToken = await createToken(
       client,
       {
         name: "Deposit Token",
@@ -33,17 +36,28 @@ describe("Token transfer", () => {
         unpause: true,
       }
     );
+
     const admin = await getUserData(DEFAULT_ADMIN);
+    const investor = await getUserData(DEFAULT_INVESTOR);
     await client.token.mint({
       contract: depositToken.id,
-      amounts: [from("500", depositToken.decimals)],
-      recipients: [admin.wallet],
+      amounts: [
+        from("500", depositToken.decimals),
+        from("100", depositToken.decimals),
+      ],
+      recipients: [admin.wallet, investor.wallet],
       walletVerification: {
         secretVerificationCode: DEFAULT_PINCODE,
         verificationType: "PINCODE",
       },
     });
+  });
 
+  it("can transfer tokens", async () => {
+    const headers = await signInWithUser(DEFAULT_ADMIN);
+    const client = getOrpcClient(headers);
+
+    const admin = await getUserData(DEFAULT_ADMIN);
     const investor = await getUserData(DEFAULT_INVESTOR);
     await client.token.transfer({
       contract: depositToken.id,
@@ -68,6 +82,47 @@ describe("Token transfer", () => {
       updatedHolders.token?.balances.find(
         (balance) => balance.account.id === investor.wallet
       )?.value
-    ).toEqual(from("5"));
+    ).toEqual(from("105"));
+  });
+
+  it("can batch transfer tokens", async () => {
+    const headers = await signInWithUser(DEFAULT_ADMIN);
+    const client = getOrpcClient(headers);
+
+    const admin = await getUserData(DEFAULT_ADMIN);
+    const investor = await getUserData(DEFAULT_INVESTOR);
+    const issuer = await getUserData(DEFAULT_ISSUER);
+    await client.token.transfer({
+      contract: depositToken.id,
+      amounts: [
+        from("5", depositToken.decimals),
+        from("10", depositToken.decimals),
+      ],
+      recipients: [investor.wallet, issuer.wallet],
+      walletVerification: {
+        secretVerificationCode: DEFAULT_PINCODE,
+        verificationType: "PINCODE",
+      },
+    });
+
+    const updatedHolders = await client.token.holders({
+      tokenAddress: depositToken.id,
+    });
+    expect(updatedHolders.token?.balances.length).toBe(3);
+    expect(
+      updatedHolders.token?.balances.find(
+        (balance) => balance.account.id === admin.wallet
+      )?.value
+    ).toEqual(from("480"));
+    expect(
+      updatedHolders.token?.balances.find(
+        (balance) => balance.account.id === investor.wallet
+      )?.value
+    ).toEqual(from("110"));
+    expect(
+      updatedHolders.token?.balances.find(
+        (balance) => balance.account.id === issuer.wallet
+      )?.value
+    ).toEqual(from("10"));
   });
 });
