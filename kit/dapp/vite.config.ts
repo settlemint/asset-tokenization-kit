fa38524349564e266637fb1b943080dad5a12d93
Snigdha Singh <snigdha920@gmail.com>
2025-09-11 12:52:43 +0200

    remove not required vite config changes

--- kit/dapp/vite.config.ts
+++ kit/dapp/vite.config.ts
@@ -40,11 +40,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  test: {
-    typecheck: {
-      enabled: true,
-    },
-  },
   plugins: [
     tsConfigPaths(),
     tanstackStart({
