ec0eb55ed22da89a58d6631d18b3f3d757fc7633
Snigdha Singh <snigdha920@gmail.com>
2025-09-24 23:49:15 +0200

    assets overview

--- kit/dapp/src/routes/_private/_onboarded/_sidebar/index.tsx
+++ kit/dapp/src/routes/_private/_onboarded/_sidebar/index.tsx
@@ -14,6 +14,7 @@
  * @see {@link https://tanstack.com/query/latest/docs/react/guides/suspense} - React Query suspense mode
  */
 
+import { AssetOverviewSection } from "@/components/dashboard/asset-overview-section";
 import { IdentityProgress } from "@/components/dashboard/identity-progress/identity-progress";
 import { PortfolioDetails } from "@/components/dashboard/portfolio-details/portfolio-details";
 import { createFileRoute } from "@tanstack/react-router";
@@ -63,6 +64,7 @@ function Home() {
           <PortfolioDetails />
         </>
       )}
+      {system.userPermissions?.roles.tokenManager && <AssetOverviewSection />}
     </div>
   );
 }
