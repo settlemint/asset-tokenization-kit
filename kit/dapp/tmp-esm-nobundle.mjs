de9a8c041f927622fb7263c4834f22d6cda28ff9
Patrick Mualaba <patrick.mualaba@live.be>
2025-10-14 16:57:02 +0200

    merge main

--- kit/dapp/tmp-esm-nobundle.mjs
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-const schema = z.string();
-export {
-  schema
-};
--- kit/dapp/tmp-resolver.ts
+++
@@ -1,3 +0,0 @@
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-export const resolver = zodResolver(z.string());
